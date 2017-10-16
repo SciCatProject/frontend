c6fcf73a7dfe66f02925a195ad4f3906ff4f40db
Chris Gwilliams <christopher.gwilliams@psi.ch>
2017-10-16 11:22:46 +0200

    added subscription handling for app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { Subscription } from 'rxjs/Subscription';
-import { LoopBackConfig } from 'shared/sdk';
-import { UserApi } from 'shared/sdk/services';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {Store} from '@ngrx/store';
+import {Subscription} from 'rxjs/Subscription';
+import {LoopBackConfig} from 'shared/sdk';
+import {UserApi} from 'shared/sdk/services';
 import * as dsa from 'state-management/actions/datasets.actions';
 import * as ua from 'state-management/actions/user.actions';
 
-import { environment } from '../environments/environment';
+import {environment} from '../environments/environment';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [UserApi]
+  selector : 'app-root',
+  templateUrl : './app.component.html',
+  styleUrls : [ './app.component.css' ],
+  providers : [ UserApi ]
 })
 export class AppComponent implements OnDestroy, OnInit {
   title = 'CATANIE';
@@ -22,38 +22,34 @@ export class AppComponent implements OnDestroy, OnInit {
   message$ = null;
   msgClass$ = null;
 
+  subscriptions = [];
+
   constructor(private router: Router, private store: Store<any>) {
     LoopBackConfig.setBaseURL(environment.lbBaseURL);
     console.log(LoopBackConfig.getPath());
     localStorage.clear();
     if (window.location.pathname.indexOf('logout') !== -1) {
       this.logout();
-      this.router.navigate(['/login']);
+      this.router.navigate([ '/login' ]);
     }
     this.message$ = this.store.select(state => state.root.user.message.content);
     this.msgClass$ = this.store.select(state => state.root.user.message.class);
-    this.store
-      .select(state => state.root.user.currentUser)
-      .subscribe(current => {
-        if (current && current['username']) {
-          this.username = current['username'].replace('ms-ad.', '');
-          if (!('realm' in current)) {
-            this.store.dispatch({
-              type: dsa.ADD_GROUPS,
-              payload: this.username
-            });
-            this.store.dispatch({
-              type: ua.ACCESS_USER_EMAIL,
-              payload: this.username
-            });
+    this.subscriptions.push(this.store.select(state => state.root.user.currentUser)
+        .subscribe(current => {
+          if (current && current['username']) {
+            this.username = current['username'].replace('ms-ad.', '');
+            if (!('realm' in current)) {
+              this.store.dispatch(
+                  {type : dsa.ADD_GROUPS, payload : this.username});
+              this.store.dispatch(
+                  {type : ua.ACCESS_USER_EMAIL, payload : this.username});
+            }
+          } else if (current && current['loggedOut']) {
+            if (window.location.pathname.indexOf('login') === -1) {
+              window.location.replace('/login');
+            }
           }
-        } else if (current && current['loggedOut']) {
-          if (window.location.pathname.indexOf('login') === -1) {
-            window.location.replace('/login');
-          }
-        }
-      });  
-    
+        }));
   }
 
   /**
@@ -61,19 +57,15 @@ export class AppComponent implements OnDestroy, OnInit {
    * auth service (loopback does not by default)
    * @memberof AppComponent
    */
-  ngOnInit() {
-    this.store.dispatch({ type: ua.RETRIEVE_USER });
-  }
+  ngOnInit() { this.store.dispatch({type : ua.RETRIEVE_USER}); }
 
   ngOnDestroy() {
-    // unsubscribe to ensure no memory leaks
+    for (let i = 0; i < this.subscriptions.length; i++) {
+      this.subscriptions[i].unsubscribe();
+    }
   }
 
-  logout() {
-    this.store.dispatch({ type: ua.LOGOUT });
-  }
+  logout() { this.store.dispatch({type : ua.LOGOUT}); }
 
-  login() {
-    this.router.navigateByUrl('/login');
-  }
+  login() { this.router.navigateByUrl('/login'); }
 }
