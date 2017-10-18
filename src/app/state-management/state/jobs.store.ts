fa9f15b445e93b2a1750ebb68c3b8587311e5b92
Gareth Murphy <garethcmurphy@gmail.com>
2017-10-18 12:11:16 +0200

    Adding skip function to store

--- src/app/state-management/state/jobs.store.ts
+++ src/app/state-management/state/jobs.store.ts
@@ -3,11 +3,13 @@ import * as lb from 'shared/sdk/models';
 export interface JobsState {
   currentJobs: lb.Job[];
   jobSubmission: lb.Job[];
+  skip: any;
   ui: any;
   }
 
 export const initialJobsState: JobsState = {
   currentJobs : [],
   jobSubmission : undefined,
+  skip: 0,
   ui: []
 };
