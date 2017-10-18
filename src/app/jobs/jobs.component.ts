d6152d64aa5b63783d47130490be6d5999fe399b
Gareth Murphy <garethcmurphy@gmail.com>
2017-10-18 11:19:32 +0200

    reverting to master version

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -1,46 +1,53 @@
-import {Component, OnInit} from '@angular/core';
-import {Store} from '@ngrx/store';
-import * as JobActions from 'state-management/actions/jobs.actions';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Store } from "@ngrx/store";
+import * as JobActions from "state-management/actions/jobs.actions";
 
 @Component({
-  selector: 'app-jobs',
-  templateUrl: './jobs.component.html',
-  styleUrls: ['./jobs.component.css']
+  selector: "app-jobs",
+  templateUrl: "./jobs.component.html",
+  styleUrls: ["./jobs.component.css"]
 })
-export class JobsComponent implements OnInit {
-
+export class JobsComponent implements OnInit, OnDestroy {
   jobs = [];
 
   cols = [
-    {field: 'creationTime', header: 'Creation Time', sortable: true},
-    {field: 'emailJobInitiator', header: 'Email', sortable: true},
-    {field: 'type', header: 'Type', sortable: true},
-    {field: 'jobStatusMessage', header: 'Status', sortable: true}
+    { field: "creationTime", header: "Creation Time", sortable: true },
+    { field: "emailJobInitiator", header: "Email", sortable: true },
+    { field: "type", header: "Type", sortable: true },
+    { field: "jobStatusMessage", header: "Status", sortable: true }
   ];
 
+  subscriptions = [];
+
   constructor(private store: Store<any>) {
-    const jobsTree = {'data': []};
-    this.store.select(state => state.root.jobs.currentJobs).subscribe(jobs => {
-      let index = -1;
-      jobs.map(job => {
-        const entry = {
-          'data': {
-            'creationTime': job['creationTime'],
-            'emailJobInitiator': job['emailJobInitiator'],
-            'type': job['type'],
-            'jobStatusMessage': job['jobStatusMessage'],
-            'datasetList': job['datasetList'],
-            'index': index += 1
-          },
-          'leaf': false
-        };
-        this.jobs.push(entry);
-      });
-    });
+    const jobsTree = { data: [] };
   }
 
   ngOnInit() {
-    this.store.dispatch({type: JobActions.RETRIEVE});
+    this.subscriptions.push(this.store.select(state => state.root.jobs.currentJobs).subscribe(jobs => {
+      let index = -1;
+      jobs.map(job => {
+        const entry = {
+          data: {
+            creationTime: job["creationTime"],
+            emailJobInitiator: job["emailJobInitiator"],
+            type: job["type"],
+            jobStatusMessage: job["jobStatusMessage"],
+            datasetList: job["datasetList"],
+            index: (index += 1)
+          },
+          leaf: false
+        };
+        this.jobs.push(entry);
+      });
+    }));
+    this.store.dispatch({ type: JobActions.RETRIEVE });
+  }
+
+  ngOnDestroy() {
+    for (let i = 0; i < this.subscriptions.length; i++) {
+      this.subscriptions[i].unsubscribe();
+    }
   }
 
   onRowSelect(event) {
@@ -48,12 +55,17 @@ export class JobsComponent implements OnInit {
   }
 
   nodeExpand(event) {
-    this.store.dispatch({type: JobActions.CHILD_RETRIEVE, payload: event.node});
-    event.node.children = [];
-    this.store.select(state => state.root.jobs.ui).take(1).subscribe(jobs => {
-      console.log(jobs);
-      event.node.children = jobs;
+    this.store.dispatch({
+      type: JobActions.CHILD_RETRIEVE,
+      payload: event.node
     });
+    event.node.children = [];
+    this.store
+      .select(state => state.root.jobs.ui)
+      .take(1)
+      .subscribe(jobs => {
+        console.log(jobs);
+        event.node.children = jobs;
+      });
   }
-
 }
