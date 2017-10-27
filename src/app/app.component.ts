4b74dbf10c2386ae6f3851c9344cca6f9f22d7c6
Gareth Murphy <garethcmurphy@gmail.com>
2017-10-27 11:52:56 +0200

    Adding template for notifications and generic create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,21 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {Store} from '@ngrx/store';
-import {Subscription} from 'rxjs/Subscription';
 import {LoopBackConfig} from 'shared/sdk';
 import {UserApi} from 'shared/sdk/services';
 import * as dsa from 'state-management/actions/datasets.actions';
 import * as ua from 'state-management/actions/user.actions';
 
+import {NotificationsService} from 'angular2-notifications';
+
 import {environment} from '../environments/environment';
 
 @Component({
-  selector : 'app-root',
-  templateUrl : './app.component.html',
-  styleUrls : [ './app.component.css' ],
-  providers : [ UserApi ]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  // template: '<simple-notifications [options]="options"></simple-notifications>',
+  providers: [UserApi]
 })
 export class AppComponent implements OnDestroy, OnInit {
   title = 'CATANIE';
@@ -21,10 +23,11 @@ export class AppComponent implements OnDestroy, OnInit {
   username: string = null;
   message$ = null;
   msgClass$ = null;
-
   subscriptions = [];
 
-  constructor(private router: Router, private store: Store<any>) {
+  constructor(private router: Router,
+              private _service: NotificationsService,
+              private store: Store<any>) {
   }
 
   /**
@@ -32,34 +35,50 @@ export class AppComponent implements OnDestroy, OnInit {
    * auth service (loopback does not by default)
    * @memberof AppComponent
    */
+
+
+  create() {
+    this._service.success(
+      'Some Title',
+      'Some Content',
+      {
+        showProgressBar: true,
+        pauseOnHover: false,
+        clickToClose: false,
+        maxLength: 10
+      }
+    )
+  };
+
   ngOnInit() {
+
     LoopBackConfig.setBaseURL(environment.lbBaseURL);
     console.log(LoopBackConfig.getPath());
     localStorage.clear();
     if (window.location.pathname.indexOf('logout') !== -1) {
       this.logout();
-      this.router.navigate([ '/login' ]);
+      this.router.navigate(['/login']);
     }
     this.message$ = this.store.select(state => state.root.user.message.content);
     this.msgClass$ = this.store.select(state => state.root.user.message.class);
     this.subscriptions.push(this.store.select(state => state.root.user.currentUser)
-        .subscribe(current => {
-          if (current && current['username']) {
-            this.username = current['username'].replace('ms-ad.', '');
-            if (!('realm' in current)) {
-              this.store.dispatch(
-                  {type : dsa.ADD_GROUPS, payload : this.username});
-              this.store.dispatch(
-                  {type : ua.ACCESS_USER_EMAIL, payload : this.username});
-            }
-          } else if (current && current['loggedOut']) {
-            if (window.location.pathname.indexOf('login') === -1) {
-              window.location.replace('/login');
-            }
+      .subscribe(current => {
+        if (current && current['username']) {
+          this.username = current['username'].replace('ms-ad.', '');
+          if (!('realm' in current)) {
+            this.store.dispatch(
+              {type: dsa.ADD_GROUPS, payload: this.username});
+            this.store.dispatch(
+              {type: ua.ACCESS_USER_EMAIL, payload: this.username});
           }
-        }));
-    this.store.dispatch({type : ua.RETRIEVE_USER});
- }
+        } else if (current && current['loggedOut']) {
+          if (window.location.pathname.indexOf('login') === -1) {
+            window.location.replace('/login');
+          }
+        }
+      }));
+    this.store.dispatch({type: ua.RETRIEVE_USER});
+  }
 
   ngOnDestroy() {
     for (let i = 0; i < this.subscriptions.length; i++) {
@@ -67,7 +86,11 @@ export class AppComponent implements OnDestroy, OnInit {
     }
   }
 
-  logout() { this.store.dispatch({type : ua.LOGOUT}); }
+  logout() {
+    this.store.dispatch({type: ua.LOGOUT});
+  }
 
-  login() { this.router.navigateByUrl('/login'); }
+  login() {
+    this.router.navigateByUrl('/login');
+  }
 }
