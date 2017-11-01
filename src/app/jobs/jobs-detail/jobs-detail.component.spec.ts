fff91796e913e1c6165dc2dec3bcfa68f3aa2999
Gareth Murphy <garethcmurphy@gmail.com>
2017-11-01 12:03:02 +0100

    Fixing unit test failures to include mock routing services

--- src/app/jobs/jobs-detail/jobs-detail.component.spec.ts
+++ src/app/jobs/jobs-detail/jobs-detail.component.spec.ts
@@ -1,11 +1,13 @@
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
-import { Store, StoreModule } from '@ngrx/store';
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ReactiveFormsModule} from '@angular/forms';
+import {Store, StoreModule} from '@ngrx/store';
+import {MockActivatedRoute, MockStore} from '../../shared/MockStubs';
 
 
-import { JobsDetailComponent } from './jobs-detail.component';
-import { rootReducer } from 'state-management/reducers/root.reducer';
+import {JobsDetailComponent} from './jobs-detail.component';
+import {rootReducer} from 'state-management/reducers/root.reducer';
+import {ActivatedRoute} from '@angular/router';
 
 describe('JobsDetailComponent', () => {
   let component: JobsDetailComponent;
@@ -13,11 +15,19 @@ describe('JobsDetailComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      schemas : [ NO_ERRORS_SCHEMA ],
-      imports : [ ReactiveFormsModule, StoreModule.forRoot({rootReducer}) ],
-      declarations: [ JobsDetailComponent ]
-    })
-    .compileComponents();
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [ReactiveFormsModule, StoreModule.forRoot({rootReducer})],
+      declarations: [JobsDetailComponent]
+    });
+    TestBed.overrideComponent(JobsDetailComponent, {
+      set: {
+        providers: [
+          {provide: Store, useClass: MockStore},
+          {provide: ActivatedRoute, useClass: MockActivatedRoute}
+        ]
+      }
+    });
+    TestBed.compileComponents();
   }));
 
   beforeEach(() => {
