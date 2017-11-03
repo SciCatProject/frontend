bdae324f5564a80d800febe59e74639e718a4b13
encima <encima@gmail.com>
2017-11-03 11:12:14 +0100

    updated deplot script to allow individual deployments to envs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,12 +11,10 @@ for ((i=0;i<${#envarray[@]};i++)); do
    cd $DACATHOME/catanie 
    export CATANIE_IMAGE_VERSION=$(git rev-parse HEAD)
    export LOCAL_ENV="${envarray[i]}"
-   echo $LOCAL_ENV 
-   echo pwd
-   echo "Building release"
    read -r -p "Deploy to $LOCAL_ENV? [y/N] " response
    if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
    then
+      echo "Building release"
       ./node_modules/@angular/cli/bin/ng build --environment $LOCAL_ENV -op dist/$LOCAL_ENV
       docker build -t registry.psi.ch:5000/egli/catanie:$CATANIE_IMAGE_VERSION$LOCAL_ENV --build-arg env=$LOCAL_ENV .
       docker push registry.psi.ch:5000/egli/catanie:$CATANIE_IMAGE_VERSION$LOCAL_ENV
@@ -25,6 +23,6 @@ for ((i=0;i<${#envarray[@]};i++)); do
       helm del --purge catanie-$LOCAL_ENV
       helm install dacat-gui --name catanie-$LOCAL_ENV --namespace $LOCAL_ENV --set image.tag=$CATANIE_IMAGE_VERSION$LOCAL_ENV
    else
-      break
+      continue
    fi
 done
