fbaa32b4d723ad2376bec958ebf4bd02c55c0b43
encima <encima@gmail.com>
2018-01-10 10:01:28 +0100

    updated protractor tests to headless driver for jenkins

--- protractor.conf.js
+++ protractor.conf.js
@@ -13,8 +13,8 @@ exports.config = {
         'browserName': 'chrome',
         // chromeDriver: '../node_modules/protractor/node_modules/webdriver-manager/selenium/chromedriver_2.30',
         chromeOptions: {
-            // args: ["--headless", "--disable-gpu", "--no-sandbox"]
-           args: ["--window-size=800x600"]
+            args: ["--headless", "--disable-gpu", "--no-sandbox"]
+        //    args: ["--window-size=800x600"]
         }
 
     },
