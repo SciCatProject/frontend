1a683f85cca6b8799181c98e30ca274b5fdd2c43
encima <encima@gmail.com>
2018-02-12 15:40:18 +0100

    added unit tests for dataset-filters

--- src/app/datasets/datasets-filter/datasets-filter.component.spec.ts
+++ src/app/datasets/datasets-filter/datasets-filter.component.spec.ts
@@ -1,12 +1,13 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Store, StoreModule } from '@ngrx/store';
 import { AutoCompleteModule, DataTableModule, SharedModule, TreeModule } from 'primeng/primeng';
 import { rootReducer } from 'state-management/reducers/root.reducer';
 import { DatasetsFilterComponent } from 'datasets/datasets-filter/datasets-filter.component';
-import {MockStore, MockActivatedRoute, MockRouter} from '../../shared/MockStubs';
-import {ActivatedRoute, Router} from '@angular/router';
-import {MatAutocompleteModule} from '@angular/material';
+import { MockStore, MockActivatedRoute, MockRouter } from '../../shared/MockStubs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatAutocompleteModule } from '@angular/material';
 
 describe('DatasetsFilterComponent', () => {
   let component: DatasetsFilterComponent;
@@ -14,23 +15,23 @@ describe('DatasetsFilterComponent', () => {
 
   beforeEach(async(() => {
     TestBed
-        .configureTestingModule({
-          schemas : [ NO_ERRORS_SCHEMA ],
-          imports : [
-            TreeModule, MatAutocompleteModule, SharedModule, DataTableModule,
-            StoreModule.forRoot({rootReducer})
-          ],
-          declarations : [ DatasetsFilterComponent ],
-          providers : [
-            {provide : Store, useClass : MockStore},
-          ]
-        });
+      .configureTestingModule({
+        schemas: [NO_ERRORS_SCHEMA],
+        imports: [
+          TreeModule, MatAutocompleteModule, SharedModule, DataTableModule,
+          StoreModule.forRoot({ rootReducer })
+        ],
+        declarations: [DatasetsFilterComponent],
+        providers: [
+          { provide: Store, useClass: MockStore },
+        ]
+      });
     TestBed.overrideComponent(DatasetsFilterComponent, {
       set: {
         providers: [
-          {provide: Store, useClass: MockStore},
-          {provide: Router, useClass: MockRouter},
-          {provide: ActivatedRoute, useClass: MockActivatedRoute}
+          { provide: Store, useClass: MockStore },
+          { provide: Router, useClass: MockRouter },
+          { provide: ActivatedRoute, useClass: MockActivatedRoute }
         ]
       }
     });
@@ -46,7 +47,25 @@ describe('DatasetsFilterComponent', () => {
   it('should be created', () => { expect(component).toBeTruthy(); });
 
   it('should contain a date range field', () => {
-      const compiled = fixture.debugElement.nativeElement;
-      expect(compiled.querySelector('form').textContent).toContain('Date Range');
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('form').textContent).toContain('Date Range');
+  });
+
+  it('should contain a beamline input', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    const beamline = compiled.querySelector('.beamline-input')
+    expect(beamline.childNodes[1].attributes.getNamedItem('placeholder').textContent).toContain('Beamline');
+  });
+
+  it('should contain a groups input', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    const group = compiled.querySelector('.group-input')
+    expect(group.childNodes[1].attributes.getNamedItem('placeholder').textContent).toContain('Group');
+  });
+
+  it('should contain a clear button', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    const btn = compiled.querySelector('.clear-filters-btn')
+    expect(btn.textContent).toContain('Clear');
   });
 });
