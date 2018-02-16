21268321766ac6b27e2ae048d176e72182837488
encima <encima@gmail.com>
2018-02-16 17:00:41 +0100

    removed dataset detail test

--- e2e/dataset/dataset.e2e-spec.ts
+++ e2e/dataset/dataset.e2e-spec.ts
@@ -7,19 +7,22 @@ describe('catanie Dataset Detail', function() {
   let lp: LoginPage;
   let page: DatasetDetailPage;
 
-  beforeAll(() => {
-    lp = new LoginPage();
-    lp.navigateTo().then(() => {
-      lp.enterDetails('ingestor', 'aman');
-      lp.login();
-      browser.sleep(2000);
-      page = new DatasetDetailPage();
-      page.navigateTo();
-    });
-  });
+  // beforeAll(() => {
+  //   lp = new LoginPage();
+  //   lp.navigateTo().then(() => {
+  //     lp.enterDetails('ingestor', 'aman');
+  //     lp.login();
+  //     browser.sleep(2000);
+  //     page = new DatasetDetailPage();
+  //     page.navigateTo();
+  //   });
+  // });
 
-  it('should be on correct page', () => {
-    expect(browser.getCurrentUrl()).toContain('dataset/' + browser.params.pid);
-  });
+  // it('should be on correct page', () => {
+  //   expect(browser.getCurrentUrl()).toContain('dataset/' + browser.params.pid);
+  // });
+
+  // TODO create a test that clicks on an entry in the dataset table or queries for a valid ID first
+  // to make it site independent
 
 });
