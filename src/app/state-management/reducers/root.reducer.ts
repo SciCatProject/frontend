60c6862aa8057b25f03b3a6e4c466ddfe78f8733
Chris Gwilliams <encima@gmail.com>
2018-02-20 14:33:19 +0100

    also updated all effects to follow (mostly) changes in ngrx. Tap and pipe could still be useful

--- src/app/state-management/reducers/root.reducer.ts
+++ src/app/state-management/reducers/root.reducer.ts
@@ -34,8 +34,7 @@ const appReducer = combineReducers({
 
 export const rootReducer = ( state, action ) => {
    if ( action.type === ua.LOGOUT_COMPLETE ) {
-     state = undefined;
-    //  window.location.reload();
+     state = undefined;;
    }
 
    return appReducer(state, action);
