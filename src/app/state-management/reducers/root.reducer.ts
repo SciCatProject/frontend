13879307034e02edeab16d5f382efedaf223463b
Hannes Petri <hannes.petri@maxiv.lu.se>
2018-03-20 14:07:01 +0100

    Added proposal reducer to root reducer

--- src/app/state-management/reducers/root.reducer.ts
+++ src/app/state-management/reducers/root.reducer.ts
@@ -1,43 +1,49 @@
 import { Action, combineReducers } from '@ngrx/store';
+
 import { dashboardUIReducer } from 'state-management/reducers/dashboard-ui.reducer';
 import { datasetsReducer } from 'state-management/reducers/datasets.reducer';
 import { jobsReducer } from 'state-management/reducers/jobs.reducer';
 import { userReducer } from 'state-management/reducers/user.reducer';
+import { proposalsReducer } from 'state-management/reducers/proposals.reducer';
+
 import { DashboardUIState, initialDashboardUIState } from 'state-management/state/dashboard-ui.store';
 import { DatasetState, initialDatasetState } from 'state-management/state/datasets.store';
 import { initialJobsState, JobsState } from 'state-management/state/jobs.store';
 import { initialUserState, UserState } from 'state-management/state/user.store';
+import { initialProposalState, ProposalState } from 'state-management/state/proposal.store';
+
 import * as ua from 'state-management/actions/user.actions';
 
-// NOTE It IS ok to make up a state of other sub states
 export interface AppState {
     datasets: DatasetState;
     user: UserState;
     dashboardUI: DashboardUIState;
     jobs: JobsState;
+    proposal: ProposalState;
 }
 
 export const initialState: AppState = {
     datasets: initialDatasetState,
     user: initialUserState,
     dashboardUI: initialDashboardUIState,
-    jobs: initialJobsState
+    jobs: initialJobsState,
+    proposal: initialProposalState,
 };
-
 
 const appReducer = combineReducers({
     user: userReducer,
     datasets: datasetsReducer,
     dashboardUI: dashboardUIReducer,
-    jobs: jobsReducer
+    jobs: jobsReducer,
+    proposal: proposalsReducer, // NEcessary?
  });
 
-export const rootReducer = ( state, action ) => {
-   if ( action.type === ua.LOGOUT_COMPLETE ) {
-     state = undefined;
-   }
+export const rootReducer = (state, action) => {
+    if (action.type === ua.LOGOUT_COMPLETE) {
+        state = undefined;
+    }
 
-   return appReducer(state, action);
+    return appReducer(state, action);
 };
 
 export const getDatasetsState = (state: any) => state.root.datasets;
