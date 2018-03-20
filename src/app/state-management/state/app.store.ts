712d22cb3b9d699e0822b69c372befac34475c19
Hannes Petri <hannes.petri@maxiv.lu.se>
2018-03-20 14:39:39 +0100

    Removed explicit link to proposal state in app state, in favour of feature state

--- src/app/state-management/state/app.store.ts
+++ src/app/state-management/state/app.store.ts
@@ -1,14 +1,12 @@
 import {DatasetState, initialDatasetState} from 'state-management/state/datasets.store';
 import {UserState, initialUserState} from 'state-management/state/user.store';
 import {DashboardUIState, initialDashboardUIState} from 'state-management/state/dashboard-ui.store';
-import {ProposalState, initialProposalState} from 'state-management/state/proposal.store';
 import {JobsState, initialJobsState} from 'state-management/state/jobs.store';
 
 export interface AppState {
     datasets: DatasetState;
     user: UserState;
     dashboardUI: DashboardUIState;
-    proposals: ProposalState;
     jobs: JobsState;
 }
 
@@ -16,6 +14,5 @@ export const initialState: AppState = {
     datasets: initialDatasetState,
     user: initialUserState,
     dashboardUI: initialDashboardUIState,
-    proposals: initialProposalState,
     jobs: initialJobsState,
 };
