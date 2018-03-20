4160eca8a120c5a01b6d2e10fcdba71ca5fb679e
Hannes Petri <hannes.petri@maxiv.lu.se>
2018-03-20 14:06:46 +0100

    Added proposal state to app state

--- src/app/state-management/state/app.store.ts
+++ src/app/state-management/state/app.store.ts
@@ -1,18 +1,21 @@
 import {DatasetState, initialDatasetState} from 'state-management/state/datasets.store';
 import {UserState, initialUserState} from 'state-management/state/user.store';
 import {DashboardUIState, initialDashboardUIState} from 'state-management/state/dashboard-ui.store';
-import {ProposalsState, initialProposalsState} from 'state-management/state/proposals.store';
+import {ProposalState, initialProposalState} from 'state-management/state/proposal.store';
+import {JobsState, initialJobsState} from 'state-management/state/jobs.store';
 
 export interface AppState {
     datasets: DatasetState;
     user: UserState;
     dashboardUI: DashboardUIState;
-    proposals: ProposalsState;
+    proposals: ProposalState;
+    jobs: JobsState;
 }
 
 export const initialState: AppState = {
     datasets: initialDatasetState,
     user: initialUserState,
     dashboardUI: initialDashboardUIState,
-    proposals: initialProposalsState,
+    proposals: initialProposalState,
+    jobs: initialJobsState,
 };
