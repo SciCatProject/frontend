3bad183225238cad13453176d32586987dc7e3ed
Hannes Petri <hannes.petri@maxiv.lu.se>
2018-03-20 14:38:48 +0100

    Added proposal detail component

---
+++ src/app/proposals/proposal-detail/proposal-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Proposal } from 'state-management/models';
+
+@Component({
+	selector: 'proposal-detail',
+	templateUrl: 'proposal-detail.component.html',
+	styleUrls: ['proposal-detail.component.css']
+})
+export class ProposalDetailComponent {
+};
