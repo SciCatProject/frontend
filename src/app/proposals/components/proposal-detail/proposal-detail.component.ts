58677d00da18af9dbdd30ab74bc50f58e97ab3f6
Hannes Petri <hannes.petri@maxiv.lu.se>
2018-03-20 19:02:56 +0100

    Work on proposal components

--- src/app/proposals/components/proposal-detail/proposal-detail.component.ts
+++ src/app/proposals/components/proposal-detail/proposal-detail.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
 import { Proposal } from 'state-management/models';
+/*import { AppState } from 'state-management/state/app.store';
+import { ProposalState } from 'state-management/state/proposal.store';*/
 
 @Component({
 	selector: 'proposal-detail',
@@ -7,4 +14,5 @@ import { Proposal } from 'state-management/models';
 	styleUrls: ['proposal-detail.component.css']
 })
 export class ProposalDetailComponent {
+	@Input() proposal: Proposal;
 };
--- src/app/proposals/components/proposals-list/proposals-list.component.ts
+++ src/app/proposals/components/proposals-list/proposals-list.component.ts
@@ -1,10 +1,4 @@
-import { Component } from '@angular/core';
-import { Store, select } from '@ngrx/store';
-import { Observable } from 'rxjs/Observable';
-
-import { AppState } from 'state-management/state/app.store';
-import { ProposalState } from 'state-management/state/proposal.store';
-
+import { Component, Input } from '@angular/core';
 import { Proposal } from 'state-management/models';
 
 @Component({
@@ -13,12 +7,5 @@ import { Proposal } from 'state-management/models';
 	styleUrls: ['proposals-list.component.css']
 })
 export class ProposalsListComponent {
-	proposals$: Observable<Proposal[]>;
-
-	constructor(private store: Store<AppState>) {
-		this.proposals$ = store.pipe(
-			select('proposals'),
-			select((propState: ProposalState) => propState.list)
-		);
-	}
+	@Input() proposals: Proposal[];
 };
