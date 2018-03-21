373aa091837e1fa4612dcf68afcbdc75c68c8edf
Hannes Petri <hannes.petri@maxiv.lu.se>
2018-03-21 09:56:57 +0100

    Added proposals service

---
+++ src/app/proposals/proposals.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { ProposalApi } from 'shared/sdk/services';
+import { Proposal } from 'shared/sdk/models';
+
+@Injectable()
+export default class ProposalService {
+	constructor(private proposalApi: ProposalApi) {}
+
+	getProposals(): Observable<Proposal[]> {
+		return Observable.of([
+			new Proposal({
+				proposalId: '123456789',
+				email: 'e@mail.com',
+				title: 'My first proposal',
+				abstract: 'Investigate something interesting',
+				ownerGroup: '123456789-group'
+			}),
+			new Proposal({
+				proposalId: '34234234',
+				email: 'sddfs@sdfsd.com',
+				title: 'Another proposal',
+				abstract: 'Another abstract blablabla',
+				ownerGroup: '34234234-group'
+			})
+		]);
+	}
+}
