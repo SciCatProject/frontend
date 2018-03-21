2eda7b3a44161383b95ef1f289c1fcb2d24c5b1c
Hannes Petri <hannes.petri@maxiv.lu.se>
2018-03-21 18:35:01 +0100

    Work on proposal components

--- src/app/proposals/components/proposal-detail/proposal-detail.component.ts
+++ src/app/proposals/components/proposal-detail/proposal-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Store, select } from '@ngrx/store';
@@ -6,11 +6,44 @@ import { Observable } from 'rxjs/Observable';
 
 import { Proposal } from 'state-management/models';
 
+interface Proposer {
+	name: string;
+	email: string;
+	isPresent: boolean;
+}
+
 @Component({
 	selector: 'proposal-detail',
 	templateUrl: 'proposal-detail.component.html',
 	styleUrls: ['proposal-detail.component.css']
 })
-export class ProposalDetailComponent {
-	@Input() proposal: Proposal;
+export class ProposalDetailComponent implements OnInit {
+	@Input() proposal: Proposal;	
+	private mainProposer: Proposer; // kolla private
+	private principalInvestigator: Proposer;
+
+	ngOnInit() {
+		// Set up fallback values for main proposer
+		const {firstname, lastname} = this.proposal;
+		const mpName = firstname && lastname
+			? `${firstname} ${lastname}`
+			: this.proposal.email; // Email is mandatory so we can rely on it being present.
+
+		this.mainProposer = {
+			name: mpName,
+			email: this.proposal.email,
+			isPresent: true
+		};
+
+		// Set up fallback values for principalInvestigator
+		const {pi_firstname, pi_lastname} = this.proposal;
+		const piFullName = pi_firstname && pi_lastname ? `${pi_firstname} ${pi_lastname}` : null;
+		const piEmail = this.proposal.pi_email || null;
+
+		this.principalInvestigator = {
+			name: piFullName || piEmail,
+			email: piEmail,
+			isPresent: piFullName !== null || piEmail !== null
+		}
+	}
 };
