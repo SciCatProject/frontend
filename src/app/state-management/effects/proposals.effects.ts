66ff75f6a721274623d180e6b23392eecfc1b79a
Hannes Petri <hannes.petri@maxiv.lu.se>
2018-03-21 17:07:04 +0100

    Limit with take(1) to eliminate unnecessary calls

--- src/app/state-management/effects/proposals.effects.ts
+++ src/app/state-management/effects/proposals.effects.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Effect, Actions, ofType } from '@ngrx/effects';
 
 import { Observable } from 'rxjs/Observable';
-import { map, mergeMap, catchError } from 'rxjs/operators';
+import { map, mergeMap, take, catchError } from 'rxjs/operators';
 
 import { ProposalsService } from 'proposals/proposals.service';
 
@@ -23,6 +23,7 @@ import { Proposal } from '../models';
 export class ProposalsEffects {
 	@Effect() getProposals$: Observable<GetProposalsOutcomeAction> = this.actions$.pipe(
 		ofType(GET_PROPOSALS),
+		take(1),
 		mergeMap(action => 
 			this.proposalsService.getProposals().pipe(
 				map(proposals => new GetProposalsCompleteAction(proposals)),
@@ -30,12 +31,7 @@ export class ProposalsEffects {
 			)
 		)
 	);
-/*
-	@Effect() loginComplete$: Observable<ProposalsAction> = this.actions$.pipe(
-		ofType(LOGIN_COMPLETE),
-		map(action => new GetProposalsAction())
-	)
-*/
+
 	constructor(
 		private actions$: Actions,
 		private proposalsService: ProposalsService,
