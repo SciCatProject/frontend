b8a8ba97e76f564f415404fde03537dc39a38649
Hannes Petri <hannes.petri@maxiv.lu.se>
2018-03-22 10:58:48 +0100

    Using the API to fetch proposals

--- src/app/proposals/proposals.service.ts
+++ src/app/proposals/proposals.service.ts
@@ -9,21 +9,6 @@ export class ProposalsService {
 	constructor(private proposalApi: ProposalApi) {}
 
 	getProposals(): Observable<Proposal[]> {
-		return Observable.of([
-			new Proposal({
-				proposalId: '123456789',
-				email: 'e@mail.com',
-				title: 'My first proposal',
-				abstract: 'Investigate something interesting',
-				ownerGroup: '123456789-group'
-			}),
-			new Proposal({
-				proposalId: '34234234',
-				email: 'sddfs@sdfsd.com',
-				title: 'Another proposal',
-				abstract: 'Another abstract blablabla',
-				ownerGroup: '34234234-group'
-			})
-		]);
+		return this.proposalApi.find();
 	}
 }
