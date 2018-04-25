a8c0c3e615d0151b2a4ecb8aa3a87b8a7010dd31
Gareth Murphy <garethcmurphy@gmail.com>
2018-04-25 11:33:36 +0200

    add filesever rto dmsc env

--- src/environments/environment.dmsc.ts
+++ src/environments/environment.dmsc.ts
@@ -6,5 +6,6 @@
 export const environment = {
   production: false,
   lbBaseURL: 'https://kubetest02.dm.esss.dk:32223',
+  fileserverBaseURL: 'https://kubetest04.dm.esss.dk:32223',
   externalAuthEndpoint: '/auth/msad'
 };
