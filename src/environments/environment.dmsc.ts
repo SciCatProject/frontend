d9230481a6c0851a6d191fde2613f3f0ccf3ed21
Gareth Murphy <garethcmurphy@gmail.com>
2018-04-25 11:12:56 +0200

    update for TLS

--- src/environments/environment.dmsc.ts
+++ src/environments/environment.dmsc.ts
@@ -5,6 +5,6 @@
 
 export const environment = {
   production: false,
-  lbBaseURL: 'http://kubetest02.dm.esss.dk:32222',
+  lbBaseURL: 'https://kubetest02.dm.esss.dk:32223',
   externalAuthEndpoint: '/auth/msad'
 };
