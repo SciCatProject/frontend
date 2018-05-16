530942eae318c5d50fb4dda186277873ffd2ab75
Gareth Murphy <garethcmurphy@gmail.com>
2018-05-16 06:05:58 +0000

    add  filter update test

--- src/app/state-management/reducers/datasets.reducer.spec.ts
+++ src/app/state-management/reducers/datasets.reducer.spec.ts
@@ -5,16 +5,30 @@ import { Dataset } from 'shared/sdk/models';
 import { DatasetState, initialDatasetState } from 'state-management/state/datasets.store';
 
 describe('DatasetsReducer', () => {
+
   describe('search complete action', () => {
     it('should return the state loading', () => {
 	  const payload = [new Dataset()];
       const action = new fromActions.SearchCompleteAction(payload);
       const state = fromDatasets.datasetsReducer(initialDatasetState, action);
-
 	  expect(state.loading).toEqual(false);
-
     });
   });
+  
+
+
   // I nest all tests under the reducer's name 
   // for readability in the terminal
+
+
+  describe('search complete action', () => {
+    it('should return the state loading', () => {
+	  const payload = [new Dataset()];
+      const action = new fromActions.FilterValueAction(payload);
+      const state = fromDatasets.datasetsReducer(initialDatasetState, action);
+	  expect(state.loading).toEqual(true);
+    });
+  });
+  
+
 });
