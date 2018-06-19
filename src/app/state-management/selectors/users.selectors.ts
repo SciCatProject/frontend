027169f26742ff9c78a5d9ab6697dd023cd5ed2a
Hannes Petri <hannes.petri@maxiv.lu.se>
2018-06-19 12:00:00 +0200

    Work on user state management

--- src/app/state-management/selectors/users.selectors.ts
+++ src/app/state-management/selectors/users.selectors.ts
@@ -1,7 +1,43 @@
-import {createSelector, Store} from '@ngrx/store';
+import { createSelector, createFeatureSelector } from '@ngrx/store';
+import { UserState } from '../state/user.store';
+
+const getUserState = createFeatureSelector<UserState>('users');
+
+export const getCurrentUser = createSelector(
+    getUserState,
+    state => state.currentUser || null
+);
+
+export const getIsLoggedIn = createSelector(
+    getCurrentUser,
+    user => user !== null
+);
+
+const getCurrentUserName = createSelector(
+    getCurrentUser,
+    user => user ? user.username : null
+);
+
+const getCurrentUserAccountType = createSelector(
+    getCurrentUser,
+    user => user ? user['accountType'] : null // ??? Why doesn't the User class have an accountType?
+);
+
+export const getIsAdmin = createSelector(
+    getCurrentUserName,
+    getCurrentUserAccountType,
+    (name, type) => 
+        name && ['ingestor', 'admin', 'archiveManager'].indexOf(name) !== -1 ||
+        type && type === 'functional'
+)
+
+export const getIsLoading = createSelector(
+    getUserState,
+    state => state.loading
+);
 
 export const getState = (state: any) => state.root.user;
-export const getCurrentUser = (state: any) => state.root.user.currentUser;
+//export const getCurrentUser = (state: any) => state.root.user.currentUser;
 export const getLoading = (state: any) => state.root.user.loading;
 export const getCurrentUserGroups = (state: any) => state.root.user.currentUserGroups;
 export const getSettings = (state: any) => state.root.user.settings;
