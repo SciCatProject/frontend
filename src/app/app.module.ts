9bc258b06ff52056ff44dc7a58cae35d0ac41bca
Gareth Murphy <garethcmurphy@gmail.com>
2018-06-21 10:29:57 +0200

    Import MatNativeDateModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,35 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { EffectsModule } from '@ngrx/effects';
-import { routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { NguiDatetimePickerModule } from '@ngui/datetime-picker';
-import { AppRoutingModule, routes } from 'app-routing/app-routing.module';
-import { DatasetService } from 'datasets/dataset.service';
-import { DatasetsModule } from 'datasets/datasets.module';
-import { JobsTableComponent } from 'jobs/jobs-table/jobs-table.component';
-import { localStorageSync } from 'ngrx-store-localstorage';
-import { AppConfigModule } from 'app-config.module';
-import { SampleDataFormComponent } from 'sample-data-form/sample-data-form.component';
-import { SDKBrowserModule } from 'shared/sdk/index';
-import { UserApi } from 'shared/sdk/services';
-import { SharedCatanieModule } from 'shared/shared.module';
-import { DatasetEffects } from 'state-management/effects/datasets.effects';
-import { UserEffects } from 'state-management/effects/user.effects';
-import { JobsEffects } from 'state-management/effects/jobs.effects';
-import { rootReducer } from 'state-management/reducers/root.reducer';
-import { UsersModule } from 'users/users.module';
-import { ProposalsModule } from 'proposals/proposals.module';
-import { FileHelpersModule } from 'ngx-file-helpers';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {EffectsModule} from '@ngrx/effects';
+import {routerReducer, StoreRouterConnectingModule} from '@ngrx/router-store';
+import {StoreModule} from '@ngrx/store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {NguiDatetimePickerModule} from '@ngui/datetime-picker';
+import {AppRoutingModule, routes} from 'app-routing/app-routing.module';
+import {DatasetService} from 'datasets/dataset.service';
+import {DatasetsModule} from 'datasets/datasets.module';
+import {JobsTableComponent} from 'jobs/jobs-table/jobs-table.component';
+import {localStorageSync} from 'ngrx-store-localstorage';
+import {AppConfigModule} from 'app-config.module';
+import {SampleDataFormComponent} from 'sample-data-form/sample-data-form.component';
+import {SDKBrowserModule} from 'shared/sdk/index';
+import {UserApi} from 'shared/sdk/services';
+import {SharedCatanieModule} from 'shared/shared.module';
+import {DatasetEffects} from 'state-management/effects/datasets.effects';
+import {UserEffects} from 'state-management/effects/user.effects';
+import {JobsEffects} from 'state-management/effects/jobs.effects';
+import {rootReducer} from 'state-management/reducers/root.reducer';
+import {UsersModule} from 'users/users.module';
+import {ProposalsModule} from 'proposals/proposals.module';
+import {FileHelpersModule} from 'ngx-file-helpers';
 
-import { FilePickerComponent } from './datasets/file-picker/file-picker.component';
-import { FileDropzoneComponent } from './datasets/file-dropzone/file-dropzone.component';
+import {FilePickerComponent} from './datasets/file-picker/file-picker.component';
+import {FileDropzoneComponent} from './datasets/file-dropzone/file-dropzone.component';
+import {DateAdapter} from '@angular/material';
 
 
 import {
@@ -67,40 +68,40 @@ import {
 } from '@angular/material';
 
 import {CdkTableModule} from '@angular/cdk/table';
-import { AppComponent } from './app.component';
-import { AuthCheck } from './AuthCheck';
-import { JobsDetailComponent } from './jobs/jobs-detail/jobs-detail.component';
-
+import {AppComponent} from './app.component';
+import {AuthCheck} from './AuthCheck';
+import {JobsDetailComponent} from './jobs/jobs-detail/jobs-detail.component';
 
 
 export function localStorageSyncWrapper(reducer: any) {
-  return localStorageSync({keys: ['root'], rehydrate: true}) (reducer);
+  return localStorageSync({keys: ['root'], rehydrate: true})(reducer);
 }
 
 @NgModule({
-  declarations : [
+  declarations: [
     AppComponent,
     JobsTableComponent,
     SampleDataFormComponent,
     JobsDetailComponent,
   ],
-  imports : [
-    MatToolbarModule,
-    MatIconModule,
-    MatSnackBarModule,
-    MatCardModule,
-    MatListModule,
-    MatMenuModule,
-    MatSidenavModule,
-    MatTableModule,
-    MatPaginatorModule,
+  imports: [
+    AppRoutingModule,
+    BrowserAnimationsModule, SharedCatanieModule,
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
-    AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule, SharedCatanieModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatSidenavModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatToolbarModule,
     NguiDatetimePickerModule,
+    ReactiveFormsModule,
 
     DatasetsModule,
     UsersModule,
@@ -110,22 +111,22 @@ export function localStorageSyncWrapper(reducer: any) {
     SDKBrowserModule.forRoot(),
     // StoreModule.forRoot({router: routerReducer, root: rootReducer}, {metaReducers: [localStorageSyncWrapper]}),
     StoreModule.forRoot({router: routerReducer, root: rootReducer}),
-    RouterModule.forRoot(routes, { useHash: false }),
+    RouterModule.forRoot(routes, {useHash: false}),
     StoreDevtoolsModule.instrument({
       maxAge: 25 //  Retains last 25 states
     }),
     EffectsModule.forRoot([DatasetEffects, UserEffects, JobsEffects]),
     StoreRouterConnectingModule,
   ],
-  exports: [
-  ],
-  providers : [
-      AuthCheck,
-      DatasetService,
-      UserApi,
+  exports: [],
+  providers: [
+    AuthCheck,
+    DatasetService,
+    UserApi,
+    MatNativeDateModule,
     //      {provide: RouteReuseStrategy, useClass: CustomReuseStrategy}
   ],
-  bootstrap : [ AppComponent ]
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
