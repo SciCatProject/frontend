c29f24323aa0f329b8dc34dcf40b04c63ce78a86
Gareth Murphy <garethcmurphy@gmail.com>
2018-08-09 12:00:24 +0200

    change title to facility (see Issue #47)

--- src/environments/environment.dmscprod.ts
+++ src/environments/environment.dmscprod.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   lbBaseURL: 'https://scicatapi.esss.dk',
   fileserverBaseURL: 'https://scicatfileserver.esss.dk',
   externalAuthEndpoint: '/auth/msad',
