5177e941ccebb4707ca6478ee12c6b1d49b8950d
Gareth Murphy <garethcmurphy@gmail.com>
2018-08-13 13:00:43 +0200

    fix e2e not running - see Issue #53

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -5,34 +5,34 @@
 var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 
 exports.config = {
-    allScriptsTimeout: 15000,
-    specs: [
-      './**/*.e2e-spec.ts'
-    ],
-    capabilities: {
-        'browserName': 'chrome',
-
+  allScriptsTimeout: 15000,
+  specs: [
+    './**/*.e2e-spec.ts'
+  ],
+  capabilities: {
+    'browserName': 'chrome'
+  },
+  params: {
+    login: {
+      user: 'ingestor',
+      pwd: 'aman'
     },
-    params: {
-        login: {
-            user: 'ingestor',
-            pwd: 'aman'
-        },
-        pid: '20.500.11935%2F0006c3e0-d1fa-440c-8ed0-431a9636f9bc'
-    },
-    directConnect: true,
-    getPageTimeout: 15000,
-    baseUrl: 'http://localhost:4200/',
-    framework: 'jasmine',
-    jasmineNodeOpts: {
-        showColors: true,
-        defaultTimeoutInterval: 30000,
-        print: function () {}
-    },
-    onPrepare () {
-        require('ts-node').register({
-            project: 'e2e/tsconfig.e2e.json'
-      });
-      jasmine.getEnv().addReporter(new SpecReporter());
+    pid: '20.500.11935%2F0006c3e0-d1fa-440c-8ed0-431a9636f9bc'
+  },
+  directConnect: true,
+  getPageTimeout: 15000,
+  baseUrl: 'http://localhost:4200/',
+  framework: 'jasmine',
+  jasmineNodeOpts: {
+    showColors: true,
+    defaultTimeoutInterval: 30000,
+    print: function () {
     }
+  },
+  onPrepare() {
+    require('ts-node').register({
+      project: 'e2e/tsconfig.e2e.json'
+    });
+    jasmine.getEnv().addReporter(new SpecReporter());
+  }
 };
