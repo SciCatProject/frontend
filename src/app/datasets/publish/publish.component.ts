e39796c8d92e010736d0dc3301976176fbfc3ecc
Hannes Petri <hannes.petri@maxiv.lu.se>
2018-09-10 11:58:40 +0200

    Work on publication form

--- src/app/datasets/publish/publish.component.ts
+++ src/app/datasets/publish/publish.component.ts
@@ -1,18 +1,37 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, Inject } from "@angular/core";
 import { Store, select } from "@ngrx/store";
 import { getDatasetsInBatch } from "state-management/selectors/datasets.selectors";
 import { map } from "rxjs/operators";
+import { PrefillBatchAction } from "state-management/actions/datasets.actions";
+import { APP_CONFIG } from "app-config.module";
 
 @Component({
   selector: "publish",
   templateUrl: "./publish.component.html",
   styleUrls: ["./publish.component.scss"]
 })
-export class PublishComponent {
-  private datasetCount$ = this.store.pipe(
+export class PublishComponent implements OnInit {
+  protected datasetCount$ = this.store.pipe(
     select(getDatasetsInBatch),
     map(datasets => datasets.length)
   );
 
-  constructor(private store: Store<any>) {}
+  protected form = {
+    firstName: "",
+    lastName: "",
+    affiliation: this.appConfig.facility,
+    publisher: this.appConfig.facility,
+    publicationYear: new Date().getFullYear()
+  };
+
+  constructor(
+    private store: Store<any>,
+    @Inject(APP_CONFIG) private appConfig
+  ) {}
+
+  public ngOnInit() {
+    this.store.dispatch(new PrefillBatchAction());
+  }
+
+  protected onPublish() {}
 }
