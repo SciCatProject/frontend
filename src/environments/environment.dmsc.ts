73ca0836a2ae2cef8ffa1f2a6da53654fe614279
Gareth Murphy <garethcmurphy@gmail.com>
2018-09-10 14:29:18 +0200

    add production

--- src/environments/environment.dmsc.ts
+++ src/environments/environment.dmsc.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   lbBaseURL: "https://kubetest02.dm.esss.dk:32223",
   fileserverBaseURL: "https://kubetest04.dm.esss.dk:32223",
   externalAuthEndpoint: "/auth/msad",
