682dd6143b36af3d86fd266255b41671c0c5df20
Gareth Murphy <garethcmurphy@gmail.com>
2018-09-13 14:14:33 +0200

    add take and unsubscribe

--- src/app/datasets/dataset-form/dataset-form.component.ts
+++ src/app/datasets/dataset-form/dataset-form.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from "@angular/core";
-import { Observable, of } from "rxjs";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Observable, of, Subscription } from "rxjs";
 import { RawDataset } from "../../shared/sdk/models";
 import { SaveDatasetAction } from "../../state-management/actions/datasets.actions";
 import { getCurrentDataset } from "../../state-management/selectors/datasets.selectors";
 import { select, Store } from "@ngrx/store";
+import { take } from "rxjs/operators";
 
 interface MetadataField {
   name: string;
@@ -15,25 +16,32 @@ interface MetadataField {
   templateUrl: "./dataset-form.component.html",
   styleUrls: ["./dataset-form.component.css"]
 })
-export class DatasetFormComponent implements OnInit {
+export class DatasetFormComponent implements OnInit, OnDestroy {
   dataset$: Observable<RawDataset>;
   metadataField$: Observable<MetadataField>;
   submitted = false;
+  datasetSubscription: void;
+  metadataSubscription: Subscription;
 
   constructor(private store: Store<any>) {}
 
   onSubmit() {
     this.submitted = true;
-    this.metadataField$.subscribe(output => {});
-    this.dataset$.subscribe(updated_dataset => {
-      this.metadataField$.subscribe(metadata => {
-        updated_dataset.scientificMetadata[metadata.name] = metadata.value;
-        this.store.dispatch(new SaveDatasetAction(updated_dataset));
-      });
-    });
+    this.datasetSubscription = this.dataset$.pipe(take(1)).subscribe(updated_dataset => {
+      this.metadataSubscription = this.metadataField$
+        .pipe(take(1))
+        .subscribe(metadata => {
+          updated_dataset.scientificMetadata[metadata.name] = metadata.value;
+          this.store.dispatch(new SaveDatasetAction(updated_dataset));
+        });
+    }).unsubscribe();
+  }
+
+  ngOnDestroy() {
   }
 
   ngOnInit() {
+    this.submitted = false;
     this.dataset$ = this.store.pipe(select<RawDataset>(getCurrentDataset));
     this.metadataField$ = of({ name: "Name", value: "Value" });
   }
