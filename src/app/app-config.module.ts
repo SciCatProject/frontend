519558c9e3c186a588daa1e7475301e3146436d5
Gareth Murphy <garethcmurphy@gmail.com>
2018-09-17 10:05:19 +0200

    update to include environment in unit test see Issue #18

--- src/app/app-config.module.ts
+++ src/app/app-config.module.ts
@@ -1,34 +1,36 @@
-import { NgModule, InjectionToken } from '@angular/core';
-import { environment } from '../environments/environment';
+import { NgModule, InjectionToken } from "@angular/core";
+import { environment } from "../environments/environment";
 
-export const APP_CONFIG = new InjectionToken<AppConfig>('app.config');
+export const APP_CONFIG = new InjectionToken<AppConfig>("app.config");
 
 export class AppConfig {
-    externalAuthEndpoint: string;
-    fileserverBaseURL: string;
-    production: boolean;
-    disabledDatasetColumns: string[];
-    archiveWorkflowEnabled: boolean;
-    editMetadataEnabled: boolean;
-    facility: string;
-    shoppingCartEnabled: boolean;
+  externalAuthEndpoint: string;
+  fileserverBaseURL: string;
+  production: boolean;
+  disabledDatasetColumns: string[];
+  archiveWorkflowEnabled: boolean;
+  editMetadataEnabled: boolean;
+  facility: string;
+  shoppingCartEnabled: boolean;
 }
 
 export const APP_DI_CONFIG: AppConfig = {
-    externalAuthEndpoint: environment.externalAuthEndpoint,
-    fileserverBaseURL: environment['fileserverBaseURL'] || null,
-    production: environment.production,
-    disabledDatasetColumns: environment['disabledDatasetColumns'] || [],
-    archiveWorkflowEnabled: environment['archiveWorkflowEnabled'] || null,
-    editMetadataEnabled: environment['editMetadataEnabled'] || null,
-    facility: environment['facility'] || null,
-    shoppingCartEnabled: environment['shoppingCartEnabled'] || false
+  externalAuthEndpoint: environment.externalAuthEndpoint,
+  fileserverBaseURL: environment["fileserverBaseURL"] || null,
+  production: environment.production,
+  disabledDatasetColumns: environment["disabledDatasetColumns"] || [],
+  archiveWorkflowEnabled: environment["archiveWorkflowEnabled"] || null,
+  editMetadataEnabled: environment["editMetadataEnabled"] || null,
+  facility: environment["facility"] || null,
+  shoppingCartEnabled: environment["shoppingCartEnabled"] || false
 };
 
 @NgModule({
-    providers: [{
-        provide: APP_CONFIG,
-        useValue: APP_DI_CONFIG,
-    }]
+  providers: [
+    {
+      provide: APP_CONFIG,
+      useValue: APP_DI_CONFIG
+    }
+  ]
 })
 export class AppConfigModule {}
--- src/app/datasets/dataset-detail/dataset-detail.component.spec.ts
+++ src/app/datasets/dataset-detail/dataset-detail.component.spec.ts
@@ -1,16 +1,17 @@
-import { NO_ERRORS_SCHEMA } from "@angular/core";
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-import { ReactiveFormsModule } from "@angular/forms";
+import { APP_CONFIG, AppConfigModule } from "app-config.module";
 import { ActivatedRoute } from "@angular/router";
-import { Store, StoreModule } from "@ngrx/store";
-import { DatafilesComponent } from "datasets/datafiles/datafiles.component";
 import { ConfigFormComponent } from "shared/modules/config-form/config-form.component";
-import { MockActivatedRoute, MockStore } from "shared/MockStubs";
-import { ObjKeysPipe, TitleCasePipe } from "shared/pipes/index";
-import { rootReducer } from "state-management/reducers/root.reducer";
-import { MatTableModule } from "@angular/material";
+import { DatafilesComponent } from "datasets/datafiles/datafiles.component";
 import { DatasetDetailComponent } from "./dataset-detail.component";
 import { FileSizePipe } from "../filesize.pipe";
+import { MatTableModule } from "@angular/material";
+import { MockActivatedRoute, MockStore } from "shared/MockStubs";
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { ObjKeysPipe, TitleCasePipe } from "shared/pipes/index";
+import { ReactiveFormsModule } from "@angular/forms";
+import { Store, StoreModule } from "@ngrx/store";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { rootReducer } from "state-management/reducers/root.reducer";
 
 describe("DatasetDetailComponent", () => {
   let component: DatasetDetailComponent;
@@ -20,6 +21,7 @@ describe("DatasetDetailComponent", () => {
     TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
       imports: [
+        AppConfigModule,
         ReactiveFormsModule,
         MatTableModule,
         StoreModule.forRoot({ rootReducer })
@@ -36,6 +38,12 @@ describe("DatasetDetailComponent", () => {
     TestBed.overrideComponent(DatasetDetailComponent, {
       set: {
         providers: [
+          {
+            provide: APP_CONFIG,
+            useValue: {
+              editMetadataEnabled: true
+            }
+          },
           { provide: ActivatedRoute, useClass: MockActivatedRoute },
           { provide: Store, useClass: MockStore }
         ]
