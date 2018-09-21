ceb4d49e5d27eba07e6a884db398f8d9e0c95ce6
Gareth Murphy <garethcmurphy@gmail.com>
2018-09-21 11:35:20 +0200

    add forms import to shared module for config form

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,12 @@ import { ConfigService } from "./services";
 import { DialogModule } from "shared/modules/dialog/dialog.module";
 import { ErrorPageModule } from "shared/modules/error-page/error-page.module";
 import { FileSizePipe } from "./pipes/filesize.pipe";
+import { FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { NgModule } from "@angular/core";
 import { KeysPipe } from "./pipes";
 
 @NgModule({
-  imports: [BreadcrumbModule, ConfigFormModule, ErrorPageModule, DialogModule],
+  imports: [BreadcrumbModule, ConfigFormModule, ErrorPageModule, FormsModule, DialogModule, ReactiveFormsModule],
   declarations: [FileSizePipe, KeysPipe],
   providers: [ConfigService],
   exports: [
