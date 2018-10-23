1de6376c640b826c61a56028f56e501436c54c61
Gareth Murphy <garethcmurphy@gmail.com>
2018-10-23 15:14:46 +0200

    add concatmap

--- src/app/datasets/publish/publish.component.ts
+++ src/app/datasets/publish/publish.component.ts
@@ -1,12 +1,13 @@
 import { Component, Inject, OnInit } from "@angular/core";
 import { select, Store } from "@ngrx/store";
 import { getDatasetsInBatch } from "state-management/selectors/datasets.selectors";
-import { map } from "rxjs/operators";
+import { concatMap, map } from "rxjs/operators";
 import { PrefillBatchAction } from "state-management/actions/datasets.actions";
 import { APP_CONFIG } from "app-config.module";
 
 import { PublishedDataApi } from "../../shared/sdk/services/custom";
 import { PublishedData } from "../../shared/sdk/models";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "publish",
@@ -45,8 +46,32 @@ export class PublishComponent implements OnInit {
 
   public onPublish() {
     console.log("register dataset");
-    this.store.pipe(select(getDatasetsInBatch), map(datasets => {
+    const batch_datasets$ = this.store.pipe(select(getDatasetsInBatch), map(datasets => {
       console.log(datasets);
+      const pub = new PublishedData();
+      pub.pidArray = [];
+      pub.authors = [];
+      for (const dataset of datasets) {
+        pub.affiliation = dataset.license;
+        pub.abstract = dataset.license;
+        pub.authors.push(dataset.owner);
+        pub.creator = dataset.owner;
+        pub.dataDescription = dataset.owner;
+        pub.doi = dataset.owner;
+        pub.doiRegisteredSuccessfullyTime = new Date(Date.now());
+        pub.numberOfFiles = 27;
+        pub.pidArray.push(dataset.pid);
+        pub.publicationYear = 2018;
+        pub.publisher = dataset.license;
+        pub.resourceType = "NeXus HDF5 files";
+        pub.sizeOfArchive = 27;
+        pub.thumbnail = "Test data from Scicat";
+        pub.title = "Test data from Scicat";
+        pub.url = "https://scicat.esss.se";
+        pub.doi = "new" + Math.random().toString(36).substring(7);
+      }
+      console.log(pub);
+      return pub;
     }));
     if (this.formIsValid()) {
       // Publish
@@ -56,25 +81,34 @@ export class PublishComponent implements OnInit {
     }
     const published_data: PublishedData = {
       affiliation: "ESS",
-      doi: "newdoi" + Math.random().toString(36).substring(7),
-      creator: "Addison Neutron",
-      publisher: "ESS",
-      publicationYear: 2018,
-      title: "Test Data from SciCat",
-      url: "https://scicat.esss.se",
       abstract: "Test abstract",
-      dataDescription: "string",
-      thumbnail: "string",
-      resourceType: "NeXus files",
-      numberOfFiles: 21,
-      sizeOfArchive: 21,
-      pidArray: ["2121"],
       authors: ["Addison Neutron"],
-      doiRegisteredSuccessfullyTime: new Date(Date.now())
+      creator: "Addison Neutron",
+      dataDescription: "string",
+      doi: "new" + Math.random().toString(36).substring(7),
+      doiRegisteredSuccessfullyTime: new Date(Date.now()),
+      numberOfFiles: 21,
+      pidArray: ["2121"],
+      publicationYear: 2018,
+      publisher: "ESS",
+      resourceType: "NeXus files",
+      sizeOfArchive: 21,
+      thumbnail: "string",
+      title: "Test Data from SciCat 2",
+      url: "https://scicat.esss.se"
     };
-    this.pdapi.create(published_data).subscribe();
-    this.pdapi.register(published_data.doi).subscribe();
+    published_data.doi = "new" + Math.random().toString(36).substring(7);
+    console.log(published_data.doi);
+    const create_pub$ = this.pdapi.create(published_data);
+    const register_pub$ = this.pdapi.register(published_data.doi);
 
+    const result$ = batch_datasets$.pipe(concatMap(val => this.getPubtoCreate(val)));
+    result$.subscribe();
+
+  }
+
+  public getPubtoCreate(pub: PublishedData): Observable<any> {
+    return this.pdapi.create(pub);
   }
 
   private formIsValid() {
