a98ddd2a60eaec1e5a51f4bf47ec844c8bd42920
Gareth Murphy <garethcmurphy@gmail.com>
2018-11-14 15:18:17 +0100

    add import to unit test

--- src/app/samples/sample-table/sample-table.component.spec.ts
+++ src/app/samples/sample-table/sample-table.component.spec.ts
@@ -2,13 +2,8 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { SampleTableComponent } from "./sample-table.component";
 import { Store } from "@ngrx/store";
-import { MatTableModule } from "@angular/material";
-import {
-  MockHttp,
-  MockRouter,
-  MockSampleService,
-  MockStore
-} from "../../shared/MockStubs";
+import { MatCardModule, MatTableModule } from "@angular/material";
+import { MockHttp, MockRouter, MockSampleService, MockStore } from "../../shared/MockStubs";
 import { HttpClient } from "@angular/common/http";
 import { Router } from "@angular/router";
 import { SampleService } from "../sample.service";
@@ -20,7 +15,7 @@ describe("SampleTableComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [SampleTableComponent],
-      imports: [MatTableModule]
+      imports: [MatTableModule, MatCardModule]
     });
     TestBed.overrideComponent(SampleTableComponent, {
       set: {
