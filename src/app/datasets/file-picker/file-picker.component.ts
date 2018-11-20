dbc998cc21e7237b2bb7b4e9a73961dd026b6cf6
Gareth Murphy <garethcmurphy@gmail.com>
2018-11-20 10:42:47 +0100

    rearrange image upload

--- src/app/datasets/file-picker/file-picker.component.ts
+++ src/app/datasets/file-picker/file-picker.component.ts
@@ -1,23 +1,27 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
-import { ViewChild } from "@angular/core";
-import { Inject } from "@angular/core";
-import { Store, select } from "@ngrx/store";
+import { Component, Inject, OnDestroy, OnInit, ViewChild } from "@angular/core";
+import { select, Store } from "@ngrx/store";
 import { Observable } from "rxjs";
 import { Dataset } from "shared/sdk/models";
 import { APP_CONFIG, AppConfig } from "../../app-config.module";
 import * as lb from "shared/sdk/services";
 import { FilePickerDirective, ReadFile, ReadMode } from "ngx-file-helpers";
 import { filter } from "rxjs/operators";
+import { getCurrentAttachments } from "../../state-management/selectors/datasets.selectors";
 
 @Component({
   selector: "app-file-picker",
   templateUrl: "./file-picker.component.html",
-  styleUrls: ["./file-picker.component.css"]
+  styleUrls: ["./file-picker.component.scss"]
 })
 export class FilePickerComponent implements OnInit, OnDestroy {
   dataset$: Observable<Dataset>;
   dataset: any;
   subscriptions = [];
+  public readMode = ReadMode.dataURL;
+  public picked: ReadFile;
+  public status: string;
+  @ViewChild(FilePickerDirective)
+  private filePicker;
 
   constructor(
     @Inject(APP_CONFIG) private config: AppConfig,
@@ -38,13 +42,6 @@ export class FilePickerComponent implements OnInit, OnDestroy {
       this.dataset = dataset;
     });
   }
-
-  public readMode = ReadMode.dataURL;
-  public picked: ReadFile;
-  public status: string;
-
-  @ViewChild(FilePickerDirective)
-  private filePicker;
 
   onReadStart(fileCount: number) {
     this.status = `Now reading ${fileCount} file(s)...`;
@@ -69,6 +66,7 @@ export class FilePickerComponent implements OnInit, OnDestroy {
       return this.daSrv.create(creds).subscribe(res => {
         console.log(res);
         this.filePicker.reset();
+        const attachment$ = this.store.pipe(select(getCurrentAttachments));
       });
     }
   }
