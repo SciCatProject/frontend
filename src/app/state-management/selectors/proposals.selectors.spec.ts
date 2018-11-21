d86ced63408a52b811dfaea891725aa3d4c14885
Gareth Murphy <garethcmurphy@gmail.com>
2018-11-21 13:42:39 +0100

    add fxitures

--- src/app/state-management/selectors/proposals.selectors.spec.ts
+++ src/app/state-management/selectors/proposals.selectors.spec.ts
@@ -1,4 +1,29 @@
-import { createSelector, createFeatureSelector } from "@ngrx/store";
-import { ProposalsState } from "../state/proposals.store";
-// import { getDatasets } from './datasets.selectors';
+const createProposal = ({
+  proposalId = "string",
+  pi_email = "string",
+  pi_firstname = "string",
+  pi_lastname = "string",
+  email = "string",
+  firstname = "string",
+  lastname = "string",
+  title = "string",
+  abstract = "string",
+  attachments = ["string"],
+  ownerGroup = "string",
+  accessGroups = ["string"],
+  createdBy = "string",
+  updatedBy = "string",
+  createdAt = Date,
+  updatedAt = Date,
+  MeasurementPeriodList = ["string"],
+  measurementPeriods = ["string"]
+} ={}): Proposal => ({
+  proposalId: proposalId,
+  pi_email: proposalId,
+  pi_firstname: proposalId,
+});
 
+
+const createProposalState = ({
+  
+});
