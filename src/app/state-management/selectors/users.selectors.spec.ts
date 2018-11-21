451885eec28b9bd6e2f047a2b6c551678461b1b6
Gareth Murphy <garethcmurphy@gmail.com>
2018-11-21 14:56:48 +0100

    add user unit selector test

---
+++ src/app/state-management/selectors/users.selectors.spec.ts
@@ -0,0 +1,26 @@
+
+import * as fromUserSelectors from "./users.selectors";
+
+import { UserState } from "../state/user.store";
+
+
+const initialUserState: UserState = {
+  currentUser: null,
+  currentUserGroups: [],
+  email: undefined,
+  isLoggingIn: false,
+  message: { content: undefined, type: undefined, duration: undefined},
+  settings: {
+    tapeCopies: "one",
+    datasetCount: 30,
+    jobCount: 30,
+    darkTheme: false
+  }, // TODO sync with server settings?
+  isLoggedIn: false
+};
+
+describe("test select", () => {
+  it("should get select", () => {
+    expect(fromUserSelectors.getIsLoggingIn.projector(initialUserState)).toEqual(false);
+  });
+});
