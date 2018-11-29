70a93b112a722dcf9d842c3dbe6a27ec669c5c74
Gareth Murphy <garethcmurphy@gmail.com>
2018-11-29 10:41:47 +0100

    add complete and failed action

--- src/app/state-management/actions/samples.actions.ts
+++ src/app/state-management/actions/samples.actions.ts
@@ -18,12 +18,21 @@ export const FETCH_SAMPLE_FAILED = "[Sample] Fetch Sample Failed";
 export class SelectSampleAction implements Action {
   type = SELECT_SAMPLE;
 
-  constructor(readonly samplelId: string) {}
+  constructor(readonly samplelId: string) { }
 }
 
-export class AddSampleAction implements Action { readonly type = ADD_SAMPLE; }
-export class AddSampleCompleteAction implements Action { readonly type = ADD_SAMPLE_COMPLETE; }
-export class AddSampleFailedAction implements Action { readonly type = ADD_SAMPLE_FAILED; }
+export class AddSampleAction implements Action {
+  readonly type = ADD_SAMPLE;
+  constructor(readonly sample: Sample) { }
+}
+export class AddSampleCompleteAction implements Action {
+  readonly type = ADD_SAMPLE_COMPLETE;
+  constructor(readonly sample: Sample) { }
+}
+export class AddSampleFailedAction implements Action {
+  readonly type = ADD_SAMPLE_FAILED;
+  constructor(readonly sample: Sample) { }
+}
 export class FetchSamplesAction implements Action {
   readonly type = FETCH_SAMPLES;
 }
@@ -31,7 +40,7 @@ export class FetchSamplesAction implements Action {
 export class FetchSamplesCompleteAction implements Action {
   readonly type = FETCH_SAMPLES_COMPLETE;
 
-  constructor(readonly samples: Sample[]) {}
+  constructor(readonly samples: Sample[]) { }
 }
 
 export class FetchSamplesFailedAction implements Action {
@@ -41,13 +50,13 @@ export class FetchSamplesFailedAction implements Action {
 export class FetchSampleAction implements Action {
   readonly type = FETCH_SAMPLE;
 
-  constructor(readonly samplelId: string) {}
+  constructor(readonly samplelId: string) { }
 }
 
 export class FetchSampleCompleteAction implements Action {
   readonly type = FETCH_SAMPLE_COMPLETE;
 
-  constructor(readonly currentSample: Sample) {}
+  constructor(readonly currentSample: Sample) { }
 }
 
 export class FetchSampleFailedAction implements Action {
