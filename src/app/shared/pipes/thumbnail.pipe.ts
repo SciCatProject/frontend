5df435c097ca7e35e18289b52abb141c5d8a9462
Gareth Murphy <garethcmurphy@gmail.com>
2018-12-11 17:20:33 +0100

    thumbnail

--- src/app/shared/pipes/thumbnail.pipe.ts
+++ src/app/shared/pipes/thumbnail.pipe.ts
@@ -1,12 +1,21 @@
 import { Pipe, PipeTransform } from "@angular/core";
 
+import { DatasetAttachmentApi, DatasetAttachment } from "shared/sdk";
+
 @Pipe({
   name: "thumbnail"
 })
 export class ThumbnailPipe implements PipeTransform {
+  attach: string;
+  constructor(private da: DatasetAttachmentApi) {}
 
   transform(value: any, args?: any): any {
-    return null;
+    const attach = this.da
+      .findById("xx")
+      .subscribe((data: DatasetAttachment) => {
+        this.attach = data.thumbnail;
+        console.log("gm attach", this.attach);
+      });
+    return this.attach;
   }
-
 }
