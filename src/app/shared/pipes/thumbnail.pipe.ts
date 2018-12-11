7b1d05e799dea022cf2c1220b77b354dcfac3e7a
Gareth Murphy <garethcmurphy@gmail.com>
2018-12-11 21:49:04 +0100

    add stateful pipe to return thumbnail observable

--- src/app/shared/pipes/thumbnail.pipe.ts
+++ src/app/shared/pipes/thumbnail.pipe.ts
@@ -1,21 +1,17 @@
 import { Pipe, PipeTransform } from "@angular/core";
 
-import { DatasetAttachmentApi, DatasetAttachment } from "shared/sdk";
+import { DatasetAttachmentApi, DatasetAttachment, DatasetApi, Dataset } from "shared/sdk";
 
 @Pipe({
   name: "thumbnail"
 })
 export class ThumbnailPipe implements PipeTransform {
   attach: string;
-  constructor(private da: DatasetAttachmentApi) {}
+  constructor(
+    private ds: DatasetApi,
+    private da: DatasetAttachmentApi) { }
 
   transform(value: any, args?: any): any {
-    const attach = this.da
-      .findById("xx")
-      .subscribe((data: DatasetAttachment) => {
-        this.attach = data.thumbnail;
-        console.log("gm attach", this.attach);
-      });
-    return this.attach;
+    return this.ds.thumbnail(encodeURIComponent(value));
   }
 }
