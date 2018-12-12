e311609e4a0913a87b8247d3cdaec4ae0476d09e
Gareth Murphy <garethcmurphy@gmail.com>
2018-12-12 12:03:44 +0100

    add thumbnail

--- src/app/shared/pipes/thumbnail.pipe.ts
+++ src/app/shared/pipes/thumbnail.pipe.ts
@@ -1,17 +1,28 @@
 import { Pipe, PipeTransform } from "@angular/core";
 
-import { DatasetAttachmentApi, DatasetAttachment, DatasetApi, Dataset } from "shared/sdk";
+import {
+  DatasetAttachmentApi,
+  DatasetAttachment,
+  DatasetApi,
+  Dataset
+} from "shared/sdk";
+import { datasets } from "state-management/selectors";
 
 @Pipe({
   name: "thumbnail"
 })
 export class ThumbnailPipe implements PipeTransform {
   attach: string;
-  constructor(
-    private ds: DatasetApi,
-    private da: DatasetAttachmentApi) { }
+  constructor(private ds: DatasetApi, private da: DatasetAttachmentApi) {}
 
   transform(value: any, args?: any): any {
-    return this.ds.thumbnail(encodeURIComponent(value));
+    const encoded = encodeURIComponent(value);
+    const filter = {
+      where: {
+        datasetId: value
+      }
+    };
+
+    return this.ds.thumbnail(encoded);
   }
 }
