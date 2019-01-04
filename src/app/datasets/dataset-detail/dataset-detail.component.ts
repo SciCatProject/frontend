003467a84fa2b0103557f22fa116ca1090e52857
Luke Gorman <luke.gorman@psi.ch>
2019-01-04 16:09:39 +0100

    Issue #194 added tool tip to arch/ret columns and added confirm dlg to reset status admin button

--- src/app/datasets/dataset-detail/dataset-detail.component.ts
+++ src/app/datasets/dataset-detail/dataset-detail.component.ts
@@ -135,6 +135,9 @@ export class DatasetDetailComponent implements OnInit, OnDestroy {
   }
 
   resetDataset(dataset) {
+    if (!confirm("Reset datablaocks?")) {
+      return null;
+    }
     this.store
       .pipe(
         select(state => state.root.user.currentUser),
