e0cb02a18ce44ebd29a6a362b2d1de2fdd391242
Gareth Murphy <garethcmurphy@gmail.com>
2019-03-09 07:05:40 +0100

    add no metadata found, see issue #283

--- src/app/shared/pipes/json-head.pipe.ts
+++ src/app/shared/pipes/json-head.pipe.ts
@@ -4,18 +4,16 @@ import { Pipe, PipeTransform } from "@angular/core";
   name: "jsonHead"
 })
 export class JsonHeadPipe implements PipeTransform {
-
   transform(value: any, args?: any): any {
     if (!value) {
       return null;
     }
     const newvalue = {};
     let count = 0;
-    let outputString="";
+    let outputString = "";
     for (const key of Object.keys(value)) {
       count = count + 1;
       newvalue[key] = value[key];
-
 
       if (count > 1) {
         break;
@@ -25,9 +23,11 @@ export class JsonHeadPipe implements PipeTransform {
     const key2 = Object.keys(value)[1];
     const val1 = value[key1];
     const val2 = value[key2];
-    outputString = key1+":"+val1 +"\n" + key2 + ":" + val2;
+    outputString = key1 + ":" + val1 + "\n" + key2 + ":" + val2;
+    if (key1 === undefined) {
+      outputString = "No metadata found";
+    }
     const newvalue2 = JSON.stringify(newvalue, null, 2);
     return outputString;
   }
-
 }
