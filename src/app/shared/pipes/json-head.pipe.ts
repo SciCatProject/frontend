d4e22c8aba58f79bdab5b064c9248b5bd18e8583
Gareth Murphy <garethcmurphy@gmail.com>
2019-03-09 06:53:35 +0100

    see issue #283

--- src/app/shared/pipes/json-head.pipe.ts
+++ src/app/shared/pipes/json-head.pipe.ts
@@ -11,15 +11,23 @@ export class JsonHeadPipe implements PipeTransform {
     }
     const newvalue = {};
     let count = 0;
+    let outputString="";
     for (const key of Object.keys(value)) {
       count = count + 1;
       newvalue[key] = value[key];
+
+
       if (count > 1) {
         break;
       }
     }
+    const key1 = Object.keys(value)[0];
+    const key2 = Object.keys(value)[1];
+    const val1 = value[key1];
+    const val2 = value[key2];
+    outputString = key1+":"+val1 +"\n" + key2 + ":" + val2;
     const newvalue2 = JSON.stringify(newvalue, null, 2);
-    return newvalue2;
+    return outputString;
   }
 
 }
