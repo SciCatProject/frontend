ae64b24c8ac85c251a565732a9be034ab32058da
Gareth Murphy <garethcmurphy@gmail.com>
2019-05-07 11:42:13 +0200

    add link to root from title and about

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from "@angular/common";
 import { AboutComponent } from "./about/about.component";
 import { MatCardModule } from "@angular/material";
 import { AppConfigModule } from "app-config.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [AboutComponent],
-  imports: [AppConfigModule, CommonModule, MatCardModule]
+  imports: [AppConfigModule, CommonModule, FlexLayoutModule, MatCardModule]
 })
 export class AboutModule {}
--- src/app/about/about/about.component.ts
+++ src/app/about/about/about.component.ts
@@ -22,13 +22,17 @@ export class AboutComponent implements OnInit {
         "Scicat allows users to access data and metadata from neutron experiments at ESS.";
       this.accessText =
         "Access to the online catalogue of open data will be given to a user," +
-        " not associated with the PT, providing said user registers with ESS, " +
-        "fulfils the ESS requirements for access as defined in the ESS Access Policy," +
-        " and accepts the terms of the ESS scientific data policy.ESS Scientific Data Policy";
+        " providing the user registers with ESS " +
+        " and accepts the terms of the ESS scientific data policy ";
       this.termsText = "All scientific datasets are licensed under the CC-BY-4.0 license ";
     } else if (this.appConfig["facility"] === "PSI") {
       this.aboutText =
         "Scicat allows users to access data and metadata from experiments at PSI.";
+      this.accessText = "Users must comply with access policy of instruments";
+      this.termsText = "Data can be used freely under the CC-BY-4.0 licence";
+    } else if (this.appConfig["facility"] === "MAX IV") {
+      this.aboutText =
+        "Scicat allows users to access data and metadata from experiments at MAX IV.";
       this.accessText = "Users must comply with access policy of instruments";
       this.termsText = "Data can be used freely under the CC-BY-4.0 licence";
     }
