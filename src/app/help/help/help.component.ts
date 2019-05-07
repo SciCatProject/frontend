d706fc862d06a004a556b8d48dd05ed9c1b9d698
Gareth Murphy <garethcmurphy@gmail.com>
2019-05-07 12:43:53 +0200

    add help

--- src/app/help/help/help.component.ts
+++ src/app/help/help/help.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from "@angular/core";
+import { APP_CONFIG, AppConfig } from "app-config.module";
 
 @Component({
-  selector: 'help',
-  templateUrl: './help.component.html',
-  styleUrls: ['./help.component.css']
+  selector: "help",
+  templateUrl: "./help.component.html",
+  styleUrls: ["./help.component.scss"]
 })
 export class HelpComponent implements OnInit {
-
-  constructor() { }
+  facility: string;
+  constructor(@Inject(APP_CONFIG) public appConfig: AppConfig) {}
 
   ngOnInit() {
+    this.facility = this.appConfig.facility;
   }
-
 }
