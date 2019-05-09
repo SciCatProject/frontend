92a0e5c6d5555a725dc2b889832fb5f30f8055d9
Luke Gorman <luke.gorman@psi.ch>
2019-05-09 16:50:14 +0200

    chain create published data with a call to register

--- src/app/state-management/effects/published-data.effects.ts
+++ src/app/state-management/effects/published-data.effects.ts
@@ -10,18 +10,32 @@ import {
   AddPublishedData,
   FetchAllPublishedData,
   FetchPublishedData,
-  PublishedDataActionTypes
+  PublishedDataActionTypes,
+  UpsertPublishedDatas
 } from "../actions/published-data.actions";
 
 import {
   catchError,
   map,
   switchMap,
+  mergeMap
 } from "rxjs/operators";
 import { PublishedData } from "shared/sdk/models";
 
 @Injectable()
 export class PublishedDataEffects {
+  
+  // this needs work, doesnt do both a create and a register
+  @Effect()
+  UpsertPublishedDatas$ = this.actions$.pipe(
+    ofType<UpsertPublishedDatas>(PublishedDataActionTypes.UpsertPublishedDatas),
+    switchMap(action => this.publishedDataApi.create(action.payload.publishedDatas)
+    .pipe(mergeMap((data: PublishedData[]) => [ new UpsertPublishedDatas({ publishedDatas: data }),
+      this.publishedDataApi.register(data[0].doi)]),
+    catchError(err => of(new FailedPublishedDataAction(err)))))
+  );
+
+  // add referes to adding to the store, not mongo
   @Effect()
   AddPublishedData$ = this.actions$.pipe(
     ofType<FetchPublishedData>(PublishedDataActionTypes.FetchPublishedData),
