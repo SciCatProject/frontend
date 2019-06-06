b830fca4734ccdd5b2348fe335730364dba55017
Henrik Johansson <henrik.johansson712@gmail.com>
2019-06-06 16:28:17 +0200

    Refactored reduce component test

--- src/app/datasets/reduce/reduce.component.spec.ts
+++ src/app/datasets/reduce/reduce.component.spec.ts
@@ -1,12 +1,12 @@
 import { NO_ERRORS_SCHEMA } from "@angular/core";
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { Store } from "@ngrx/store";
 
-import { ReduceComponent } from './reduce.component';
+import { ReduceComponent } from "./reduce.component";
 import { MockStore } from "shared/MockStubs";
 import { MatCardModule } from "@angular/material";
 
-describe('ReduceComponent', () => {
+describe("ReduceComponent", () => {
   let component: ReduceComponent;
   let fixture: ComponentFixture<ReduceComponent>;
 
@@ -14,13 +14,11 @@ describe('ReduceComponent', () => {
     TestBed.configureTestingModule({
       schemas: [NO_ERRORS_SCHEMA],
       imports: [MatCardModule],
-      declarations: [ ReduceComponent ]
+      declarations: [ReduceComponent]
     });
     TestBed.overrideComponent(ReduceComponent, {
       set: {
-        providers: [
-          { provide: Store, useClass: MockStore }
-        ]
+        providers: [{ provide: Store, useClass: MockStore }]
       }
     });
     TestBed.compileComponents();
@@ -32,7 +30,7 @@ describe('ReduceComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
