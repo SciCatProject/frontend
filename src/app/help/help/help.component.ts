1789843eb2de8b5bf46875dfeda219f4f9e5053e
Luke Gorman <luke.gorman@psi.ch>
2019-06-28 16:29:49 +0200

    ref env config fix

--- src/app/help/help/help.component.ts
+++ src/app/help/help/help.component.ts
@@ -10,11 +10,13 @@ export class HelpComponent implements OnInit {
   facility: string;
   ingestManual: string;
   gettingStarted: string;
+  shoppingCartEnabled: boolean;
   constructor(@Inject(APP_CONFIG) public appConfig: AppConfig) {}
 
   ngOnInit() {
     this.facility = this.appConfig.facility;
     this.ingestManual = this.appConfig.ingestManual;
     this.gettingStarted = this.appConfig.gettingStarted;
+    this.shoppingCartEnabled = this.appConfig.shoppingCartEnabled;
   }
 }
