2bca0c90d341aba0173efd5539457ffd43acf3b3
Gareth Murphy <garethcmurphy@gmail.com>
2019-07-23 12:59:30 +0200

    remove uneeded import, see #408

--- src/app/users/user-details/user-details.component.ts
+++ src/app/users/user-details/user-details.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { Store } from "@ngrx/store";
 import * as selectors from "state-management/selectors";
-import { ConsoleReporter } from "jasmine";
 
 /**
- * Compoennt to show uneditable user details
+ * Component to show uneditable user details
  * @export
  * @class UserDetailsComponent
  * @implements {OnInit}
