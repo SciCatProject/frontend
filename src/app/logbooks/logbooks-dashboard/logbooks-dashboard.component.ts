99d7e14df2c3dd8ace4ac78ce7cb4f5df8f00567
Henrik Johansson <henrik.johansson712@gmail.com>
2019-07-23 11:33:00 +0200

    Updated relevant Logbook components for new scichat wrapper

--- src/app/logbooks/logbooks-dashboard/logbooks-dashboard.component.ts
+++ src/app/logbooks/logbooks-dashboard/logbooks-dashboard.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Store, select } from "@ngrx/store";
-import { Logbook } from "shared/sdk";
+import { Logbook, Dataset } from "shared/sdk";
 import { Subscription } from "rxjs";
 import { getLogbook } from "state-management/selectors/logbooks.selector";
 import { MatIconRegistry } from "@angular/material";
 import { DomSanitizer } from "@angular/platform-browser";
+import { getCurrentDataset } from "state-management/selectors/datasets.selectors";
+import { FetchLogbookAction } from "state-management/actions/logbooks.actions";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-logbooks-dashboard",
@@ -12,12 +15,18 @@ import { DomSanitizer } from "@angular/platform-browser";
   styleUrls: ["./logbooks-dashboard.component.scss"]
 })
 export class LogbooksDashboardComponent implements OnInit, OnDestroy {
+  logbookName: string;
+
   logbook: Logbook;
   logbookSubscription: Subscription;
+
+  dataset: Dataset;
+  datasetSubscription: Subscription;
 
   constructor(
     iconRegistry: MatIconRegistry,
     sanitizer: DomSanitizer,
+    private route: ActivatedRoute,
     private store: Store<Logbook>
   ) {
     iconRegistry.addSvgIcon(
@@ -34,9 +43,32 @@ export class LogbooksDashboardComponent implements OnInit, OnDestroy {
       .subscribe(logbook => {
         this.logbook = logbook;
       });
+    this.datasetSubscription = this.store
+      .pipe(select(getCurrentDataset))
+      .subscribe(dataset => {
+        this.dataset = dataset;
+      });
+    this.datasetSubscription = this.store
+      .pipe(select(getCurrentDataset))
+      .subscribe(dataset => {
+        this.dataset = dataset;
+      });
+
+    this.route.params.subscribe(params => {
+      if (params.hasOwnProperty("name")) {
+        this.logbookName = params["name"];
+      } else {
+        if (this.dataset.hasOwnProperty("proposalId")) {
+          this.logbookName = this.dataset.proposalId;
+        }
+      }
+    });
+
+    this.store.dispatch(new FetchLogbookAction(this.logbookName));
   }
 
   ngOnDestroy() {
     this.logbookSubscription.unsubscribe();
+    this.datasetSubscription.unsubscribe();
   }
 }
--- src/app/logbooks/logbooks-table/logbooks-table.component.ts
+++ src/app/logbooks/logbooks-table/logbooks-table.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Router } from "@angular/router";
 import { select, Store } from "@ngrx/store";
-import { Observable } from "rxjs";
+import { Subscription } from "rxjs";
 
 import { FetchLogbooksAction } from "state-management/actions/logbooks.actions";
 import { getLogbooks } from "state-management/selectors/logbooks.selector";
@@ -12,17 +12,29 @@ import { Logbook } from "state-management/models";
   templateUrl: "./logbooks-table.component.html",
   styleUrls: ["./logbooks-table.component.scss"]
 })
-export class LogbooksTableComponent implements OnInit {
-  logbooks$: Observable<Logbook[]>;
+export class LogbooksTableComponent implements OnInit, OnDestroy {
+  logbooks: Logbook[];
+  logbooksSubscription: Subscription;
 
   columnsToDisplay: string[] = ["name", "latestEntry", "sender", "entry"];
 
-  constructor(private router: Router, private store: Store<Logbook[]>) {
-    this.logbooks$ = store.pipe(select(getLogbooks));
-  }
+  constructor(private router: Router, private store: Store<Logbook[]>) {}
 
   ngOnInit() {
     this.store.dispatch(new FetchLogbooksAction());
+    this.logbooksSubscription = this.store
+      .pipe(select(getLogbooks))
+      .subscribe(logbooks => {
+        logbooks.forEach(logbook => {
+          let reversedMessages = logbook.messages.reverse();
+          logbook.messages = reversedMessages;
+        });
+        this.logbooks = logbooks;
+      });
+  }
+
+  ngOnDestroy() {
+    this.logbooksSubscription.unsubscribe();
   }
 
   onClick(logbook: Logbook): void {
