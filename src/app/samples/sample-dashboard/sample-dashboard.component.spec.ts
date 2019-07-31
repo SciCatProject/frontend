9e3f7ad204172134b28808405331570bc11d22c4
Gareth Murphy <garethcmurphy@gmail.com>
2019-07-31 16:44:56 +0200

    add store mock

--- src/app/samples/sample-dashboard/sample-dashboard.component.spec.ts
+++ src/app/samples/sample-dashboard/sample-dashboard.component.spec.ts
@@ -5,6 +5,8 @@ import { SampleDashboardComponent } from "./sample-dashboard.component";
 import { SampleSearchComponent } from "samples/sample-search/sample-search.component";
 import { SampleTableComponent } from "samples/sample-table/sample-table.component";
 import { MatTableModule } from "@angular/material";
+import { MockStore } from "shared/MockStubs";
+import { Store } from "@ngrx/store";
 
 describe("SampleDashboardComponent", () => {
   let component: SampleDashboardComponent;
@@ -21,7 +23,15 @@ describe("SampleDashboardComponent", () => {
         SampleSearchComponent,
         SampleTableComponent
       ]
-    }).compileComponents();
+    });
+    TestBed.overrideComponent(SampleDashboardComponent, {
+      set: {
+        providers: [
+          { provide: Store , useClass: MockStore}
+        ]
+      }
+    });
+    TestBed.compileComponents();
   }));
 
   beforeEach(() => {
