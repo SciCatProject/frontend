ca68e65a63f008525a742d71b7561352f23cd56e
Gareth Murphy <garethcmurphy@gmail.com>
2019-08-01 17:02:08 +0200

    deepscan

--- src/app/proposals/proposal-dashboard/proposal-dashboard.component.spec.ts
+++ src/app/proposals/proposal-dashboard/proposal-dashboard.component.spec.ts
@@ -1,16 +1,24 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-
+import { APP_CONFIG } from "app-config.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ListProposalsPageComponent } from "proposals/containers/list-proposals-page/list-proposals-page.component";
+import { MockRouter, MockStore } from "shared/MockStubs";
 import { ProposalDashboardComponent } from "./proposal-dashboard.component";
 import { ProposalSearchComponent } from "proposals/proposal-search/proposal-search.component";
-import { ListProposalsPageComponent } from "proposals/containers/list-proposals-page/list-proposals-page.component";
-import { APP_CONFIG } from "app-config.module";
-import { Store } from "@ngrx/store";
-import { MatDialog, MatPaginator, MatPaginatorModule, MatInputModule, MatFormFieldModule, MatTableModule } from "@angular/material";
-import { Router } from "@angular/router";
-import { MockRouter, MockStore } from "shared/MockStubs";
 import { ProposalsListComponent } from "proposals/components/proposals-list/proposals-list.component";
-import { BrowserAnimationsModule, NoopAnimationsModule } from "@angular/platform-browser/animations";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { Router } from "@angular/router";
+import { Store } from "@ngrx/store";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import {
+  MatDialog,
+  MatPaginatorModule,
+  MatInputModule,
+  MatFormFieldModule,
+  MatTableModule
+} from "@angular/material";
+import {
+  BrowserAnimationsModule,
+  NoopAnimationsModule
+} from "@angular/platform-browser/animations";
 
 describe("ProposalDashboardComponent", () => {
   let component: ProposalDashboardComponent;
