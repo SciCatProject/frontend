134b09ce40164b4984cfb88103a4def0de6fdddf
Gareth Murphy <garethcmurphy@gmail.com>
2019-08-01 11:11:28 +0200

    add declaration

--- src/app/proposals/proposal-dashboard/proposal-dashboard.component.spec.ts
+++ src/app/proposals/proposal-dashboard/proposal-dashboard.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { ProposalDashboardComponent } from "./proposal-dashboard.component";
+import { ProposalSearchComponent } from "proposals/proposal-search/proposal-search.component";
 
 describe("ProposalDashboardComponent", () => {
   let component: ProposalDashboardComponent;
@@ -8,7 +9,7 @@ describe("ProposalDashboardComponent", () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ProposalDashboardComponent]
+      declarations: [ProposalDashboardComponent, ProposalSearchComponent]
     }).compileComponents();
   }));
 
