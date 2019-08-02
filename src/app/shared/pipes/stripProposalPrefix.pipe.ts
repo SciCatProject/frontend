fa189163f862321ac265bb43ba0d51157dee9228
Gareth Murphy <garethcmurphy@gmail.com>
2019-08-02 11:54:57 +0200

    cleanup old lint rules

--- src/app/shared/pipes/stripProposalPrefix.pipe.ts
+++ src/app/shared/pipes/stripProposalPrefix.pipe.ts
@@ -1,13 +1,11 @@
 import { Pipe, PipeTransform } from "@angular/core";
 
-
 @Pipe({
   name: "StripProposalPrefix"
 })
 export class StripProposalPrefixPipe implements PipeTransform {
-
   transform(value: string): string {
-    if (!value){
+    if (!value) {
       return "";
     }
     return value.replace(/proposal-/i, "");
