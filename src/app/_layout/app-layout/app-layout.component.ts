d12ff8b505fd22cab6f1191840bad8753658196e
Henrik Johansson <henrik.johansson712@gmail.com>
2019-08-07 15:02:58 +0200

    Added darkTheme$ property to app layout component

--- src/app/_layout/app-layout/app-layout.component.ts
+++ src/app/_layout/app-layout/app-layout.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Store, select } from "@ngrx/store";
+import * as selectors from "state-management/selectors";
 
 @Component({
-  selector: 'app-app-layout',
-  templateUrl: './app-layout.component.html',
-  styleUrls: ['./app-layout.component.scss']
+  selector: "app-app-layout",
+  templateUrl: "./app-layout.component.html",
+  styleUrls: ["./app-layout.component.scss"]
 })
 export class AppLayoutComponent implements OnInit {
+  darkTheme$;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private store: Store<any>) {
+    this.darkTheme$ = this.store.pipe(select(selectors.users.getTheme));
   }
 
+  ngOnInit() {}
 }
