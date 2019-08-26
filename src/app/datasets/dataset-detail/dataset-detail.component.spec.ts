68194e272cd632d26772c57154d66a4af3881837
Henrik Johansson <henrik.johansson712@gmail.com>
2019-08-26 14:08:46 +0200

    Added unit test for onClickKeyword

--- src/app/datasets/dataset-detail/dataset-detail.component.spec.ts
+++ src/app/datasets/dataset-detail/dataset-detail.component.spec.ts
@@ -5,18 +5,32 @@ import { DatasetDetailComponent } from "./dataset-detail.component";
 import { LinkyPipe } from "ngx-linky";
 import { MatTableModule } from "@angular/material";
 import { MockActivatedRoute, MockStore } from "shared/MockStubs";
-import { MockRouter } from "shared/MockStubs";
 import { Router } from "@angular/router";
 import { NO_ERRORS_SCHEMA } from "@angular/core";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { Store, StoreModule } from "@ngrx/store";
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  inject
+} from "@angular/core/testing";
 import { rootReducer } from "state-management/reducers/root.reducer";
 import { SharedCatanieModule } from "shared/shared.module";
+import {
+  ClearFacetsAction,
+  AddKeywordFilterAction
+} from "state-management/actions/datasets.actions";
 
 describe("DatasetDetailComponent", () => {
   let component: DatasetDetailComponent;
   let fixture: ComponentFixture<DatasetDetailComponent>;
+
+  let router = {
+    navigateByUrl: jasmine.createSpy("navigateByUrl")
+  };
+  let store: MockStore;
+  let dispatchSpy;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -34,15 +48,14 @@ describe("DatasetDetailComponent", () => {
     TestBed.overrideComponent(DatasetDetailComponent, {
       set: {
         providers: [
-          { provide: Router, useClass: MockRouter },
+          { provide: Router, useValue: router },
           {
             provide: APP_CONFIG,
             useValue: {
               editMetadataEnabled: true
             }
           },
-          { provide: ActivatedRoute, useClass: MockActivatedRoute },
-          { provide: Store, useClass: MockStore }
+          { provide: ActivatedRoute, useClass: MockActivatedRoute }
         ]
       }
     });
@@ -55,11 +68,28 @@ describe("DatasetDetailComponent", () => {
     fixture.detectChanges();
   });
 
+  beforeEach(inject([Store], (mockStore: MockStore) => {
+    store = mockStore;
+  }));
+
   afterEach(() => {
     fixture.destroy();
   });
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("#onClickKeyword should update datasets keyword filter and navigate to datasets table", () => {
+    dispatchSpy = spyOn(store, "dispatch");
+    const keyword = "test";
+    component.onClickKeyword(keyword);
+
+    expect(dispatchSpy).toHaveBeenCalledTimes(2);
+    expect(dispatchSpy).toHaveBeenCalledWith(new ClearFacetsAction());
+    expect(dispatchSpy).toHaveBeenCalledWith(
+      new AddKeywordFilterAction(keyword)
+    );
+    expect(router.navigateByUrl).toHaveBeenCalledWith("/datasets");
   });
 });
