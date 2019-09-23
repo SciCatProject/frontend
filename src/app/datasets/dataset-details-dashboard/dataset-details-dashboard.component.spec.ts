fce8f0aabb626da66e41244bd9036565ab70d1d1
Henrik Johansson <henrik.johansson712@gmail.com>
2019-09-23 16:57:22 +0200

    Refactored dataset details

--- src/app/datasets/dataset-details-dashboard/dataset-details-dashboard.component.spec.ts
+++ src/app/datasets/dataset-details-dashboard/dataset-details-dashboard.component.spec.ts
@@ -1,15 +1,67 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  inject
+} from "@angular/core/testing";
 
 import { DatasetDetailsDashboardComponent } from "./dataset-details-dashboard.component";
+import { MockStore, MockActivatedRoute, MockUserApi } from "shared/MockStubs";
+import { Store, StoreModule } from "@ngrx/store";
+import {
+  ClearFacetsAction,
+  AddKeywordFilterAction,
+  AddAttachment,
+  UpdateAttachmentCaptionAction,
+  DeleteAttachment
+} from "state-management/actions/datasets.actions";
+import { Dataset, UserApi } from "shared/sdk";
+import { ReadFile, ReadMode } from "ngx-file-helpers";
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { AppConfigModule, APP_CONFIG } from "app-config.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatTableModule } from "@angular/material";
+import { SharedCatanieModule } from "shared/shared.module";
+import { rootReducer } from "state-management/reducers/root.reducer";
+import { Router, ActivatedRoute } from "@angular/router";
 
 describe("DetailsDashboardComponent", () => {
   let component: DatasetDetailsDashboardComponent;
   let fixture: ComponentFixture<DatasetDetailsDashboardComponent>;
 
+  let router = {
+    navigateByUrl: jasmine.createSpy("navigateByUrl")
+  };
+  let store: MockStore;
+  let dispatchSpy;
+  let pipeSpy;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [DatasetDetailsDashboardComponent]
-    }).compileComponents();
+      schemas: [NO_ERRORS_SCHEMA],
+      declarations: [DatasetDetailsDashboardComponent],
+      imports: [
+        AppConfigModule,
+        SharedCatanieModule,
+        StoreModule.forRoot({ rootReducer })
+      ]
+    });
+    TestBed.overrideComponent(DatasetDetailsDashboardComponent, {
+      set: {
+        providers: [
+          { provide: Router, useValue: router },
+          {
+            provide: APP_CONFIG,
+            useValue: {
+              editMetadataEnabled: true
+            }
+          },
+          { provide: ActivatedRoute, useClass: MockActivatedRoute },
+          { provide: UserApi, useClass: MockUserApi }
+        ]
+      }
+    });
+    TestBed.compileComponents();
   }));
 
   beforeEach(() => {
@@ -18,7 +70,151 @@ describe("DetailsDashboardComponent", () => {
     fixture.detectChanges();
   });
 
+  beforeEach(inject([Store], (mockStore: MockStore) => {
+    store = mockStore;
+  }));
+
+  afterEach(() => {
+    fixture.destroy();
+  });
+
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  describe("#onClickProposal()", () => {
+    it("should navigate to a proposal", () => {
+      const proposalId = "ABC123";
+      component.onClickProposal(proposalId);
+
+      expect(router.navigateByUrl).toHaveBeenCalledWith(
+        "/proposals/" + proposalId
+      );
+    });
+  });
+
+  describe("#onClickSample()", () => {
+    it("should navigate to a sample", () => {
+      const sampleId = "testId";
+      component.onClickSample(sampleId);
+
+      expect(router.navigateByUrl).toHaveBeenCalledWith("/samples/" + sampleId);
+    });
+  });
+
+  describe("#onClickKeyword()", () => {
+    it(" should update datasets keyword filter and navigate to datasets table", () => {
+      dispatchSpy = spyOn(store, "dispatch");
+      const keyword = "test";
+      component.onClickKeyword(keyword);
+
+      expect(dispatchSpy).toHaveBeenCalledTimes(2);
+      expect(dispatchSpy).toHaveBeenCalledWith(new ClearFacetsAction());
+      expect(dispatchSpy).toHaveBeenCalledWith(
+        new AddKeywordFilterAction(keyword)
+      );
+      expect(router.navigateByUrl).toHaveBeenCalledWith("/datasets");
+    });
+  });
+
+  describe("#resetDataset()", () => {
+    it("should return 'null' without confirmation", () => {
+      dispatchSpy = spyOn(store, "dispatch");
+      pipeSpy = spyOn(store, "pipe");
+      const dataset = new Dataset();
+      const res = component.resetDataset(dataset);
+
+      expect(res).toBeNull();
+      expect(dispatchSpy).toHaveBeenCalledTimes(0);
+      expect(pipeSpy).toHaveBeenCalledTimes(0);
+    });
+  });
+
+  describe("#onFileUploaderFilePicked()", () => {
+    it("should set the value of pickedFile", () => {
+      expect(component.pickedFile).toBeUndefined();
+      const file: ReadFile = {
+        name: "test",
+        size: 100,
+        type: "image/png",
+        readMode: ReadMode.dataURL,
+        content: "abc123",
+        underlyingFile: {
+          lastModified: 123,
+          name: "test",
+          size: 100,
+          type: "image/png",
+          slice: () => new Blob().slice()
+        }
+      };
+      component.onFileUploaderFilePicked(file);
+
+      expect(component.pickedFile).toEqual(file);
+    });
+  });
+
+  describe("#onFileUploaderReadEnd()", () => {
+    it("should do nothing if fileCount is not larger than zero", () => {
+      dispatchSpy = spyOn(store, "dispatch");
+
+      component.onFileUploaderReadEnd(0);
+
+      expect(dispatchSpy).toHaveBeenCalledTimes(0);
+    });
+
+    it("should dispatch an AddAttchment action if fileCount is larger than zero", () => {
+      dispatchSpy = spyOn(store, "dispatch");
+
+      component.dataset = new Dataset();
+      component.pickedFile = {
+        name: "test",
+        size: 100,
+        type: "image/png",
+        readMode: ReadMode.dataURL,
+        content: "abc123",
+        underlyingFile: {
+          lastModified: 123,
+          name: "test",
+          size: 100,
+          type: "image/png",
+          slice: () => new Blob().slice()
+        }
+      };
+      component.onFileUploaderReadEnd(1);
+
+      expect(dispatchSpy).toHaveBeenCalledTimes(1);
+      expect(dispatchSpy).toHaveBeenCalledWith(
+        new AddAttachment(component.attachment)
+      );
+    });
+  });
+
+  describe("#updateCaption()", () => {
+    it("should dispatch an UpdateAttachmentCaptionAction", () => {
+      dispatchSpy = spyOn(store, "dispatch");
+      const datasetId = "testDatasetId";
+      const attachmentId = "testAttachmentId";
+      const caption = "Test caption";
+      component.updateCaption(datasetId, attachmentId, caption);
+
+      expect(dispatchSpy).toHaveBeenCalledTimes(1);
+      expect(dispatchSpy).toHaveBeenCalledWith(
+        new UpdateAttachmentCaptionAction(datasetId, attachmentId, caption)
+      );
+    });
+  });
+
+  describe("#deleteAttachment()", () => {
+    it("should dispatch a DeleteAttachment action", () => {
+      dispatchSpy = spyOn(store, "dispatch");
+      const datasetId = "testDatasetId";
+      const attachmentId = "testAttachmentId";
+      component.deleteAttachment(datasetId, attachmentId);
+
+      expect(dispatchSpy).toHaveBeenCalledTimes(1);
+      expect(dispatchSpy).toHaveBeenCalledWith(
+        new DeleteAttachment(datasetId, attachmentId)
+      );
+    });
   });
 });
--- src/app/datasets/dataset-details-dashboard/dataset-details-dashboard.component.ts
+++ src/app/datasets/dataset-details-dashboard/dataset-details-dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy, Inject } from "@angular/core";
 import { Store, select } from "@ngrx/store";
-import { Dataset, UserApi } from "shared/sdk";
+import { Dataset, UserApi, User, Job, Attachment } from "shared/sdk";
 import {
   getCurrentDataset,
   getCurrentDatasetWithoutOrigData,
@@ -9,15 +9,27 @@ import {
   getCurrentAttachments,
   getViewPublicMode
 } from "state-management/selectors/datasets.selectors";
-import { getIsAdmin } from "state-management/selectors/users.selectors";
+import {
+  getIsAdmin,
+  getCurrentUser
+} from "state-management/selectors/users.selectors";
 import { ActivatedRoute, Router } from "@angular/router";
 import { Subscription, Observable } from "rxjs";
-import { pluck } from "rxjs/operators";
+import { pluck, take } from "rxjs/operators";
 import { APP_CONFIG, AppConfig } from "app-config.module";
 import { Message, MessageType } from "state-management/models";
 import { submitJob, getError } from "state-management/selectors/jobs.selectors";
 import { ShowMessageAction } from "state-management/actions/user.actions";
-import { DatablocksAction } from "state-management/actions/datasets.actions";
+import {
+  DatablocksAction,
+  UpdateAttachmentCaptionAction,
+  DeleteAttachment,
+  AddAttachment,
+  ClearFacetsAction,
+  AddKeywordFilterAction
+} from "state-management/actions/datasets.actions";
+import { SubmitAction } from "state-management/actions/jobs.actions";
+import { ReadFile } from "ngx-file-helpers";
 
 @Component({
   selector: "dataset-details-dashboard",
@@ -33,10 +45,94 @@ export class DatasetDetailsDashboardComponent implements OnInit, OnDestroy {
   jwt$: Observable<any>;
 
   dataset: Dataset;
-  datasetPid: string;
   viewPublic: boolean;
+  pickedFile: ReadFile;
+  attachment: Attachment;
 
   private subscriptions: Subscription[] = [];
+
+  onClickKeyword(keyword: string) {
+    this.store.dispatch(new ClearFacetsAction());
+    this.store.dispatch(new AddKeywordFilterAction(keyword));
+    this.router.navigateByUrl("/datasets");
+  }
+
+  onClickProposal(proposalId: string) {
+    const id = encodeURIComponent(proposalId);
+    this.router.navigateByUrl("/proposals/" + id);
+  }
+
+  onClickSample(sampleId: string) {
+    const id = encodeURIComponent(sampleId);
+    this.router.navigateByUrl("/samples/" + id);
+  }
+
+  resetDataset(dataset: Dataset) {
+    if (!confirm("Reset datablocks?")) {
+      return null;
+    }
+    this.store
+      .pipe(
+        select(getCurrentUser),
+        take(1)
+      )
+      .subscribe((user: User) => {
+        const job = new Job();
+        job.emailJobInitiator = user.email;
+        job.jobParams = {};
+        job.jobParams["username"] = user.username;
+        job.creationTime = new Date();
+        job.type = "reset";
+        const fileObj = {};
+        const fileList = [];
+        fileObj["pid"] = dataset["pid"];
+        if (dataset["datablocks"]) {
+          dataset["datablocks"].map(d => {
+            fileList.push(d["archiveId"]);
+          });
+        }
+        fileObj["files"] = fileList;
+        job.datasetList = [fileObj];
+        console.log(job);
+        this.store.dispatch(new SubmitAction(job));
+      });
+  }
+
+  onFileUploaderFilePicked(file: ReadFile) {
+    this.pickedFile = file;
+  }
+
+  onFileUploaderReadEnd(fileCount: number) {
+    if (fileCount > 0) {
+      this.attachment = {
+        thumbnail: this.pickedFile.content,
+        caption: this.pickedFile.name,
+        creationTime: new Date(),
+        id: null,
+        dataset: this.dataset,
+        datasetId: this.dataset.pid,
+        rawDatasetId: null,
+        derivedDatasetId: null,
+        proposal: null,
+        proposalId: null,
+        sample: null,
+        sampleId: null
+      };
+      this.store.dispatch(new AddAttachment(this.attachment));
+    }
+  }
+
+  updateCaption(datasetId: string, attachmentId: string, caption: string) {
+    this.store.dispatch(
+      new UpdateAttachmentCaptionAction(datasetId, attachmentId, caption)
+    );
+  }
+
+  deleteAttachment(dataset_id, dataset_attachment_id) {
+    this.store.dispatch(
+      new DeleteAttachment(dataset_id, dataset_attachment_id)
+    );
+  }
 
   constructor(
     @Inject(APP_CONFIG) public appConfig: AppConfig,
@@ -51,7 +147,15 @@ export class DatasetDetailsDashboardComponent implements OnInit, OnDestroy {
 
     this.subscriptions.push(
       this.route.params.pipe(pluck("id")).subscribe((id: string) => {
-        this.datasetPid = id;
+        if (id) {
+          if (this.viewPublic) {
+            this.store.dispatch(
+              new DatablocksAction(id, { isPublished: this.viewPublic })
+            );
+          } else {
+            this.store.dispatch(new DatablocksAction(id));
+          }
+        }
       })
     );
 
@@ -92,14 +196,6 @@ export class DatasetDetailsDashboardComponent implements OnInit, OnDestroy {
         this.viewPublic = viewPublic;
       })
     );
-
-    if (this.viewPublic) {
-      this.store.dispatch(
-        new DatablocksAction(this.datasetPid, { isPublished: this.viewPublic })
-      );
-    } else {
-      this.store.dispatch(new DatablocksAction(this.datasetPid));
-    }
 
     this.jwt$ = this.userApi.jwt();
   }
