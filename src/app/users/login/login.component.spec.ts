19d17a413ff4abfd866d6c791272ca2c756c5a30
Henrik Johansson <henrik.johansson712@gmail.com>
2019-11-12 11:33:57 +0100

    Added privacy notice dialog

--- src/app/users/login/login.component.spec.ts
+++ src/app/users/login/login.component.spec.ts
@@ -1,7 +1,12 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import {
+  async,
+  ComponentFixture,
+  TestBed,
+  inject
+} from "@angular/core/testing";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
-import { Store } from "@ngrx/store";
+import { Store, StoreModule } from "@ngrx/store";
 import { MockActivatedRoute, MockRouter, MockStore } from "shared/MockStubs";
 
 import { LoginComponent } from "./login.component";
@@ -11,30 +16,38 @@ import {
   MatInputModule,
   MatIconModule,
   MatFormFieldModule,
-  MatButtonModule
+  MatButtonModule,
+  MatDialogModule
 } from "@angular/material";
 import { APP_CONFIG, AppConfigModule } from "app-config.module";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { loginAction } from "state-management/actions/user.actions";
+import { PrivacyDialogComponent } from "users/privacy-dialog/privacy-dialog.component";
 
 describe("LoginComponent", () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
+  let store: MockStore;
+  let dispatchSpy;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      declarations: [LoginComponent],
       imports: [
         AppConfigModule,
         BrowserAnimationsModule,
         FormsModule,
-        ReactiveFormsModule,
         MatButtonModule,
-        MatCheckboxModule,
         MatCardModule,
+        MatDialogModule,
+        MatCheckboxModule,
         MatFormFieldModule,
         MatIconModule,
-        MatInputModule
-      ],
-      declarations: [LoginComponent]
+        MatInputModule,
+        ReactiveFormsModule,
+        StoreModule.forRoot({})
+      ]
     });
     TestBed.overrideComponent(LoginComponent, {
       set: {
@@ -48,8 +61,7 @@ describe("LoginComponent", () => {
             }
           },
           { provide: ActivatedRoute, useClass: MockActivatedRoute },
-          { provide: Router, useClass: MockRouter },
-          { provide: Store, useClass: MockStore }
+          { provide: Router, useClass: MockRouter }
         ]
       }
     });
@@ -62,6 +74,14 @@ describe("LoginComponent", () => {
     fixture.detectChanges();
   });
 
+  beforeEach(inject([Store], (mockStore: MockStore) => {
+    store = mockStore;
+  }));
+
+  afterEach(() => {
+    fixture.destroy();
+  });
+
   it("should create component", () => {
     expect(component).toBeTruthy();
   });
@@ -70,5 +90,32 @@ describe("LoginComponent", () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector("form").textContent).toContain("Username");
     expect(compiled.querySelector("form").textContent).toContain("Password");
+  });
+
+  describe("#openPrivacyDialog()", () => {
+    it("should open the privacy dialog", () => {
+      spyOn(component.dialog, "open");
+
+      component.openPrivacyDialog();
+
+      expect(component.dialog.open).toHaveBeenCalledTimes(1);
+      expect(component.dialog.open).toHaveBeenCalledWith(
+        PrivacyDialogComponent,
+        { width: "auto" }
+      );
+    });
+  });
+
+  describe("#onLogin()", () => {
+    it("should dispatch a loginAction", () => {
+      dispatchSpy = spyOn(store, "dispatch");
+
+      component.onLogin();
+
+      expect(dispatchSpy).toHaveBeenCalledTimes(1);
+      expect(dispatchSpy).toHaveBeenCalledWith(
+        loginAction({ form: { username: "", password: "", rememberMe: true } })
+      );
+    });
   });
 });
