203f24b654da08d1c6980c692332878c8d582525
Luke Gorman <luke.gorman@psi.ch>
2019-11-12 18:25:36 +0100

    toggle feature

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -46,6 +46,7 @@ export const environment = {
   searchSamples: true,
   sftpHost: "login.esss.dk",
   shoppingCartEnabled: true,
+  shoppingCartOnHeader: true,
   tableSciDataEnabled: true,
   userNamePromptEnabled: true,
   userProfileImageEnabled: true
