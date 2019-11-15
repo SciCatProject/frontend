b57eb81864bbe9dc110aeda53f489efbde8c1f62
Henrik Johansson <henrik.johansson712@gmail.com>
2019-11-15 12:35:19 +0100

    Updated attachment test

--- cypress/integration/datasets-attachment.spec.js
+++ cypress/integration/datasets-attachment.spec.js
@@ -47,41 +47,5 @@ describe("Datasets", () => {
         "SciCatLogo.png"
       );
     });
-
-    it("should go to dataset details and add an attachment using the browse button", () => {
-      cy.visit("/datasets");
-
-      cy.get(".mat-row")
-        .contains("Cypress Dataset")
-        .click();
-
-      cy.get(".mat-tab-label")
-        .contains("Attachments")
-        .click();
-
-      cy.fixture("attachment-image").then(file => {
-        cy.get("button")
-          .contains("Browse")
-          .click()
-          .upload(
-            {
-              fileContent: file.content,
-              fileName: file.name,
-              mimeType: file.mimeType
-            },
-            { subjectType: "drag-n-drop" }
-          );
-      });
-
-      cy.wait("@upload").then(response => {
-        expect(response.method).to.eq("POST");
-        expect(response.status).to.eq(200);
-      });
-
-      cy.get(".attachment-card #caption").should(
-        "have.value",
-        "SciCatLogo.png"
-      );
-    });
   });
 });
