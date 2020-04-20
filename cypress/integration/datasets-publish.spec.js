becfad8be6ec5850a6520829486c2f87a0edbbc7
Henrik Johansson <henrik.johansson712@gmail.com>
2020-04-20 13:06:23 +0200

    Updated e2e env file

--- cypress/integration/datasets-publish.spec.js
+++ cypress/integration/datasets-publish.spec.js
@@ -31,8 +31,6 @@ describe("Datasets", () => {
 
       cy.get("#addToBatchButton").click();
 
-      cy.wait(5000);
-
       cy.get("#cartOnHeaderButton").click();
 
       cy.get("a.button").click();
