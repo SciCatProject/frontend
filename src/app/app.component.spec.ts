25a11c787f5e8977778e6d3b0177b5b5c33b8e08
egli <stephan.egli@psi.ch>
2020-11-23 11:52:13 +0100

    Revert "Further adaptation to tests for upgrade"
    This reverts commit bcc9dc7ecb48056523b752c1091b52b6dca74a19.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -34,14 +34,14 @@ describe("AppComponent", () => {
   it("should create the app", async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    app.store = TestBed.inject(Store) as jasmine.SpyObj<Store>;
+    app.store = TestBed.inject(Store);
     expect(app).toBeTruthy();
   }));
 
   it(`should have as title 'SciCat'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    app.store = TestBed.inject(Store) as jasmine.SpyObj<Store>;
+    app.store = TestBed.inject(Store);
     expect(app.title).toContain("SciCat");
   }));
 });
