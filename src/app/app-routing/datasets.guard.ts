ed35f8c3367e2b08a9548c1673dae10c50eee092
Henrik Johansson <henrik.johansson712@gmail.com>
2020-11-25 16:20:13 +0100

    Switch from UserApi to user store

--- src/app/app-routing/datasets.guard.ts
+++ src/app/app-routing/datasets.guard.ts
@@ -5,25 +5,32 @@ import {
   Router,
   RouterStateSnapshot,
 } from "@angular/router";
-import { UserApi } from "shared/sdk";
+import { select, Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { User } from "shared/sdk";
+import { getIsLoggedIn } from "state-management/selectors/user.selectors";
 
 @Injectable({
   providedIn: "root",
 })
 export class DatasetsGuard implements CanActivate {
-  constructor(private router: Router, private userApi: UserApi) {}
+  constructor(private router: Router, private store: Store<User>) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): Promise<boolean> {
-    return this.userApi
-      .getCurrent()
-      .toPromise()
-      .catch(() => {
+  ): Observable<boolean> {
+    return this.store.pipe(
+      select(getIsLoggedIn),
+      map((isLoggedIn) => {
+        if (isLoggedIn) {
+          return true;
+        }
+
         this.router.navigateByUrl("/anonymous" + state.url);
         return false;
       })
-      .then(() => true);
+    );
   }
 }
