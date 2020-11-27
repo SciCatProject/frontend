7fda091e8f2be156d2de6ea05fa66069d5d41420
Henrik Johansson <henrik.johansson712@gmail.com>
2020-11-27 10:26:24 +0100

    Fix tests

--- src/app/shared/modules/shared-table/shared-table.component.spec.ts
+++ src/app/shared/modules/shared-table/shared-table.component.spec.ts
@@ -1,25 +1,54 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { MatTable } from "@angular/material/table";
+import { of } from "rxjs";
+import { SciCatDataSource } from "shared/services/scicat.datasource";
 
-import { SharedTableComponent } from './shared-table.component';
+import { SharedTableComponent } from "./shared-table.component";
 
-describe('SharedTableComponent', () => {
+describe("SharedTableComponent", () => {
   let component: SharedTableComponent;
   let fixture: ComponentFixture<SharedTableComponent>;
 
+  const dataSource = {
+    loadAllData: () => {},
+    loadExportData: () => {},
+  } as SciCatDataSource;
+
+  // const dataTable = jasmine.createSpyObj("MatTable", ["_elementRef"]);
+  const dataTable = ({
+    _elementRef: {
+      nativeElement: {
+        clientWidth: 0,
+      },
+    },
+  } as unknown) as MatTable<Element>;
+
+  const columnsDef = [];
+
+  const paginator = {
+    page: of({}),
+  } as MatPaginator;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SharedTableComponent ]
-    })
-    .compileComponents();
+      declarations: [SharedTableComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SharedTableComponent);
     component = fixture.componentInstance;
+    component.dataSource = dataSource;
+    component.dataTable = dataTable;
+    component.columnsdef = columnsDef;
+    component.sort = new MatSort();
+    component.paginator = paginator;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
--- src/app/shared/services/scicat-data-service.spec.ts
+++ src/app/shared/services/scicat-data-service.spec.ts
@@ -1,16 +1,31 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { TestBed } from "@angular/core/testing";
+import { LoopBackAuth } from "shared/sdk";
 
-import { ScicatDataService } from './scicat-data-service';
+import { ScicatDataService } from "./scicat-data-service";
 
-describe('ScicatDataServiceService', () => {
+describe("ScicatDataServiceService", () => {
   let service: ScicatDataService;
 
+  const loopBackAuth = {
+    getToken: () => ({ id: "test" }),
+  };
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        ScicatDataService,
+        { provide: HttpClient, useClass: HttpClientModule },
+        {
+          provide: LoopBackAuth,
+          useValue: loopBackAuth,
+        },
+      ],
+    });
     service = TestBed.inject(ScicatDataService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
   });
 });
