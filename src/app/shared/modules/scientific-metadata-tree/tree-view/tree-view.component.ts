2ce97f6b23789ec705be9bef7a2b7e9e08a7119b
Linh Nguyen <linh.nguyen@maxiv.lu.se>
2021-03-01 09:07:46 +0100

    filter nodes using css instead

--- src/app/shared/modules/scientific-metadata-tree/tree-view/tree-view.component.ts
+++ src/app/shared/modules/scientific-metadata-tree/tree-view/tree-view.component.ts
@@ -1,8 +1,6 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FlatTreeControl } from '@angular/cdk/tree';
-import { MatTreeFlatDataSource, MatTreeFlattener, MatTreeNestedDataSource } from '@angular/material/tree';
-import { FormGroup } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
 
 export class TreeNode {
   children: TreeNode[];
@@ -16,13 +14,14 @@ export class FlatTreeNode {
   value: any;
   level: number;
   expandable: boolean;
+  visible: boolean;
 }
 @Component({
   selector: 'app-tree-view',
   templateUrl: './tree-view.component.html',
   styleUrls: ['./tree-view.component.scss'],
 })
-export class TreeViewComponent implements OnInit{
+export class TreeViewComponent implements OnInit {
   treeControl: FlatTreeControl<FlatTreeNode>;
   treeFlattener: MatTreeFlattener<TreeNode, FlatTreeNode>;
   dataSource: MatTreeFlatDataSource<TreeNode, FlatTreeNode>;
@@ -40,24 +39,23 @@ export class TreeViewComponent implements OnInit{
 
   }
   ngOnInit() {
-    this.dataTree = this.buildDataTree (this.data, 0);
-    this.filteredDataTree = this.dataTree;
-    this.dataSource.data = this.filteredDataTree;
+    this.dataTree = this.buildDataTree(this.data, 0);
+    this.dataSource.data = this.dataTree;
   }
-  buildDataTree(obj: {[key:string]: any}, level: number): TreeNode[]{
+  buildDataTree(obj: { [key: string]: any }, level: number): TreeNode[] {
     return Object.keys(obj).reduce<TreeNode[]>((accumulator, key) => {
       const value = obj[key];
       const node = new TreeNode();
       node.key = key;
-      if (value !== null){
-        if (typeof value === "object"){
-          node.children = this.buildDataTree(value, level+1);
-        }else{
+      if (value !== null) {
+        if (typeof value === "object") {
+          node.children = this.buildDataTree(value, level + 1);
+        } else {
           node.value = value;
         }
       }
       return accumulator.concat(node);
-    },[]);
+    }, []);
   }
   get filterText(): string {
     return this._filterText;
@@ -65,38 +63,49 @@ export class TreeViewComponent implements OnInit{
   set filterText(value: string) {
     this._filterText = value;
     this.treeControl.collapseAll();
-    this.filteredDataTree = this._filterText ? this.performFilter(this._filterText) : this.dataTree;
-    this.dataSource.data = this.filteredDataTree;
+    this._filterText ? this.performFilter(this._filterText) : this.showAllNodes();
   }
-  performFilter(filterKey: string): TreeNode[] {
-    filterKey = filterKey.toLocaleLowerCase();
-    const filteredNestedNodes: Set<TreeNode> = new Set<TreeNode>();
-
-    this.flatNodeMap.forEach((nestedNode: TreeNode, flatNode: FlatTreeNode,) => {
-      if (flatNode.key.toLowerCase().indexOf(filterKey) !== - 1) {
-        // Node has no parent
-        if (flatNode.level === 0) {
-          filteredNestedNodes.add(nestedNode);
-          return;
-          // Node has parent
-        } else {
-          let currentNode = flatNode;
-          while (currentNode !== null) {
-            const parentNode = this.getParentNode(currentNode);
-            if (parentNode) {
-              // Expand parent node contains filter text
-              this.treeControl.expand(parentNode);
-              // Only add the top parent to the list
-              if (parentNode.level === 0) {
-                filteredNestedNodes.add(this.flatNodeMap.get(parentNode));
-              }
-            }
-            currentNode = parentNode;
-          };
-        }
+  isVisible(_: number, node: FlatTreeNode) {
+    return node.visible;
+  }
+  showAllNodes() {
+    this.treeControl.dataNodes.forEach((node: FlatTreeNode) => node.visible = true);
+  }
+  hideAllNodes() {
+    this.treeControl.dataNodes.forEach((node: FlatTreeNode) => node.visible = false)
+  }
+  performFilter(filterText: string) {
+    filterText = filterText.toLowerCase();
+    const filteredNodes = this.treeControl.dataNodes.filter((node: FlatTreeNode) => node.key.toLowerCase().indexOf(filterText) !== -1);
+    this.hideAllNodes();
+    filteredNodes.forEach((node: FlatTreeNode) => {
+      node.visible = true;
+      if (node.expandable) {
+        const nestedNode = this.flatNodeMap.get(node);
+        this.setChildrenVisible(nestedNode.children);
+      }
+      if (node.level > 0) {
+        let currentNode = node;
+        while (currentNode !== null) {
+          const parentNode = this.getParentNode(currentNode);
+          if (parentNode) {
+            // Expand parent node contains filter text
+            parentNode.visible = true;
+            this.treeControl.expand(parentNode);
+          }
+          currentNode = parentNode;
+        };
       }
     });
-    return Array.from(filteredNestedNodes);
+  }
+  setChildrenVisible(children: TreeNode[]) {
+    children.map((node: TreeNode) => {
+      const flatNode = this.nestNodeMap.get(node);
+      flatNode.visible = true;
+      if (node.children) {
+        this.setChildrenVisible(node.children);
+      }
+    });
   }
   getParentNode(node: FlatTreeNode): FlatTreeNode | null {
     const currentLevel = this.getLevel(node);
@@ -114,27 +123,28 @@ export class TreeViewComponent implements OnInit{
     }
     return null;
   }
-  toggleExpand(){
+  toggleExpand() {
     this.expanded = !this.expanded;
-    this.expanded? this.treeControl.expandAll() : this.treeControl.collapseAll();
+    this.expanded ? this.treeControl.expandAll() : this.treeControl.collapseAll();
   }
   getLevel = (node: FlatTreeNode) => node.level;
   isExpandable = (node: FlatTreeNode) => node.expandable;
   getChildren = (node: TreeNode): TreeNode[] => node.children;
   transformer = (node: TreeNode, level: number) => {
     const existingNode = this.nestNodeMap.get(node);
-    const flatNode = existingNode && existingNode.key === node.key? existingNode : new FlatTreeNode();
+    const flatNode = existingNode && existingNode.key === node.key ? existingNode : new FlatTreeNode();
     flatNode.key = node.key;
     flatNode.level = level;
     flatNode.value = node.value;
     flatNode.expandable = !!node.children?.length;
+    flatNode.visible = true;
     this.flatNodeMap.set(flatNode, node);
     this.nestNodeMap.set(node, flatNode);
     return flatNode;
   }
 
   hasChild = (_: number, _nodeData: FlatTreeNode) => _nodeData.expandable;
-  getPadding(node: FlatTreeNode){
+  getPadding(node: FlatTreeNode) {
     const indentPixel = 40;
     return (node.level * indentPixel).toString();
   }
