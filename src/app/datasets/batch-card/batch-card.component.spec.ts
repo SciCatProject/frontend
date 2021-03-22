9bc36b8b8a5652abd507917d891e9ce7f5e9fe5c
Henrik Johansson <henrik.johansson712@gmail.com>
2021-03-22 11:39:34 +0100

    test(batch-card): add spec file

---
+++ src/app/datasets/batch-card/batch-card.component.spec.ts
@@ -0,0 +1,56 @@
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
+import { MatCardModule } from "@angular/material/card";
+import { MockStore, provideMockStore } from "@ngrx/store/testing";
+import { getDatasetsInBatch } from "state-management/selectors/datasets.selectors";
+import { DatasetState } from "state-management/state/datasets.store";
+import { BatchCardComponent } from "./batch-card.component";
+
+describe("BatchCardComponent", () => {
+  let component: BatchCardComponent;
+  let fixture: ComponentFixture<BatchCardComponent>;
+
+  let store: MockStore<DatasetState>;
+  let dispatchSpy: jasmine.Spy;
+
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        schemas: [NO_ERRORS_SCHEMA],
+        declarations: [BatchCardComponent],
+        imports: [MatCardModule],
+        providers: [
+          provideMockStore({
+            selectors: [{ selector: getDatasetsInBatch, value: [] }],
+          }),
+        ],
+      }).compileComponents();
+
+      store = TestBed.inject(MockStore);
+    })
+  );
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BatchCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    fixture.destroy();
+  });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe("#clear()", () => {
+    it("should dispatch a clearBatchAction", () => {
+      dispatchSpy = spyOn(store, "dispatch");
+
+      component.clear();
+
+      expect(dispatchSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+});
