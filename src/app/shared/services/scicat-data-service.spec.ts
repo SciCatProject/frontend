4fdb5d9ba5dfe4b8b5e34a24fd2bb6113ca5e5ea
egli <stephan.egli@psi.ch>
2021-03-23 11:20:47 +0100

    Added test case for scicat-data-service

--- src/app/shared/services/scicat-data-service.spec.ts
+++ src/app/shared/services/scicat-data-service.spec.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { TestBed } from "@angular/core/testing";
 import { LoopBackAuth } from "shared/sdk";
-
+import { Column } from "shared/modules/shared-table/shared-table.module";
 import { ScicatDataService } from "./scicat-data-service";
+var moment = require('moment-timezone');
 
 describe("ScicatDataServiceService", () => {
   let service: ScicatDataService;
@@ -28,4 +29,50 @@ describe("ScicatDataServiceService", () => {
   it("should be created", () => {
     expect(service).toBeTruthy();
   });
+
+  describe("#createColumnFilterMongoExpression()", () => {
+    it("should return the mongo filter expression", () => {
+      let columns: Column[];
+      columns = [
+        {
+          "id": "type",
+          "label": "Type",
+          "icon": "unarchive",
+          "canSort": true,
+          "matchMode": "is",
+          "hideOrder": 2
+        },
+        {
+          "id": "creationTime",
+          "icon": "schedule",
+          "label": "Created at local time",
+          "format": "date medium ",
+          "canSort": true,
+          "matchMode": "between",
+          "hideOrder": 3,
+          "sortDefault": "desc"
+        },
+      ];
+
+      const filterExpressions = {
+        "type": "retrieve",
+        "creationTime.start": "2021-02-01",
+        "creationTime.end": "2021-02-23"
+      }
+
+      const result = service.createColumnFilterMongoExpression(columns, filterExpressions);
+
+      expect(result).toEqual({
+        "type": "retrieve",
+        "creationTime": {
+          "begin": "2021-01-31T23:00:00.000Z",
+          "end": "2021-02-23T23:00:00.000Z"
+        }
+      });
+
+    });
+  });
+
 });
+
+
