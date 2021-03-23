57c11464da46df55846be4143af91bfcb40e09d8
Henrik Johansson <henrik.johansson712@gmail.com>
2021-03-23 09:12:42 +0100

    test(adauthservice): add test

--- src/app/users/adauth.service.spec.ts
+++ src/app/users/adauth.service.spec.ts
@@ -3,12 +3,28 @@ import { HttpClient } from "@angular/common/http";
 import { ADAuthService } from "./adauth.service";
 
 import { MockHttp } from "shared/MockStubs";
+import { APP_CONFIG } from "app-config.module";
 
 describe("ADAuthService", () => {
+  let service: ADAuthService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ADAuthService, { provide: HttpClient, useClass: MockHttp }]
+      providers: [
+        ADAuthService,
+        {
+          provide: APP_CONFIG,
+          useValue: { externalAuthEndpoint: "/auth/msad" },
+        },
+        { provide: HttpClient, useClass: MockHttp },
+      ],
     });
+
+    service = TestBed.inject(ADAuthService);
+  });
+
+  it("should be created", () => {
+    expect(service).toBeTruthy();
   });
 
   // TODO update to call login method and test resuling URL
