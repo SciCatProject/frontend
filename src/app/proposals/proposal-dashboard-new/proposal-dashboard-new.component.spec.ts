b0b4f1fd48afb0769e33ba1f2a26e45533619f4c
Linh Nguyen <linh.nguyen@maxiv.lu.se>
2021-05-06 11:19:04 +0200

    fixed failed test case

--- src/app/proposals/proposal-dashboard-new/proposal-dashboard-new.component.spec.ts
+++ src/app/proposals/proposal-dashboard-new/proposal-dashboard-new.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { Router } from "@angular/router";
+import { MockRouter } from "shared/MockStubs";
+import { ExportExcelService } from "shared/services/export-excel.service";
+import { ScicatDataService } from "shared/services/scicat-data-service";
 
 import { ProposalDashboardNewComponent } from "./proposal-dashboard-new.component";
 
-describe("ProposalDashboardNewComponent", () => {
+fdescribe("ProposalDashboardNewComponent", () => {
   let component: ProposalDashboardNewComponent;
   let fixture: ComponentFixture<ProposalDashboardNewComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProposalDashboardNewComponent ]
+      declarations: [ ProposalDashboardNewComponent ],
+      providers: [
+        { provide: ExportExcelService, useValue: {} },
+        { provide: Router, useClass: MockRouter },
+        { provide: ScicatDataService, useValue: {} },
+      ]
     })
     .compileComponents();
   });
