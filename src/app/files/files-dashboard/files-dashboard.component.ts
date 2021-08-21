3d47eb79cafbefe7e16a67bf10a682f9d40c1cd8
Linh Nguyen <linh.nguyen@maxiv.lu.se>
2021-08-21 11:22:35 +0200

    fixed deepscan

--- src/app/files/files-dashboard/files-dashboard.component.ts
+++ src/app/files/files-dashboard/files-dashboard.component.ts
@@ -2,7 +2,6 @@ import { Component, OnDestroy } from "@angular/core";
 import { SciCatDataSource } from "../../shared/services/scicat.datasource";
 import { ScicatDataService } from "../../shared/services/scicat-data-service";
 import { ExportExcelService } from "../../shared/services/export-excel.service";
-import { Router } from "@angular/router";
 import { Column } from "shared/modules/shared-table/shared-table.module";
 
 @Component({
