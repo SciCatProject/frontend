4e7d558dbbfdb239db54597b3b7a66f22347beaf
Henrik Johansson <henrik.johansson712@gmail.com>
2021-12-16 14:14:59 +0100

    Create drag and drop directives

---
+++ src/app/shared/modules/file-uploader/directives/drag-and-drop.directive.ts
@@ -0,0 +1,40 @@
+import {
+  Directive,
+  EventEmitter,
+  HostBinding,
+  HostListener,
+  Output,
+} from "@angular/core";
+
+@Directive({
+  selector: "[appDragAndDrop]",
+})
+export class DragAndDropDirective {
+  @HostBinding("class.fileover") fileOver = false;
+  @Output() fileDropped = new EventEmitter<unknown>();
+
+  // Dragover listener
+  @HostListener("dragover", ["$event"]) onDragOver(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.fileOver = true;
+  }
+
+  // Dragleave listener
+  @HostListener("dragleave", ["$event"]) public onDragLeave(event: DragEvent) {
+    event.preventDefault();
+    this.fileOver = false;
+  }
+
+  // Drop listener
+  @HostListener("drop", ["$event"]) public onDrop(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.fileOver = false;
+    const files = event.dataTransfer.files;
+
+    if (files.length > 0) {
+      this.fileDropped.emit(files);
+    }
+  }
+}
