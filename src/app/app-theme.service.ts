fa12ac79d3314fafebf7eeface3a463e8e99b63e
Henrik Johansson <henrik.johansson712@gmail.com>
2022-03-24 16:32:20 +0100

    Create app theme service for setting theme

---
+++ src/app/app-theme.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { light, Theme } from "theme";
+
+@Injectable()
+export class AppThemeService {
+  private activeTheme: Theme = light;
+
+  constructor(private http: HttpClient) {}
+
+  async loadTheme(): Promise<void> {
+    try {
+      this.activeTheme = (await this.http
+        .get("/client/theme.json")
+        .toPromise()) as Theme;
+    } catch (err) {
+      console.log("No theme available in backend, using local theme.");
+      this.activeTheme = (await this.http
+        .get("/assets/theme.json")
+        .toPromise()) as Theme;
+    }
+    this.setActiveTheme();
+  }
+
+  setActiveTheme(): void {
+    Object.keys(this.activeTheme.properties).forEach((property) => {
+      document.documentElement.style.setProperty(
+        property,
+        this.activeTheme.properties[property]
+      );
+    });
+  }
+}
