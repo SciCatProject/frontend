fd3077a7701e4450bd0587873ef4b6209c0f11a8
Henrik Johansson <henrik.johansson712@gmail.com>
2022-06-16 13:46:55 +0200

    Set up table data

--- src/app/datasets/related-datasets/related-datasets.component.ts
+++ src/app/datasets/related-datasets/related-datasets.component.ts
@@ -1,136 +1,92 @@
-import {
-  AfterViewChecked,
-  ChangeDetectorRef,
-  Component,
-  OnDestroy,
-  OnInit,
-} from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
+import { DatePipe } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 import { Store } from "@ngrx/store";
-import { AppConfigService } from "app-config.service";
-import { Subscription } from "rxjs";
-import { Column } from "shared/modules/shared-table/shared-table.module";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { TableColumn } from "shared/modules/table/table.component";
 import { Dataset } from "shared/sdk";
-import { ExportExcelService } from "shared/services/export-excel.service";
-import { ScicatDataService } from "shared/services/scicat-data-service";
-import { SciCatDataSource } from "shared/services/scicat.datasource";
-import { selectCurrentDataset } from "state-management/selectors/datasets.selectors";
+import { fetchRelatedDatasetsAction } from "state-management/actions/datasets.actions";
+import { selectRelatedDatasets } from "state-management/selectors/datasets.selectors";
 
 @Component({
   selector: "app-related-datasets",
   templateUrl: "./related-datasets.component.html",
   styleUrls: ["./related-datasets.component.scss"],
 })
-export class RelatedDatasetsComponent
-  implements OnInit, OnDestroy, AfterViewChecked {
-  dataset$ = this.store.select(selectCurrentDataset);
-  datasetSubscription = new Subscription();
-  dataset: Dataset;
-  dataSource: SciCatDataSource;
+export class RelatedDatasetsComponent implements OnInit {
+  tableData$: Observable<Partial<Dataset>[]> = this.store
+    .select(selectRelatedDatasets)
+    .pipe(map((relatedDatasets) => this.formatTableData(relatedDatasets)));
 
-  columns: Column[] = [
+  tableColumns: TableColumn[] = [
     {
-      id: "datasetName",
-      label: "Name",
-      canSort: true,
-      icon: "perm_device_information",
-      matchMode: "contains",
-      hideOrder: 0,
+      name: "datasetName",
+      icon: "fingerprint",
+      sort: true,
+      inList: true,
     },
     {
-      id: "sourceFolder",
-      label: "Source Folder",
-      canSort: true,
-      icon: "perm_device_information",
-      matchMode: "contains",
-      hideOrder: 1,
+      name: "sourceFolder",
+      icon: "description",
+      sort: true,
+      inList: true,
     },
     {
-      id: "size",
-      label: "Size",
-      canSort: true,
-      icon: "perm_device_information",
-      matchMode: "contains",
-      hideOrder: 2,
+      name: "size",
+      icon: "face",
+      sort: true,
+      inList: true,
     },
     {
-      id: "type",
-      label: "Type",
-      canSort: true,
-      icon: "perm_device_information",
-      matchMode: "contains",
-      hideOrder: 3,
-      filterDefault: "derived"
+      name: "type",
+      icon: "date_range",
+      sort: true,
+      inList: true,
     },
     {
-      id: "createdAt",
-      label: "Creation Time",
-      format: "date",
-      canSort: true,
-      icon: "perm_device_information",
-      matchMode: "between",
-      hideOrder: 4,
-      sortDefault: "desc",
+      name: "createdAt",
+      icon: "group",
+      sort: true,
+      inList: true,
     },
     {
-      id: "owner",
-      label: "Owner",
-      canSort: true,
-      icon: "perm_device_information",
-      matchMode: "contains",
-      hideOrder: 5,
+      name: "owner",
+      icon: "group",
+      sort: true,
+      inList: true,
     },
   ];
 
-  tableDefinition = {
-    collection: "Datasets",
-    columns: this.columns,
-  };
-
   constructor(
-    private appConfigService: AppConfigService,
-    private cdRef: ChangeDetectorRef,
-    private exportService: ExportExcelService,
-    private dataService: ScicatDataService,
-    private route: ActivatedRoute,
+    private datePipe: DatePipe,
     private router: Router,
     private store: Store
-  ) {
-    this.dataSource = new SciCatDataSource(
-      this.appConfigService,
-      this.dataService,
-      this.exportService,
-      this.tableDefinition
-    );
-  }
+  ) {}
 
-  ngOnInit(): void {
-    this.datasetSubscription = this.dataset$.subscribe((dataset) => {
-      this.dataset = dataset;
-    });
-
-    let url = this.router.url;
-    console.log({ url });
-
-    if (this.dataset && this.dataset.type === "raw") {
-      this.router.navigate([], {
-        queryParams: { fields: JSON.stringify({type: "derived"}) },
-        queryParamsHandling: "merge",
-      });
+  formatTableData(datasets: Dataset[]): any {
+    if (datasets) {
+      return datasets.map((dataset) => ({
+        pid: dataset.pid,
+        datasetName: dataset.datasetName,
+        sourceFolder: dataset.sourceFolder,
+        size: dataset.size,
+        type: dataset.type,
+        createdAt: this.datePipe.transform(
+          dataset.createdAt,
+          "yyyy-MM-dd, hh:mm"
+        ),
+        owner: dataset.owner,
+      }));
     }
   }
 
-  ngOnDestroy(): void {
-    this.datasetSubscription.unsubscribe();
-    this.dataSource.disconnectExportData();
-  }
-
-  ngAfterViewChecked(): void {
-    this.cdRef.detectChanges();
-  }
-
-  onRowClick(dataset: Dataset) {
+  onRowClick(dataset: Dataset): void {
     const pid = encodeURIComponent(dataset.pid);
     this.router.navigateByUrl("/datasets/" + pid);
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(fetchRelatedDatasetsAction());
   }
 }
