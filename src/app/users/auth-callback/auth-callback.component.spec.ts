8415a81e9e72d0d3045ce4946a7266ee2a0d6f65
jekabskarklins <jekabs.karklins@ess.eu>
2022-11-10 12:32:35 +0100

    fix: fixing auth callback component test

--- src/app/users/auth-callback/auth-callback.component.spec.ts
+++ src/app/users/auth-callback/auth-callback.component.spec.ts
@@ -1,24 +1,65 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
-
+import {
+  ComponentFixture,
+  inject,
+  TestBed,
+  waitForAsync,
+} from "@angular/core/testing";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Store, StoreModule } from "@ngrx/store";
+import { provideMockStore } from "@ngrx/store/testing";
+import { MockActivatedRoute, MockRouter, MockStore } from "shared/MockStubs";
+import { selectLoginPageViewModel } from "state-management/selectors/user.selectors";
 import { AuthCallbackComponent } from "./auth-callback.component";
 
 describe("AuthCallbackComponent", () => {
   let component: AuthCallbackComponent;
   let fixture: ComponentFixture<AuthCallbackComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  let store: MockStore;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
       declarations: [AuthCallbackComponent],
-    }).compileComponents();
+      imports: [StoreModule.forRoot({})],
+      providers: [
+        provideMockStore({
+          selectors: [
+            {
+              selector: selectLoginPageViewModel,
+              value: { isLoggedIn: true, isLoggingIn: false },
+            },
+          ],
+        }),
+      ],
+    });
+    TestBed.overrideComponent(AuthCallbackComponent, {
+      set: {
+        providers: [
+          { provide: ActivatedRoute, useClass: MockActivatedRoute },
+          { provide: Router, useClass: MockRouter },
+        ],
+      },
+    });
+    TestBed.compileComponents();
+  }));
+
+  beforeEach(inject([Store], (mockStore: MockStore) => {
+    store = mockStore;
+  }));
+
+  afterEach(() => {
+    fixture.destroy();
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AuthCallbackComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it("should create", () => {
-    expect(component).toBeTruthy();
+  describe("component construction", () => {
+    beforeEach(() => {
+      fixture = TestBed.createComponent(AuthCallbackComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+      TestBed.compileComponents();
+    });
+    it("should create", () => {
+      expect(component).toBeTruthy();
+    });
   });
 });
