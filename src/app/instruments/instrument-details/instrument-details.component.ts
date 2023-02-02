56656fad7d5a62fb3fec012b76da60f723c69dcf
Martin Voigt <m.voigt@hzdr.de>
2023-02-02 18:45:38 +0100

    Implement support for customMetadata of instruments in metadataStructure "tree"

--- src/app/instruments/instrument-details/instrument-details.component.ts
+++ src/app/instruments/instrument-details/instrument-details.component.ts
@@ -1,21 +1,32 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Store } from "@ngrx/store";
+import {fromEvent, Subscription} from "rxjs";
 import {
   fetchInstrumentAction,
   saveCustomMetadataAction,
 } from "state-management/actions/instruments.actions";
 import { selectCurrentInstrument } from "state-management/selectors/instruments.selectors";
+import {EditableComponent} from "app-routing/pending-changes.guard";
+import {AppConfigService} from "app-config.service";
 
 @Component({
   selector: "app-instrument-details",
   templateUrl: "./instrument-details.component.html",
   styleUrls: ["./instrument-details.component.scss"],
 })
-export class InstrumentDetailsComponent implements OnInit {
-  instrument$ = this.store.select(selectCurrentInstrument);
+export class InstrumentDetailsComponent implements OnInit, OnDestroy, EditableComponent {
+  private _hasUnsavedChanges = false;
 
-  constructor(private route: ActivatedRoute, private store: Store) {}
+  instrument$ = this.store.select(selectCurrentInstrument);
+  appConfig = this.appConfigService.getConfig();
+  subscriptions: Subscription[] = [];
+
+  constructor(
+    private appConfigService: AppConfigService,
+    private route: ActivatedRoute,
+    private store: Store
+  ) {}
 
   onSaveCustomMetadata(
     pid: string,
@@ -27,5 +38,24 @@ export class InstrumentDetailsComponent implements OnInit {
   ngOnInit() {
     const pid = this.route.snapshot.paramMap.get("id") ?? "";
     this.store.dispatch(fetchInstrumentAction({ pid }));
+
+    // Prevent user from reloading page if there are unsaved changes
+    this.subscriptions.push(
+      fromEvent(window, "beforeunload").subscribe((event) => {
+        if (this.hasUnsavedChanges()) {
+          event.preventDefault();
+        }
+      })
+    );
+  }
+
+  hasUnsavedChanges() {
+    return this._hasUnsavedChanges;
+  }
+  onHasUnsavedChanges($event: boolean) {
+    this._hasUnsavedChanges = $event;
+  }
+  ngOnDestroy() {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
   }
 }
