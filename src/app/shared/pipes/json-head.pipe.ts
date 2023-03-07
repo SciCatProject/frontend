1ae4ed3ec35b4cacb2980ae0c45ef9ef6d6bf3ee
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2023-03-07 14:59:03 +0100

    Fix metadata display in table
    use a logic similar the detail page display
    
    Change-Id: Ie024668af93bef57f6c4130b3bb1b988d71431fe

--- src/app/shared/pipes/json-head.pipe.ts
+++ src/app/shared/pipes/json-head.pipe.ts
@@ -35,7 +35,7 @@ export class JsonHeadPipe implements PipeTransform {
       key2 = " ";
       val2 = " ";
     }
-    outputString = key1 + ":" + val1 + "\n" + key2 + ":" + val2;
+    outputString = key1 + ":" + (val1?.value.toString() || val1)+ "\n" + key2 + ":" + val2;
     if (key1 === undefined) {
       outputString = "No metadata found";
     }
