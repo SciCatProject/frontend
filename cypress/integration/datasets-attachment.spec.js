1c14ba05144f900883a0858eff40ad7f93ef1db0
Martin Trajanovski <martin.trajanovski@gmail.com>
2023-03-20 13:31:31 +0100

    add delete attachment e2e test

--- cypress/integration/datasets-attachment.spec.js
+++ cypress/integration/datasets-attachment.spec.js
@@ -1,5 +1,5 @@
 /// <reference types="Cypress" />
-var path = require('path');
+var path = require("path");
 
 describe("Dataset attachments", () => {
   beforeEach(() => {
@@ -51,6 +51,8 @@ describe("Dataset attachments", () => {
       cy.wait("@upload").then(({ request, response }) => {
         expect(request.method).to.eq("POST");
         expect(response.statusCode).to.eq(201);
+
+        cy.get(".snackbar-success").should("exist");
       });
 
       cy.get(".attachment-card #caption").should(
@@ -86,6 +88,36 @@ describe("Dataset attachments", () => {
 
       const downloadsFolder = Cypress.config("downloadsFolder");
       cy.readFile(path.join(downloadsFolder, "SciCatLogo.png")).should("exist");
+    });
+
+    it("should be able to delete dataset attachment", () => {
+      cy.visit("/datasets");
+
+      cy.get(".dataset-table mat-table mat-header-row").should("exist");
+
+      cy.finishedLoading();
+
+      cy.get('input[type="search"][data-placeholder="Text Search"]')
+        .clear()
+        .type("Cypress");
+
+      cy.isLoading();
+
+      cy.finishedLoading();
+
+      cy.get(".mat-row").contains("Cypress Dataset").first().click();
+
+      cy.isLoading();
+
+      cy.finishedLoading();
+
+      cy.get(".mat-tab-link").contains("Attachments").click();
+
+      cy.get(".delete-button").click();
+
+      cy.get(".attachment-card #caption").should("not.exist");
+
+      cy.get(".snackbar-success").should("exist");
     });
   });
 });
