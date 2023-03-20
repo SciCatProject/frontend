327f47e8878855a22a7e52de249a832a38775491
Martin Trajanovski <martin.trajanovski@gmail.com>
2023-03-20 13:23:34 +0100

    add e2e tests for attachment downloading

--- cypress/integration/datasets-attachment.spec.js
+++ cypress/integration/datasets-attachment.spec.js
@@ -1,24 +1,20 @@
 /// <reference types="Cypress" />
+var path = require('path');
 
-describe("Datasets", () => {
+describe("Dataset attachments", () => {
   beforeEach(() => {
     cy.login(Cypress.config("username"), Cypress.config("password"));
-
-    cy.createDataset("raw");
 
     cy.intercept("POST", "/api/v3/Datasets/**/*").as("upload");
   });
 
-  afterEach(() => {
-    cy.login(
-      Cypress.config("secondaryUsername"),
-      Cypress.config("secondaryPassword")
-    );
+  after(() => {
     cy.removeDatasets();
   });
 
-  describe("Add Attachment", () => {
+  describe("Attachment tests", () => {
     it("should go to dataset details and add an attachment using the dropzone", () => {
+      cy.createDataset("raw");
       cy.visit("/datasets");
 
       cy.get(".dataset-table mat-table mat-header-row").should("exist");
@@ -61,6 +57,35 @@ describe("Datasets", () => {
         "have.value",
         "SciCatLogo.png"
       );
+    });
+
+    it("should be able to download dataset attachment", () => {
+      cy.visit("/datasets");
+
+      cy.get(".dataset-table mat-table mat-header-row").should("exist");
+
+      cy.finishedLoading();
+
+      cy.get('input[type="search"][data-placeholder="Text Search"]')
+        .clear()
+        .type("Cypress");
+
+      cy.isLoading();
+
+      cy.finishedLoading();
+
+      cy.get(".mat-row").contains("Cypress Dataset").first().click();
+
+      cy.isLoading();
+
+      cy.finishedLoading();
+
+      cy.get(".mat-tab-link").contains("Attachments").click();
+
+      cy.get(".download-button").click();
+
+      const downloadsFolder = Cypress.config("downloadsFolder");
+      cy.readFile(path.join(downloadsFolder, "SciCatLogo.png")).should("exist");
     });
   });
 });
