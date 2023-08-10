a196114943d92515b884b9f99bc158325233d2d2
Max Novelli <max.novelli@ess.eu>
2023-08-10 15:12:56 +0200

    changed permissions to make dataset public according to new csl model

--- src/app/datasets/dataset-detail/dataset-detail.component.ts
+++ src/app/datasets/dataset-detail/dataset-detail.component.ts
@@ -163,6 +163,7 @@ export class DatasetDetailComponent
     return this._hasUnsavedChanges;
   }
 
+  /*
   isPI(): boolean {
     if (this.user && this.dataset) {
       if (this.user.username === "admin") {
@@ -187,6 +188,7 @@ export class DatasetDetailComponent
     }
     return false;
   }
+  */
 
   onClickKeyword(keyword: string) {
     this.store.dispatch(clearFacetsAction());
