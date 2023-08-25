41a88e8800723658f9e1d7b6dd6176100c0d321b
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2023-08-25 11:06:14 +0200

    Fix shared/modules/scientific-metadata-tree tests
    Change-Id: I41d474910c3d36a8a2a9ed13af324c7e4f624096

--- src/app/shared/modules/scientific-metadata-tree/base-classes/tree-base.spec.ts
+++ src/app/shared/modules/scientific-metadata-tree/base-classes/tree-base.spec.ts
@@ -1,12 +1,11 @@
 import { DatePipe } from "@angular/common";
 import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
-import { MatDialog, MatDialogModule } from "@angular/material/dialog";
-import { MatMenuModule } from "@angular/material/menu";
-import { MatSnackBar, MatSnackBarModule } from "@angular/material/snack-bar";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatDialog } from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
 import { DateTime } from "luxon";
 import { FlatNode, TreeNode } from "../base-classes/tree-base";
 import { TreeEditComponent } from "../tree-edit/tree-edit.component";
+import { ScientificMetadataTreeModule } from "../scientific-metadata-tree.modules";
 
 describe("TreeBaseComponent", () => {
   let component: TreeEditComponent;
@@ -16,10 +15,7 @@ describe("TreeBaseComponent", () => {
     TestBed.configureTestingModule({
       declarations: [TreeEditComponent],
       imports: [
-        MatDialogModule,
-        MatSnackBarModule,
-        MatMenuModule,
-        BrowserAnimationsModule
+        ScientificMetadataTreeModule
       ],
       providers: [
         MatDialog,
