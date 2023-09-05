86c217fe7f370f437bd91157dd90fd7f63d6f8f3
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2023-09-05 12:57:07 +0200

    bump filesize from 6.4.0 to 10.0.7
    Change-Id: Ia299c444d5ddc9060574a93202a624f400142839

--- src/app/shared/pipes/dynamicPipe.pipe.spec.ts
+++ src/app/shared/pipes/dynamicPipe.pipe.spec.ts
@@ -8,6 +8,6 @@ describe("Pipe: FileSize", () => {
   it("converts properly", () => {
     const injector = Injector.create({providers: [{provide: FileSizePipe}]});
     dynamicPipe = new DynamicPipe(injector);
-    expect(dynamicPipe.transform(65536, FileSizePipe, [])).toBe("64 KB");
+    expect(dynamicPipe.transform(65536, FileSizePipe, [])).toBe("64 KiB");
   });
 });
