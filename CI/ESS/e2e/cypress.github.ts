79f0059cee0b61c75c1c39fda5037e603a2d6496
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2023-09-05 12:53:39 +0200

    Bump cypress to 13.1.0
    Change-Id: I96558094ba1a0244c771ad3d83d55941246b4c53

---
+++ CI/ESS/e2e/cypress.github.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e:
+  {
+    "baseUrl": "http://localhost:8080",
+    "lbBaseUrl": "http://localhost:8080/api/v3",
+    "lbLoginEndpoint": "/Users/login",
+    "lbTokenPrefix": "Bearer ",
+    "viewportWidth": 1280,
+    "username": "admin",
+    "password": "am2jf70TPNZsSan",
+    "secondaryUsername": "archiveManager",
+    "secondaryPassword": "aman",
+    "guestUsername":"user1",
+    "guestUserEmail":"user1@your.site",
+    "guestPassword":"a609316768619f154ef58db4d847b75e",
+    "defaultCommandTimeout": 10000,
+    "retries": 1,
+
+  }
+})
--- CI/ESS/e2e/cypress.nestjs.ts
+++ CI/ESS/e2e/cypress.nestjs.ts
@@ -1,6 +1,10 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e:
 {
-  "baseUrl": "http://localhost:8080",
-  "lbBaseUrl": "http://localhost:8080/api/v3",
+  "baseUrl": "http://localhost:80",
+  "lbBaseUrl": "http://localhost:80/api/v3",
   "lbLoginEndpoint": "/Users/login",
   "lbTokenPrefix": "Bearer ",
   "viewportWidth": 1280,
@@ -13,4 +17,5 @@
   "guestPassword":"a609316768619f154ef58db4d847b75e",
   "defaultCommandTimeout": 10000,
   "retries": 1
-}
+},
+})
