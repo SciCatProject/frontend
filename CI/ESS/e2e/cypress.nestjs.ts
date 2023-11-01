1c487f7e80163f18f583d96d699a3bdde31813ec
Jay Quan <a331998513@gmail.com>
2023-11-01 13:31:42 +0100

    fix: fixed e2e github wrong port

--- CI/ESS/e2e/cypress.nestjs.ts
+++ CI/ESS/e2e/cypress.nestjs.ts
@@ -1,21 +1,20 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e:
-{
-  "baseUrl": "http://localhost:80",
-  "lbBaseUrl": "http://localhost:80/api/v3",
-  "lbLoginEndpoint": "/Users/login",
-  "lbTokenPrefix": "Bearer ",
-  "viewportWidth": 1280,
-  "username": "admin",
-  "password": "am2jf70TPNZsSan",
-  "secondaryUsername": "archiveManager",
-  "secondaryPassword": "aman",
-  "guestUsername":"user1",
-  "guestUserEmail":"user1@your.site",
-  "guestPassword":"a609316768619f154ef58db4d847b75e",
-  "defaultCommandTimeout": 10000,
-  "retries": 1
-},
-})
+  e2e: {
+    baseUrl: "http://localhost:80",
+    lbBaseUrl: "http://localhost:80/api/v3",
+    lbLoginEndpoint: "/Users/login",
+    lbTokenPrefix: "Bearer ",
+    viewportWidth: 1280,
+    username: "admin",
+    password: "am2jf70TPNZsSan",
+    secondaryUsername: "archiveManager",
+    secondaryPassword: "aman",
+    guestUsername: "user1",
+    guestUserEmail: "user1@your.site",
+    guestPassword: "a609316768619f154ef58db4d847b75e",
+    defaultCommandTimeout: 10000,
+    retries: 1,
+  },
+});
