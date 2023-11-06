4ce319d8f8d27ff3983be8dfe0602023a8e4bfb6
Jay Quan <b331998513@gmail.com>
2023-11-06 13:31:56 +0100

    feat: Added e2e test for elastic-search (#1204)
    * feat: Added e2e test for elastic-search
    
    * fix: minor change with test name
    
    * fix: fixed e2e for elastic search
    
    * fix: include missing env
    
    * fix: included missing env for e2e test
    
    * fix: removed ports for elasticsearch e2e
    
    * fix: fixed cy.get attribute val for search input box
    
    * fix: fixed e2e github wrong port
    
    * test1
    
    * test2
    
    * test3
    
    * test4
    
    * test5
    
    * test6
    
    * test6.1
    
    * test6.2
    
    * test 7
    
    * test 7
    
    * test8
    
    * test9
    
    * test9
    
    * test 10
    
    * fix: minor cleanup for e2e test
    
    * fix: reduce memory usage of elastic search container
    
    * fix: elastic search set single-node mode for e2e
    
    * fix: configured memory max for elastic search e2e
    
    * fix: removed single node
    
    * test: increased sleeping time for backend loads
    
    * test: included wait script for backend container
    
    * test set root permission for apk command
    
    * test included apk add bash
    
    * test: removed memory max config for e2e job
    
    * test- added wait-for-it script script for e2e test job
    
    * test: removed wait.sh download from e2e job
    
    * test: removed wait-for-it step from e2e job
    
    * test: removed dockerfile for backend
    
    * test: change mount path
    
    * test: permission & mongoDB health check
    
    * test: test if mongoDB healthcheck works
    
    * test: included healtcheck for elastic search
    
    * test: es01 healtcheck
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    - testß
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * final test

--- CI/ESS/e2e/cypress.config.ts
+++ CI/ESS/e2e/cypress.config.ts
@@ -1,21 +1,20 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e:
-{
-  "baseUrl": "http://localhost:4200",
-  "lbBaseUrl": "http://localhost:3000/api/v3",
-  "lbLoginEndpoint": "/Users/login?include=user",
-  "lbTokenPrefix": "Bearer ",
-  "viewportWidth": 1280,
-  "username": "admin",
-  "password": "am2jf70TPNZsSan",
-  "secondaryUsername": "archiveManager",
-  "secondaryPassword": "aman",
-  "guestUsername":"user1",
-  "guestUserEmail":"user1@your.site",
-  "guestPassword":"a609316768619f154ef58db4d847b75e",
-  "defaultCommandTimeout": 10000,
-  "retries": 1
-},
-})
+  e2e: {
+    baseUrl: "http://localhost:4200",
+    lbBaseUrl: "http://localhost:3000/api/v3",
+    lbLoginEndpoint: "/Users/login?include=user",
+    lbTokenPrefix: "Bearer ",
+    viewportWidth: 1280,
+    username: "admin",
+    password: "am2jf70TPNZsSan",
+    secondaryUsername: "archiveManager",
+    secondaryPassword: "aman",
+    guestUsername: "user1",
+    guestUserEmail: "user1@your.site",
+    guestPassword: "a609316768619f154ef58db4d847b75e",
+    defaultCommandTimeout: 10000,
+    retries: 1,
+  },
+});
---
+++ cypress/e2e/other/elastic-search.cy.js
@@ -0,0 +1,77 @@
+/// <reference types="Cypress" />
+
+describe("Elastic search", () => {
+  const randomText1 =
+    "$PDfUCt+qX*5Km=ezGQF ELASTIC_SEARCH_vg+Mgga2#vEe=u!dQ!V+ fp$q6tz8y%hyaHzbx2X+ Vz6shS8ejGCQN3h%TEST 6j2&eqYT7GCR+CpqqD5n";
+  const randomText2 =
+    "Style never met and those among great. At no or september sportsmen he perfectly happiness attending. ";
+
+  const searchQuery1 = "elast searc";
+  const searchQuery2 = "style perfectly";
+  const searchQueryIrrelevant = "Christmas";
+
+  beforeEach(() => {
+    cy.login(Cypress.config("username"), Cypress.config("password"));
+    cy.initializeElasticSearch();
+    cy.createDatasetForElasticSearch(randomText1);
+    cy.createDatasetForElasticSearch(randomText2);
+  });
+
+  afterEach(() => {
+    cy.login(Cypress.config("username"), Cypress.config("password"));
+    cy.removeDatasetsForElasticSearch(randomText1);
+    cy.removeDatasetsForElasticSearch(randomText2);
+    cy.removeElasticSearchIndex();
+  });
+
+  describe("Elastic search query", () => {
+    it("should get relevant dataset with partial text query", () => {
+      cy.visit("/datasets");
+
+      cy.get(".dataset-table mat-table mat-header-row").should("exist");
+
+      cy.finishedLoading();
+
+      cy.get('[data-cy="text-search"] input[type="search"]')
+        .clear()
+        .type(searchQuery1);
+
+      cy.finishedLoading();
+
+      cy.get("mat-row").contains(randomText1).click();
+
+      cy.get('[data-cy="edit-general-information"]').should("exist");
+
+      cy.visit("/datasets");
+
+      cy.finishedLoading();
+
+      cy.get('[data-cy="text-search"] input[type="search"]')
+        .clear()
+        .type(searchQuery2);
+
+      cy.finishedLoading();
+
+      cy.get("mat-row").contains(randomText2).click();
+
+      cy.get('[data-cy="edit-general-information"]').should("exist");
+    });
+  });
+
+  it("should not get any dataset with irrelevant text query", () => {
+    cy.visit("/datasets");
+
+    cy.get(".dataset-table mat-table mat-header-row").should("exist");
+
+    cy.finishedLoading();
+
+    cy.get('[data-cy="text-search"] input[type="search"]')
+      .clear()
+      .type(searchQueryIrrelevant);
+
+    cy.finishedLoading();
+
+    cy.get('mat-row:contains("' + randomText1 + '")').should("not.exist");
+    cy.get('mat-row:contains("' + randomText2 + '")').should("not.exist");
+  });
+});
