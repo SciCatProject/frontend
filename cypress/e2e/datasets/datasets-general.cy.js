f9af727e87ec03c7809556c9eb438936cf9a2e7e
junjiequan <a331998513@gmail.com>
2024-03-15 10:16:05 +0100

    removed create dataset and create sample  test

--- cypress/e2e/datasets/datasets-general.cy.js
+++ cypress/e2e/datasets/datasets-general.cy.js
@@ -9,38 +9,6 @@ describe("Datasets general", () => {
     cy.removeDatasets();
   });
 
-  describe("Create dataset", () => {
-    it("should be able to create dataset in the UI", () => {
-      const newName = "Test created dataset name";
-      const newDescription = "Test created dataset description";
-      const usedSoftware = "Test software";
-
-      cy.visit("/datasets");
-
-      cy.get(".dataset-table mat-table mat-header-row").should("exist");
-
-      cy.finishedLoading();
-
-      cy.get("mat-card.add-card").contains("Create Dataset").click();
-
-      cy.get('input[formcontrolname="datasetName"]').clear().type(newName);
-      cy.get('textarea[formcontrolname="description"]')
-        .clear()
-        .type(newDescription);
-      cy.get('mat-select[formcontrolname="ownerGroup"]').click();
-      cy.get('[role="listbox"] mat-option').first().click();
-      cy.get('input[formcontrolname="usedSoftware"]')
-        .clear()
-        .type(usedSoftware);
-
-      cy.get('button[color="primary"]').contains("Save").click();
-
-      cy.finishedLoading();
-
-      cy.get('[data-cy="edit-general-information"]').should("exist");
-    });
-  });
-
   describe("Show dataset table after logout and login", () => {
     it("should be able to see datasets after visiting details page logout and login again", () => {
       const username = Cypress.config("username");
