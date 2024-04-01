311dc7ed4027ae6e1606c9caf84aa6a7cfe98e6e
Ingvord <ingvord.mail@gmail.com>
2024-04-01 16:00:44 +0200

    Add search and clear buttons; some clean ups

--- src/app/datasets/dashboard/full-text-search/full-text-search-bar.component.ts
+++ src/app/datasets/dashboard/full-text-search/full-text-search-bar.component.ts
@@ -4,20 +4,20 @@ import {MatSelectModule} from "@angular/material/select";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @Component({
   selector: 'full-text-search-bar',
   templateUrl: './full-text-search-bar.component.html',
   styleUrls: ['./full-text-search-bar.component.scss'],
   standalone: true,
-  imports: [MatFormFieldModule, MatSelectModule, FormsModule, ReactiveFormsModule, MatInputModule, MatIconModule],
+  imports: [MatFormFieldModule, MatSelectModule, FormsModule, ReactiveFormsModule, MatInputModule, MatIconModule, MatButtonModule],
 })
 export class FullTextSearchBarComponent {
   @Input() prefilledValue: string = '';
   @Input() placeholder: string = 'Text Search';
   @Input() clear: boolean;
   @Output() search = new EventEmitter<string>();
-  @Output() searchBarFocus = new EventEmitter<void>();
 
   searchTerm: string = '';
 
@@ -28,25 +28,12 @@ export class FullTextSearchBarComponent {
   }
 
   onSearch(): void {
-    console.log(this.searchTerm);
     this.search.emit(this.searchTerm);
-  }
-
-  onFocus(): void {
-    this.searchBarFocus.emit();
   }
 
   onClear(): void {
     this.searchTerm = '';
     this.onSearch(); // Optionally trigger search on clear
-  }
-
-  ngOnChanges(changes: { [propKey: string]: SimpleChange }) {
-    for (const propName in changes) {
-      if (propName === "clear" && changes[propName].currentValue === true) {
-        this.searchTerm = "";
-      }
-    }
   }
 }
 
