f58a5be1c2928a7f5a8fcbdfe2fd296671d62243
junjiequan <a331998513@gmail.com>
2024-05-21 10:54:05 +0200

    refactored fulltext-searchbar component and minor css changes for the search bar container

--- src/app/datasets/dashboard/full-text-search/full-text-search-bar.component.ts
+++ src/app/datasets/dashboard/full-text-search/full-text-search-bar.component.ts
@@ -3,6 +3,7 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatSelectModule } from "@angular/material/select";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
+import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import {
@@ -22,44 +23,8 @@ import { selectSearchTerms } from "../../../state-management/selectors/datasets.
 
 @Component({
   selector: "full-text-search-bar",
-  template: ` <mat-form-field appearance="fill" class="full-text-search-field">
-      <mat-label>Search</mat-label>
-      <input
-        matInput
-        placeholder="Type to search..."
-        type="search"
-        [(ngModel)]="searchTerm"
-        (ngModelChange)="onSearchTermChange($event)"
-      />
-    </mat-form-field>
-    <span>
-      <button
-        mat-flat-button
-        color="accent"
-        data-cy="search-button"
-        (click)="onSearch()"
-      >
-        <mat-icon>search</mat-icon>
-        Search
-      </button>
-      <button mat-flat-button data-cy="search-clear-button" (click)="onClear()">
-        <mat-icon>close</mat-icon>
-        Clear
-      </button>
-    </span>`,
-  styles: [
-    `
-      .full-text-search-field {
-        max-width: 80%;
-        width: 100%;
-        margin-left: 5em;
-      }
-
-      .full-text-search-field + span {
-        margin-left: 5em;
-      }
-    `,
-  ],
+  templateUrl: "./full-text-search-bar.component.html",
+  styleUrls: ["./full-text-search-bar.component.scss"],
   standalone: true,
   imports: [
     MatFormFieldModule,
@@ -68,6 +33,7 @@ import { selectSearchTerms } from "../../../state-management/selectors/datasets.
     ReactiveFormsModule,
     MatInputModule,
     MatIconModule,
+    MatCardModule,
     MatButtonModule,
   ],
 })
