c5f180502c3a22ed823eb5b55140e7cf708d7ec7
Max Novelli <max.novelli@ess.eu>
2024-07-26 16:31:50 +0200

    datafiles actions tests, data files action test almost done

--- src/app/datasets/datafiles-actions/datafiles-action.component.ts
+++ src/app/datasets/datafiles-actions/datafiles-action.component.ts
@@ -27,10 +27,12 @@ export class DatafilesActionComponent implements OnInit, OnChanges {
   visible = true;
   use_mat_icon = false;
   use_icon = false;
-  disabled = false;
+  //disabled = false;
   disabled_condition = "false";
   selectedTotalFileSize = 0;
   numberOfFileSelected = 0;
+
+  form: HTMLFormElement;
 
   constructor(private userApi: UserApi) {
     this.userApi.jwt().subscribe((jwt) => {
@@ -38,54 +40,66 @@ export class DatafilesActionComponent implements OnInit, OnChanges {
     });
   }
 
+  private evaluate_disabled_condition(condition: string) {
+    return condition
+      .replaceAll(
+        "#SizeLimit",
+        String(
+          this.maxFileSize > 0 &&
+            this.selectedTotalFileSize <= this.maxFileSize,
+        ),
+      )
+      .replaceAll("#Selected", String(this.numberOfFileSelected > 0));
+  }
+
+  private prepare_disabled_condition() {
+    if (this.actionConfig.enabled) {
+      this.disabled_condition =
+        "!(" +
+        this.evaluate_disabled_condition(this.actionConfig.enabled) +
+        ")";
+    } else if (this.actionConfig.disabled) {
+      this.disabled_condition = this.evaluate_disabled_condition(
+        this.actionConfig.disabled,
+      );
+    } else {
+      this.disabled_condition = "false";
+    }
+  }
+
   ngOnInit() {
     this.use_mat_icon = this.actionConfig.mat_icon !== undefined;
     this.use_icon = this.actionConfig.icon !== undefined;
     this.prepare_disabled_condition();
     this.update_status();
-    this.compute_disabled();
+    //this.compute_disabled();
   }
 
   ngOnChanges(changes: SimpleChanges) {
     console.log(changes);
     if (changes["files"]) {
       this.update_status();
-      this.compute_disabled();
+      //this.compute_disabled();
     }
   }
 
   update_status() {
     this.selectedTotalFileSize = this.files
-      .filter((item) => item.selected)
+      .filter((item) => item.selected || this.actionConfig.files === "all")
       .reduce((sum, item) => sum + item.size, 0);
     this.numberOfFileSelected = this.files.filter(
       (item) => item.selected,
     ).length;
   }
 
-  prepare_disabled_condition() {
-    if (this.actionConfig.enabled) {
-      this.disabled_condition =
-        "!(" +
-        this.actionConfig.enabled
-          .replaceAll(
-            "#SizeLimit",
-            "this.maxFileSize > 0 && this.selectedTotalFileSize <= this.maxFileSize",
-          )
-          .replaceAll("#Selected", "this.numberOfFileSelected > 0") +
-        ")";
-    } else if (this.actionConfig.disabled) {
-      this.disabled_condition = this.actionConfig.enabled
-        .replaceAll(
-          "#SizeLimit",
-          "this.maxFileSize > 0 && this.selectedTotalFileSize <= this.maxFileSize",
-        )
-        .replaceAll("#Selected", "this.numberOfFileSelected > 0");
-    }
-  }
+  // compute_disabled() {
+  //   this.disabled = eval(this.disabled_condition);
+  // }
 
-  compute_disabled() {
-    this.disabled = eval(this.disabled_condition);
+  get disabled() {
+    this.update_status();
+    this.prepare_disabled_condition();
+    return eval(this.disabled_condition);
   }
 
   add_input(name, value) {
@@ -97,20 +111,20 @@ export class DatafilesActionComponent implements OnInit, OnChanges {
   }
 
   perform_action() {
-    const form = document.createElement("form");
-    form.target = this.actionConfig.target;
-    form.method = this.actionConfig.method;
-    form.action = this.actionConfig.url;
+    this.form = document.createElement("form");
+    this.form.target = this.actionConfig.target;
+    this.form.method = this.actionConfig.method;
+    this.form.action = this.actionConfig.url;
 
-    form.appendChild(
+    this.form.appendChild(
       this.add_input("auth_token", this.userApi.getCurrentToken().id),
     );
 
-    form.appendChild(this.add_input("jwt", this.jwt));
+    this.form.appendChild(this.add_input("jwt", this.jwt));
 
-    form.appendChild(this.add_input("dataset", this.actionDataset.pid));
+    this.form.appendChild(this.add_input("dataset", this.actionDataset.pid));
 
-    form.appendChild(
+    this.form.appendChild(
       this.add_input("directory", this.actionDataset.sourceFolder),
     );
 
@@ -119,12 +133,14 @@ export class DatafilesActionComponent implements OnInit, OnChanges {
         this.actionConfig.files === "all" ||
         (this.actionConfig.files === "selected" && item.selected)
       ) {
-        form.appendChild(this.add_input("files[" + index + "]", item.path));
+        this.form.appendChild(
+          this.add_input("files[" + index + "]", item.path),
+        );
       }
     }
 
-    document.body.appendChild(form);
-    form.submit();
+    //document.body.appendChild(form);
+    this.form.submit();
     window.open("", "view");
   }
 }
