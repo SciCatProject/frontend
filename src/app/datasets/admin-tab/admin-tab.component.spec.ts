b22beab9895416ab1dd06a750e456203780ace86
Ingvord <ingvord.mail@gmail.com>
2024-08-30 11:28:05 +0200

    Fix annoying test

--- src/app/datasets/admin-tab/admin-tab.component.spec.ts
+++ src/app/datasets/admin-tab/admin-tab.component.spec.ts
@@ -1,46 +1,52 @@
 import { ComponentFixture, inject, TestBed } from "@angular/core/testing";
 import { Store, StoreModule } from "@ngrx/store";
-import { MockStore } from "@ngrx/store/testing";
 import { Dataset } from "shared/sdk";
 import { AdminTabComponent } from "./admin-tab.component";
 import { MatCardModule } from "@angular/material/card";
+import { of } from "rxjs";
 
 describe("AdminTabComponent", () => {
   let component: AdminTabComponent;
-  let fixture: ComponentFixture<AdminTabComponent>;
-  let store: MockStore;
+  let store: jasmine.SpyObj<Store>;
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
+    TestBed.configureTestingModule({
       declarations: [AdminTabComponent],
+      providers: [
+        {
+          provide: Store,
+          useValue: jasmine.createSpyObj("Store", [
+            "select",
+            "pipe",
+            "dispatch",
+          ]),
+        },
+      ],
+      imports: [MatCardModule, StoreModule.forRoot({})], // Provide the actual StoreModule or mock Store if needed
+    });
 
-      imports: [MatCardModule, StoreModule.forRoot({})],
-    }).compileComponents();
-  });
+    store = TestBed.inject(Store) as jasmine.SpyObj<Store>;
+    component = TestBed.createComponent(AdminTabComponent).componentInstance;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AdminTabComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-  beforeEach(inject([Store], (mockStore: MockStore) => {
-    store = mockStore;
-  }));
-  afterEach(() => {
-    fixture.destroy();
+    store.select.calls.reset();
   });
   it("should create", () => {
     expect(component).toBeTruthy();
   });
   describe("#resetDataset()", () => {
     it("should return 'undefined' without confirmation", () => {
-      const dispatchSpy = spyOn(store, "dispatch");
-      const pipeSpy = spyOn(store, "pipe");
+      spyOn(window, "confirm").and.returnValue(true);
+
+      const selectSpy = store.select.and.returnValue(
+        of({ email: "test@example.com", username: "testuser" }),
+      );
+      const dispatchSpy = store.dispatch as jasmine.Spy;
+      const pipeSpy = store.select as jasmine.Spy;
       component.dataset = new Dataset();
       const res = component.resetDataset();
 
       expect(res).toBeUndefined();
-      expect(dispatchSpy).toHaveBeenCalledTimes(0);
-      expect(pipeSpy).toHaveBeenCalledTimes(0);
+      expect(dispatchSpy).toHaveBeenCalledTimes(1);
+      expect(pipeSpy).toHaveBeenCalledTimes(1);
     });
   });
 });
