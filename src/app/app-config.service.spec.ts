27a65441c80270c95ef61abc827be8a72677d48e
David Wiessner <david.wiessner@indema.ch>
2024-09-12 15:34:59 +0000

    Change back the files to original state

--- src/app/app-config.service.spec.ts
+++ src/app/app-config.service.spec.ts
@@ -34,7 +34,7 @@ const appConfig: AppConfig = {
   jsonMetadataEnabled: true,
   jupyterHubUrl: "https://jupyterhub.esss.lu.se/",
   landingPage: "doi2.psi.ch/detail/",
-  lbBaseURL: "http://localhost:3000",
+  lbBaseURL: "http://127.0.0.1:3000",
   localColumns: [
     {
       name: "select",
