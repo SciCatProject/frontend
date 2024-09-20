722ff9087a427956b7ab1aa9ec581d558e1a73ff
junjiequan <a331998513@gmail.com>
2024-09-20 16:17:38 +0200

    karma surpress passed tests

--- karma.conf.js
+++ karma.conf.js
@@ -3,23 +3,23 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('karma-junit-reporter'),
-      require('karma-spec-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage"),
+      require("karma-junit-reporter"),
+      require("karma-spec-reporter"),
+      require("@angular-devkit/build-angular/plugins/karma"),
     ],
     proxies: {
-      '/assets/': '/base/src/assets/',
-      'assets/': '/base/src/assets/'
+      "/assets/": "/base/src/assets/",
+      "assets/": "/base/src/assets/",
     },
     mime: {
-      'text/x-typescript': ['ts', 'tsx']
+      "text/x-typescript": ["ts", "tsx"],
     },
     client: {
       jasmine: {
@@ -28,44 +28,48 @@ module.exports = function (config) {
         // for example, you can disable the random execution with `random: false`
         // or set a specific seed with `seed: 4321`
       },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
+      suppressAll: true, // removes the duplicated traces
     },
     junitReporter: {
-	    outputFile: 'karma-junit.xml',
-	    useBrowserName: false,
-	    outputDir: '.'
+      outputFile: "karma-junit.xml",
+      useBrowserName: false,
+      outputDir: ".",
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage'),
+      dir: require("path").join(__dirname, "./coverage"),
       reporters: [
-        { type: 'html', subdir: 'report-html' },
-        { type: 'lcovonly', subdir: '.', file: 'lcov.info' },
-        { type: 'text-summary' }
+        { type: "html", subdir: "report-html" },
+        { type: "lcovonly", subdir: ".", file: "lcov.info" },
+        { type: "text-summary" },
       ],
-      fixWebpackSourcePaths: true
+      fixWebpackSourcePaths: true,
     },
     customLaunchers: {
       ChromeHeadless: {
-        base: 'Chrome',
+        base: "Chrome",
         flags: [
-          '--headless',
-          '--disable-gpu',
-          '--no-sandbox',
+          "--headless",
+          "--disable-gpu",
+          "--no-sandbox",
           // Without a remote debugging port, Google Chrome exits immediately.
-          '--remote-debugging-port=9222'
-        ]
-      }
+          "--remote-debugging-port=9222",
+        ],
+      },
     },
-    reporters: ['progress', 'coverage','junit','spec'],
+    reporters: ["progress", "coverage", "junit", "spec"],
+    specReporter: {
+      suppressPassed: true, // Suppress passed tests, only show failures
+      suppressSkipped: true, // Optionally suppress skipped tests too
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_DEBUG,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ["ChromeHeadless"],
     singleRun: true,
-    restartOnFileChange: true
+    restartOnFileChange: true,
   });
 };
