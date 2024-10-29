8d3a96838ba82e2743e70a4e4b4dba1b14b64e64
martintrajanovski <martin.trajanovski@gmail.com>
2024-10-29 14:42:32 +0100

    feat: add the new auth service to prepare for the new sdk

---
+++ src/app/shared/services/auth/storage.browser.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class LocalStorageBrowser {
+  private parse(value: string) {
+    try {
+      return JSON.parse(value);
+    } catch (e) {
+      return value;
+    }
+  }
+
+  get(key: string) {
+    const data: string = localStorage.getItem(key);
+    return this.parse(data);
+  }
+
+  set(key: string, value: string): void {
+    localStorage.setItem(
+      key,
+      typeof value === "object" ? JSON.stringify(value) : value,
+    );
+  }
+
+  remove(key: string): void {
+    if (localStorage[key]) {
+      localStorage.removeItem(key);
+    } else {
+      console.log("Trying to remove unexisting key: ", key);
+    }
+  }
+}
