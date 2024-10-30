5099220aed679956c666c045fc0537c2d8c528c6
junjiequan <a331998513@gmail.com>
2024-10-30 13:28:08 +0100

    added thumbnailFetchLimitPerPage config variable and included cache expiration logic

--- src/app/shared/services/thumbnail.service.ts
+++ src/app/shared/services/thumbnail.service.ts
@@ -3,17 +3,29 @@ import { Store } from "@ngrx/store";
 import { distinctUntilChanged, firstValueFrom } from "rxjs";
 import { DatasetApi } from "shared/sdk";
 import { selectDatasetsPerPage } from "state-management/selectors/datasets.selectors";
+import { AppConfigService } from "app-config.service";
+
+interface ThumbnailCache {
+  [pid: string]: {
+    value: string | null;
+    timestamp: number;
+    isError: boolean;
+  };
+}
 
 @Injectable({
   providedIn: "root",
 })
 export class ThumbnailService {
-  private thumbnailCache: { [pid: string]: string | null } = {};
-  private datasetsPerPage: number | null = null;
+  private thumbnailCache: ThumbnailCache = {};
+  private datasetsPerPage: number;
+  private thumbnailFetchLimitPerPage: number;
+  private cacheTimeout = 5 * 60 * 1000; // 5 minutes in milliseconds
 
   constructor(
     private datasetApi: DatasetApi,
     private store: Store,
+    private appConfigService: AppConfigService,
   ) {
     this.store
       .select(selectDatasetsPerPage)
@@ -21,30 +33,48 @@ export class ThumbnailService {
         distinctUntilChanged(), // Only proceed on actual changes
       )
       .subscribe((datasetsPerPage) => (this.datasetsPerPage = datasetsPerPage));
+
+    this.thumbnailFetchLimitPerPage =
+      this.appConfigService.getConfig().thumbnailFetchLimitPerPage || 100;
   }
 
   async getThumbnail(pid: string): Promise<string | null> {
-    // if the number of datasets per page is over 100, don't fetch thumbnails
-    if (!this.datasetsPerPage || this.datasetsPerPage > 100) {
+    // If the number of datasets per page is over the value of thumbnailFetchLimitPerPage, don't fetch thumbnails
+    if (this.datasetsPerPage > this.thumbnailFetchLimitPerPage) {
       return null;
     }
 
-    // Return cached value if available
-    // And return null if already fetched and not found
-    if (this.thumbnailCache[pid]) {
-      return this.thumbnailCache[pid];
-    } else if (this.thumbnailCache[pid] === null) {
-      return null;
+    const cachedThumbnail = this.thumbnailCache[pid];
+
+    // Check if the cached entry exists and has not expired
+    if (cachedThumbnail) {
+      const isExpired =
+        Date.now() - cachedThumbnail.timestamp > this.cacheTimeout;
+      if (!isExpired) {
+        return cachedThumbnail.value;
+      } else {
+        delete this.thumbnailCache[pid];
+      }
     }
 
     try {
       const encodedPid = encodeURIComponent(pid);
       const res = await firstValueFrom(this.datasetApi.thumbnail(encodedPid));
-      this.thumbnailCache[pid] = res?.thumbnail || null; // Cache the result
-      return this.thumbnailCache[pid];
+      const thumbnail = res?.thumbnail || null;
+
+      this.thumbnailCache[pid] = {
+        value: thumbnail,
+        timestamp: Date.now(),
+        isError: !thumbnail,
+      };
+      return thumbnail;
     } catch (error) {
       console.error(`Failed to fetch thumbnail for PID: ${pid}`, error);
-      this.thumbnailCache[pid] = null; // Cache null for failed request
+      this.thumbnailCache[pid] = {
+        value: null,
+        timestamp: Date.now(),
+        isError: true,
+      };
       return null;
     }
   }
