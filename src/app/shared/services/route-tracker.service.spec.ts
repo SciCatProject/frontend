eda87728a8a778cb9e97638f8c88dd2c2fcaff51
Omkar Zade <omkar.zade@psi.ch>
2025-02-18 09:07:24 +0000

    Add service to track last visited route. Pass the last visited route on OIDC Login. Redirect to last visited route in auth-callback component.

---
+++ src/app/shared/services/route-tracker.service.spec.ts
@@ -0,0 +1,38 @@
+import { TestBed } from "@angular/core/testing";
+
+import { RouteTrackerService } from "./route-tracker.service";
+import { provideRouter, Router } from "@angular/router";
+import { Component } from "@angular/core";
+
+@Component({})
+class DummyComponent {}
+
+describe("RouteTrackerService", () => {
+  let service: RouteTrackerService;
+  let router: Router;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideRouter([
+          { path: "datasets/:pid", component: DummyComponent },
+          { path: "login", component: DummyComponent },
+        ]),
+      ],
+    });
+    service = TestBed.inject(RouteTrackerService);
+    router = TestBed.inject(Router);
+  });
+
+  it("should be created", () => {
+    expect(service).toBeTruthy();
+  });
+
+  it("should save the last route navigated from", async () => {
+    expect(router).toBeTruthy();
+    await router.navigateByUrl("/datasets/123");
+    await router.navigateByUrl("/login");
+    const lastRoute = service.getLastRoute();
+    expect(lastRoute).toBe("/datasets/123");
+  });
+});
