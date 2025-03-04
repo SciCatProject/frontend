8f39dd69773ae43ad013b34c27ee21897c5df1eb
Despina <despoina.adamopoulou@empa.ch>
2025-03-04 15:09:16 +0100

    activate job details page

--- src/app/jobs/jobs-dashboard-new/jobs-dashboard-new.component.ts
+++ src/app/jobs/jobs-dashboard-new/jobs-dashboard-new.component.ts
@@ -4,6 +4,7 @@ import {
   Component,
   OnDestroy,
 } from "@angular/core";
+import { Router } from "@angular/router";
 import { SciCatDataSource } from "../../shared/services/scicat.datasource";
 import { ScicatDataService } from "../../shared/services/scicat-data-service";
 import { ExportExcelService } from "../../shared/services/export-excel.service";
@@ -102,6 +103,7 @@ export class JobsDashboardNewComponent implements OnDestroy, AfterViewChecked {
     private cdRef: ChangeDetectorRef,
     private dataService: ScicatDataService,
     private exportService: ExportExcelService,
+    private router: Router,
   ) {
     this.dataSource = new SciCatDataSource(
       this.appConfigService,
@@ -120,9 +122,7 @@ export class JobsDashboardNewComponent implements OnDestroy, AfterViewChecked {
   }
 
   onRowClick(job: JobsTableData) {
-    // currently deactivated, no extra data available
-    /*     console.log("Row clicked:", job);
     const id = encodeURIComponent(job.id);
-    this.router.navigateByUrl("/user/jobs/" + id); */
+    this.router.navigateByUrl("/user/jobs/" + id);
   }
 }
