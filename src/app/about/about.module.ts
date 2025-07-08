8381f23444e08682c0286187989ed3ab8f9ac5d0
Omkar Zade <omkar.zade@psi.ch>
2025-07-08 14:50:19 +0200

    (feat) add help cards in /help and /about, based on configurable supportEmail

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -4,9 +4,10 @@ import { AboutComponent } from "./about/about.component";
 import { MatCardModule } from "@angular/material/card";
 import { FlexLayoutModule } from "@ngbracket/ngx-layout";
 import { LinkyModule } from "ngx-linky";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [AboutComponent],
-  imports: [CommonModule, FlexLayoutModule, LinkyModule, MatCardModule],
+  imports: [CommonModule, FlexLayoutModule, LinkyModule, MatCardModule, RouterModule],
 })
 export class AboutModule {}
