51b6d47c8575a8a5ca576d3415969756fa3e1dbc
Abdi Abdulle <abdi.abdulle@outlook.dk>
2025-07-14 13:28:14 +0200

    added condition in frontend.config.e2e file, eslint fix

--- src/app/datasets/datasets-filter/datasets-filter.component.ts
+++ src/app/datasets/datasets-filter/datasets-filter.component.ts
@@ -130,14 +130,8 @@ export class DatasetsFilterComponent implements OnInit, OnDestroy {
   applyEnabledConditions() {
     this.conditionConfigs$.pipe(take(1)).subscribe((conditionConfigs) => {
       (conditionConfigs || []).forEach((config) => {
-        if (
-          config.enabled &&
-          config.condition.lhs &&
-          config.condition.rhs
-        ) {
+        if (config.enabled && config.condition.lhs && config.condition.rhs) {
           this.store.dispatch(addScientificConditionAction({ condition: config.condition }));
-          this.store.dispatch(selectColumnAction({ name: config.condition.lhs, columnType: "custom" }),
-          );
         }
       });
     });
