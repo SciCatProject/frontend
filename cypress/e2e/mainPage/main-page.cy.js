6c00b58db37d45bca29acaf0f753cc7b805586c4
Max Novelli <max.novelli@ess.eu>
2025-07-31 12:55:48 +0200

    Fixed tests

--- cypress/e2e/mainPage/main-page.cy.js
+++ cypress/e2e/mainPage/main-page.cy.js
@@ -25,21 +25,31 @@ describe("Main Page", () => {
 
   describe("Main page configuration as non authenticated user", () => {
     it("should visit main page correctly configured", () => {
-      cy.visit("http://localhost:3000");
+      const username = Cypress.env("username");
+      
+      cy.visit("/");
+      cy.finishedLoading();
+
+      cy.get(".user-button").should("contain.text", username).click();
+
+      cy.get("[data-cy=logout-button]").click();
+
+      cy.finishedLoading();
+      cy.visit("/");
 
       cy.finishedLoading();
 
-      cy.get("breadcrumb div span a").should('contain', "Datasets");
+      cy.get("breadcrumb > div > span > a").should('contain', "Datasets");
 
       cy.get("div.header mat-toolbar.mat-toolbar.mat-elevation-z1.mat-primary.mat-toolbar-single-row a").eq(0).should('have.attr','href','/datasets');
-      cy.get("div.header mat-toolbar.mat-toolbar.mat-elevation-z1.mat-primary.mat-toolbar-single-row a").eq(1).should('have.attr','href','https://my.facility.eu');
-        
-      cy.get('#mat-mdc-menu-panel-0 mat-mdc-menu-content div mat-mdc-menu-item-text span').eq(0).should('contain','Datasets')
-      cy.get('#mat-mdc-menu-panel-0 mat-mdc-menu-content div mat-mdc-menu-item-text span').eq(1).should('contain','Instruments')
-      cy.get('#mat-mdc-menu-panel-0 mat-mdc-menu-content div mat-mdc-menu-item-text span').eq(0).should('contain','Proposals')
-      cy.get('#mat-mdc-menu-panel-0 mat-mdc-menu-content div mat-mdc-menu-item-text span').eq(0).should('contain','Published Data')
+      cy.get("div.header mat-toolbar.mat-toolbar.mat-elevation-z1.mat-primary.mat-toolbar-single-row a").eq(1).should('have.attr','href','https://my.facility.site');
 
-      cy.get('[data-cy="login-button"]').should('exists');
+      cy.get("div.main-menu").click()
+
+      cy.get('button.mat-mdc-menu-item:nth-child(1) > span:nth-child(2) > span:nth-child(1)').should('contain','Datasets')
+      cy.get('button.mat-mdc-menu-item:nth-child(4) > span:nth-child(2) > span:nth-child(1)').should('contain','Published Data')
+
+      cy.get('[data-cy="login-button"]').should('exist');
 
       //cy.get("mat-row").contains("Main Page Dataset Public").click();
     });
@@ -50,34 +60,38 @@ describe("Main Page", () => {
       const username = Cypress.env("username");
       const password = Cypress.env("password");
 
-      cy.visit("/login");
+      // cy.visit("/login");
 
+      // cy.finishedLoading();
+
+      // cy.url().should("include", "/login");
+
+      // cy.get('mat-tab-group [role="tab"]').contains("Local").click();
+
+      // cy.get("#usernameInput").type(username).should("have.value", username);
+      // cy.get("#passwordInput").type(password).should("have.value", password);
+
+      // cy.get("button[type=submit]").click();
+
+      cy.visit("/");
       cy.finishedLoading();
-
-      cy.url().should("include", "/login");
-
-      cy.get('mat-tab-group [role="tab"]').contains("Local").click();
-
-      cy.get("#usernameInput").type(username).should("have.value", username);
-      cy.get("#passwordInput").type(password).should("have.value", password);
-
-      cy.get("button[type=submit]").click();
 
       cy.url().should("include", "/proposals");
 
-      cy.finishedLoading();
-
-      cy.get("breadcrumb div span a").should('contain', "Proposals");
+      cy.get("breadcrumb > div > span > a").should('contain', "Proposals");
 
       cy.get("div.header mat-toolbar.mat-toolbar.mat-elevation-z1.mat-primary.mat-toolbar-single-row a").eq(0).should('have.attr','href','/proposals');
-      cy.get("div.header mat-toolbar.mat-toolbar.mat-elevation-z1.mat-primary.mat-toolbar-single-row a").eq(1).should('have.attr','href','my.facility.eu');
-        
-      cy.get('#mat-mdc-menu-panel-0 mat-mdc-menu-content div mat-mdc-menu-item-text span').eq(1).should('contain','Files')
+      cy.get("div.header mat-toolbar.mat-toolbar.mat-elevation-z1.mat-primary.mat-toolbar-single-row a").eq(1).should('have.attr','href','https://my.facility.site');
 
-      cy.get('[data-cy="login-button"]').should('not exists');
-      cy.get('user-button').should('exists');
+      cy.get("div.main-menu").click()
 
-      //cy.get("mat-row").contains("Main Page Dataset Non Public").click();
+      cy.get('button.mat-mdc-menu-item:nth-child(2) > span:nth-child(2) > span:nth-child(1)').should('contain','Files')
+
+      cy.get("div.main-menu").click()
+
+      cy.get('[data-cy="login-button"]').should('not.exist');
+      cy.get('.user-button').should('exist');
+
     });
   });
 
