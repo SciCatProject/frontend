4a7203dc1bdba08f35c2a297c2eac02ce2810151
Max Novelli <max.novelli@ess.eu>
2025-08-20 14:43:03 +0200

    header or field name as header column

--- src/app/datasets/dataset-table/dataset-table.component.ts
+++ src/app/datasets/dataset-table/dataset-table.component.ts
@@ -213,7 +213,7 @@ export class DatasetTableComponent implements OnInit, OnDestroy {
     this.columns = currentColumnSetting;
     const translated$ = forkJoin(
       currentColumnSetting.map((i) =>
-        this.translateService.get(i.name).pipe(
+        this.translateService.get(i.header || i.name).pipe(
           map((translated) => ({
             ...i,
             header: translated,
