288ab57f9f9721529e55a88ea13197c584c8e4c6
Abdi Abdulle <abdi.abdulle@outlook.dk>
2025-08-27 15:39:23 +0200

    changed which call to trigger and kept the fetchCountAction, so now 2 calls are being made

--- src/app/logbooks/logbooks-dashboard/logbooks-dashboard.component.ts
+++ src/app/logbooks/logbooks-dashboard/logbooks-dashboard.component.ts
@@ -125,6 +125,7 @@ export class LogbooksDashboardComponent
       this.store.select(selectCurrentDataset).subscribe((dataset) => {
         if (dataset) {
           this.dataset = dataset;
+          this.store.dispatch(fetchDatasetLogbookAction({ pid: dataset.pid }));
           this.ownershipService.checkDatasetAccess(
             dataset,
             this.store,
