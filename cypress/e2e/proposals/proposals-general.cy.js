6653fb28f21bc02037718fe278dd402eac48d7d1
Abdi Mo <abdi.abdulle@outlook.dk>
2026-03-30 10:48:59 +0200

    updated e2e tests and removed unused scss class

--- cypress/e2e/proposals/proposals-general.cy.js
+++ cypress/e2e/proposals/proposals-general.cy.js
@@ -639,14 +639,14 @@ describe("Proposals general", () => {
       });
     });
 
-    it("should collapse and expand checkbox filters", () => {
-      cy.get(".collapsible-filter-wrapper .collapse-toggle").first().click();
+    it.only("should collapse and expand checkbox filters", () => {
+      cy.get(".collapsible-filter-wrapper .icon-collapse").first().click();
 
       cy.get(".collapsible-filter-wrapper .checkbox-list")
         .first()
         .should("not.be.visible");
 
-      cy.get(".collapsible-filter-wrapper .collapse-toggle").first().click();
+      cy.get(".collapsible-filter-wrapper .icon-collapse").first().click();
 
       cy.get(".collapsible-filter-wrapper .checkbox-list")
         .first()
