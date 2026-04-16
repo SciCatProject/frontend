2d6a18470e0eb93097d71fbd11128b51a94be378
fpotier <58594145+fpotier@users.noreply.github.com>
2026-04-16 12:21:47 +0200

    feat(publish): default metadata from /formpopulate (#2292)
    Use the `/formpopulate` endpoint to initialize the publish form

--- src/app/datasets/publish/publish.component.spec.ts
+++ src/app/datasets/publish/publish.component.spec.ts
@@ -24,7 +24,7 @@ import { MatInputModule } from "@angular/material/input";
 import { MatSelectModule } from "@angular/material/select";
 import { MatButtonModule } from "@angular/material/button";
 import { AppConfigService } from "app-config.service";
-import { PublishedDataService } from "@scicatproject/scicat-sdk-ts-angular";
+import { PublishedDataV4Service } from "@scicatproject/scicat-sdk-ts-angular";
 import { SharedScicatFrontendModule } from "shared/shared.module";
 
 const getConfig = () => ({
@@ -67,7 +67,7 @@ describe("PublishComponent", () => {
           { provide: ActivatedRoute, useClass: MockActivatedRoute },
           { provide: ActionsSubject, useValue: of({}) },
           { provide: AppConfigService, useValue: { getConfig } },
-          { provide: PublishedDataService, useClass: MockPublishedDataApi },
+          { provide: PublishedDataV4Service, useClass: MockPublishedDataApi },
           { provide: Router, useClass: MockRouter },
           { provide: Store, useClass: MockStore },
         ],
