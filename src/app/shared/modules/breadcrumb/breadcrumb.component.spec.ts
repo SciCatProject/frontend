73599d6a4b7bb3495c5e0d1f3af5a2aa3f7ad60f
junjiequan <a331998513@gmail.com>
2026-06-01 11:14:30 +0200

    add unit test

--- src/app/shared/modules/breadcrumb/breadcrumb.component.spec.ts
+++ src/app/shared/modules/breadcrumb/breadcrumb.component.spec.ts
@@ -6,11 +6,29 @@ import {
 import { MockStore } from "shared/MockStubs";
 import { BreadcrumbComponent } from "./breadcrumb.component";
 import { Store } from "@ngrx/store";
-import { provideRouter } from "@angular/router";
+import { provideRouter, Router } from "@angular/router";
+import { Location } from "@angular/common";
+import {
+  selectArchiveViewMode,
+  selectFilters,
+} from "state-management/selectors/datasets.selectors";
+import { of } from "rxjs/internal/observable/of";
+import { ArchViewMode } from "state-management/models";
 
 describe("BreadcrumbComponent", () => {
   let component: BreadcrumbComponent;
   let fixture: ComponentFixture<BreadcrumbComponent>;
+  let router: Router;
+  let location: Location;
+  let store: Store;
+
+  const datasetsCrumb = {
+    label: "Datasets",
+    path: "datasets",
+    params: {},
+    url: "/datasets",
+    fallback: "/datasets",
+  };
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -27,6 +45,9 @@ describe("BreadcrumbComponent", () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BreadcrumbComponent);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
+    store = TestBed.inject(Store);
     fixture.detectChanges();
   });
 
@@ -36,5 +57,32 @@ describe("BreadcrumbComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should navigateByUrl to fallback when mode is empty", () => {
+    spyOn(store, "select").and.callFake((selector) => {
+      if (selector === selectFilters) return of({});
+      if (selector === selectArchiveViewMode) return of(ArchViewMode.all);
+      return of(null);
+    });
+    spyOn(router, "navigateByUrl").and.returnValue(Promise.resolve(true));
+
+    component.crumbClick(0, datasetsCrumb);
+
+    expect(router.navigateByUrl).toHaveBeenCalledWith("/datasets");
+  });
+
+  it("should call location.back() when mode is not empty", () => {
+    spyOn(store, "select").and.callFake((selector) => {
+      if (selector === selectFilters) return of({});
+      if (selector === selectArchiveViewMode)
+        return of(ArchViewMode.archivable);
+      return of(null);
+    });
+    spyOn(location, "back");
+
+    component.crumbClick(0, datasetsCrumb);
+
+    expect(location.back).toHaveBeenCalled();
   });
 });
