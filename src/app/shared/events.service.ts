ee02821afa0a3d0709fe02840a87b7c519ee07e0
junjiequan <a331998513@gmail.com>
2026-06-09 10:25:56 +0200

    init

---
+++ src/app/shared/events.service.ts
@@ -0,0 +1,60 @@
+import { Injectable, NgZone } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { fetchScicatTokenAction } from "state-management/actions/user.actions";
+import { selectUserSettingsPageViewModel } from "state-management/selectors/user.selectors";
+import { distinctUntilChanged, map, Subject, Subscription } from "rxjs";
+
+@Injectable({ providedIn: "root" })
+export class EventsService {
+  private eventSource: EventSource | null = null;
+  private tokenSub: Subscription | null = null;
+  private messageSubject = new Subject<Record<string, unknown>>();
+
+  message$ = this.messageSubject.asObservable();
+
+  constructor(
+    private ngZone: NgZone,
+    private store: Store,
+  ) {}
+
+  connect() {
+    if (this.tokenSub) return;
+
+    this.store.dispatch(fetchScicatTokenAction());
+
+    this.tokenSub = this.store
+      .select(selectUserSettingsPageViewModel)
+      .pipe(
+        map((vm) => vm?.scicatToken),
+        distinctUntilChanged(),
+      )
+      .subscribe((token) => {
+        if (token) {
+          this.openConnection(token);
+        } else {
+          this.closeConnection();
+        }
+      });
+  }
+
+  private openConnection(token: string) {
+    this.closeConnection();
+
+    this.eventSource = new EventSource(`/api/v3/events/stream?token=${token}`);
+    this.eventSource.onmessage = (event) => {
+      this.ngZone.run(() => this.messageSubject.next(JSON.parse(event.data)));
+    };
+    this.eventSource.onerror = () => this.closeConnection();
+  }
+
+  private closeConnection() {
+    this.eventSource?.close();
+    this.eventSource = null;
+  }
+
+  disconnect() {
+    this.closeConnection();
+    this.tokenSub?.unsubscribe();
+    this.tokenSub = null;
+  }
+}
