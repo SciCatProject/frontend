f68d92b6a77bdd79a1bde13ba40fe067d9fb6d19
Max Novelli <max.novelli@ess.eu>
2026-06-10 17:25:48 +0200

    added first draft of test for help page

---
+++ cypress/e2e/other/help-page.cy.js
@@ -0,0 +1,109 @@
+describe("Help configuration", () => {
+  const testHelpContent = "<p class=\"scicat_e2e_test\">SciCat E2E Test Help Content</p>";
+
+  beforeEach(() => {
+    cy.login(Cypress.env("adminUsername"), Cypress.env("adminPassword"));
+  });
+
+  afterEach(() => {
+    cy.get(".user-button").click();
+    cy.get("[data-cy=logout-button]").click();
+    cy.finishedLoading();
+  });
+
+  describe("Help Icon disabled", () => {
+    beforeEach(() => {
+      cy.updateFrontendConfig({
+        helpEnabled: false,
+      });
+      cy.get(".user-button").click();
+      cy.get("[data-cy=logout-button]").click();
+      cy.finishedLoading();
+    });
+
+    it("should not show help icon in header when disabled", () => {
+      cy.visit("/");
+      cy.finishedLoading();
+
+      cy.get("button[routerLink='/help']").should("not.exist");
+
+      cy.get("mat-icon").contains("help").should("not.exist");
+    });
+  });
+
+  describe("Help Icon enabled with custom content", () => {
+    beforeEach(() => {
+      cy.updateFrontendConfig({
+        helpEnabled: true,
+        helpHtmlContent: testHelpContent,
+      });
+      cy.get(".user-button").click();
+      cy.get("[data-cy=logout-button]").click();
+      cy.finishedLoading();
+    });
+
+    afterEach(() => {
+      cy.get(".user-button").click();
+      cy.get("[data-cy=logout-button]").click();
+      cy.finishedLoading();
+    });
+
+    it("should show help icons in header when enabled", () => {
+      cy.visit("/");
+      cy.finishedLoading();
+
+      cy.get("button[routerLink='/help']").should("exist");
+    });
+
+    it("should navigate to help page and display custom content", () => {
+      cy.visit("/");
+      cy.finishedLoading();
+
+      cy.get("button[routerLink='/help']").click();
+      cy.finishedLoading();
+      cy.url().should("include", "/help");
+
+      cy.get("p.scicat_e2e_test").should("contain", "SciCat E2E Test Help Content");
+    });
+  });
+
+  describe("Help Icon enabled with default content", () => {
+    beforeEach(() => {
+      cy.login(Cypress.env("adminUsername"), Cypress.env("adminPassword"));
+      cy.updateFrontendConfig({
+        helpEnabled: true,
+        helpHtmlContent: "",
+      });
+      cy.get(".user-button").click();
+      cy.get("[data-cy=logout-button]").click();
+      cy.finishedLoading();
+    });
+
+    afterEach(() => {
+      cy.get(".user-button").click();
+      cy.get("[data-cy=logout-button]").click();
+      cy.finishedLoading();
+    });
+
+    it("should show help icon in header when enabled", () => {
+      cy.visit("/");
+      cy.finishedLoading();
+
+      cy.get("button[routerLink='/help']").should("exist");
+    });
+
+    it("should show help icon with default page content", () => {
+      cy.visit("/");
+      cy.finishedLoading();
+
+      cy.get("button[routerLink='/help']").should("exist");
+
+      cy.get("button[routerLink='/help']").click();
+      cy.finishedLoading();
+
+      cy.url().should("include", "/help");
+
+      cy.get("div.help").should("contain", "No help content available");
+    });
+  });
+});
