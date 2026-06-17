ff0db62a1431d5088b819acc63e4c715ae4c7fc1
junjiequan <a331998513@gmail.com>
2026-06-17 10:37:46 +0200

    fix: unsubscribe subscription in search-parameters-dialog component

--- src/app/shared/modules/search-parameters-dialog/search-parameters-dialog.component.ts
+++ src/app/shared/modules/search-parameters-dialog/search-parameters-dialog.component.ts
@@ -10,6 +10,7 @@ import { ConnectedPosition } from "@angular/cdk/overlay";
 import { Store } from "@ngrx/store";
 import { selectMetadataKeys } from "state-management/selectors/datasets.selectors";
 import { fetchMetadataKeysAction } from "state-management/actions/datasets.actions";
+import { Subscription } from "rxjs";
 
 export interface SearchParametersDialogData {
   parameterKeys: string[];
@@ -26,6 +27,7 @@ export interface SearchParametersDialogData {
   standalone: false,
 })
 export class SearchParametersDialogComponent {
+  subscription: Subscription;
   appConfig = this.appConfigService.getConfig();
   unitsEnabled = this.appConfig.scienceSearchUnitsEnabled;
 
@@ -149,5 +151,9 @@ export class SearchParametersDialogComponent {
 
   getHumanName(key: string): string {
     return this.humanNameMap[key] || "";
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
