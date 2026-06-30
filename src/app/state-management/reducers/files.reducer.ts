ff9483ea5b3586ded63c98c1f68a4b57a137162e
Abdi Mo <abdi.abdulle@outlook.dk>
2026-06-30 10:11:31 +0200

    added loading spinner for files table

--- src/app/state-management/reducers/files.reducer.ts
+++ src/app/state-management/reducers/files.reducer.ts
@@ -16,10 +16,27 @@ const reducer = createReducer(
   ),
 
   on(
+    fromActions.fetchCountAction,
+    (state): FilesState => ({
+      ...state,
+      filesCountIsLoading: true,
+    }),
+  ),
+
+  on(
     fromActions.fetchCountCompleteAction,
     (state, { count }): FilesState => ({
       ...state,
       totalCount: count,
+      filesCountIsLoading: false,
+    }),
+  ),
+
+  on(
+    fromActions.fetchCountFailedAction,
+    (state): FilesState => ({
+      ...state,
+      filesCountIsLoading: false,
     }),
   ),
 
