9c8b8053b1e2cd62f1e3d06ae24e9cf0b91aa27b
André Baumberger <github.legume822@silomails.com>
2026-07-21 07:43:13 +0000

    + autofill user + autofill group

--- src/app/ingestor/ingestor-dialogs/creation-dialog/creation-pages/ingestor.new-transfer-dialog-page.component.ts
+++ src/app/ingestor/ingestor-dialogs/creation-dialog/creation-pages/ingestor.new-transfer-dialog-page.component.ts
@@ -70,6 +70,7 @@ export class IngestorNewTransferDialogPageComponent
   extractionMethodsInitialized = false;
 
   userProfile: ReturnedUserDto | null = null;
+  groups: string[] = [];
   uiNextButtonReady = false;
 
   renderView$ = this.store.select(selectIngestorRenderView);
@@ -98,6 +99,7 @@ export class IngestorNewTransferDialogPageComponent
     this.subscriptions.push(
       this.vm$.subscribe((settings) => {
         this.userProfile = settings.user;
+        this.groups = settings.profile?.accessGroups ?? [];
       }),
     );
 
@@ -231,7 +233,8 @@ export class IngestorNewTransferDialogPageComponent
     this.createNewTransferData.scicatHeader["license"] = "MIT License";
     this.createNewTransferData.scicatHeader["type"] = "raw";
     this.createNewTransferData.scicatHeader["dataFormat"] = "root";
-    this.createNewTransferData.scicatHeader["owner"] = "User";
+    this.createNewTransferData.scicatHeader["ownerGroup"] = this.groups.length > 0 ? this.groups[0] : undefined;
+    this.createNewTransferData.scicatHeader["owner"] = this.userProfile.username;
 
     this.createNewTransferData.scicatHeader["principalInvestigator"] =
       this.userProfile.username;
