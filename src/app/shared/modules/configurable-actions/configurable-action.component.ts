3f186c95fe64d4645236828c1b44f66a919cd3fb
Johannes Reppin <johannes.reppin@desy.de>
2026-08-11 10:56:55 +0000

    add #userIsLoggedIn condition for configurable actions.

--- src/app/shared/modules/configurable-actions/configurable-action.component.ts
+++ src/app/shared/modules/configurable-actions/configurable-action.component.ts
@@ -29,6 +29,7 @@ import { Store } from "@ngrx/store";
 import { AppConfigService } from "app-config.service";
 import {
   selectIsAdmin,
+  selectIsLoggedIn,
   selectProfile,
 } from "state-management/selectors/user.selectors";
 import { Subscription } from "rxjs";
@@ -71,6 +72,7 @@ export class ConfigurableActionComponent
 
   userProfile$ = this.store.select(selectProfile);
   isAdmin$ = this.store.select(selectIsAdmin);
+  isLoggedIn$ = this.store.select(selectIsLoggedIn);
 
   jwt = "";
   useMatIcon = false;
@@ -79,6 +81,7 @@ export class ConfigurableActionComponent
   variables: Record<string, unknown> = {};
   userProfile: Record<string, unknown> = {};
   isAdmin = false;
+  isLoggedIn = false;
   subscriptions: Subscription[] = [];
   form: HTMLFormElement | null = null;
 
@@ -240,9 +243,10 @@ export class ConfigurableActionComponent
   private viewHandlers(condition: string): string {
     let expr = condition;
     const symbols: Record<string, string> = {
-      "#datasetOwner": "context.isOwner",
+      "#datasetOwner": "context.isDatasetOwner",
       "#publishedDataOwner": "context.isPublishedDataOwner",
       "#userIsAdmin": "context.isAdmin",
+      "#userIsLoggedIn": "context.isLoggedIn",
       "#isPublished": String(
         this.actionItems.datasets?.[0]?.isPublished === true,
       ),
@@ -296,7 +300,8 @@ export class ConfigurableActionComponent
         variables: this.variables,
         context: {
           isAdmin: this.isAdmin,
-          isOwner: this.isDatasetOwner,
+          isLoggedIn: this.isLoggedIn,
+          isDatasetOwner: this.isDatasetOwner,
           isPublishedDataOwner: this.isPublishedDataOwner,
           maxSize: this.configService.getConfig().maxDirectDownloadSize,
         },
@@ -609,6 +614,9 @@ export class ConfigurableActionComponent
     );
     this.subscriptions.push(
       this.isAdmin$.subscribe((ia) => (this.isAdmin = ia)),
+    );
+    this.subscriptions.push(
+      this.isLoggedIn$.subscribe((il) => (this.isLoggedIn = il)),
     );
     this.useMatIcon = !!this.actionConfig.mat_icon;
     this.useIcon = this.actionConfig.icon !== undefined;
